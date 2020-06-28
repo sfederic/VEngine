f7d1f8aab0f4e18222604f0af332a4851e0ee1a7
sfederic <furshazzle@gmail.com>
2020-06-28 21:27:12 +1000

    Quick swap of singletons off of heap

--- d3d11/main.cpp
+++ d3d11/main.cpp
@@ -16,21 +16,25 @@ struct Matrices
 	XMMATRIX mvp;
 }matrices;
 
+Win32Util win32;
+DXUtil dx;
+UIContext ui;
+
 int __stdcall WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, int cmdShow)
 {
-	Win32Util* win32 = new Win32Util;
-	DXUtil* dx = new DXUtil;
-	UIContext* ui = new UIContext;
+	//Win32Util* win32 = new Win32Util;
+	//DXUtil* dx = new DXUtil;
+	//UIContext* ui = new UIContext;
 
-	win32->SetupWindow(instance, cmdShow);
-	win32->SetTimerFrequency();
+	win32.SetupWindow(instance, cmdShow);
+	win32.SetTimerFrequency();
 
-	dx->CreateDevice();
-	dx->CreateSwapchain();
-	dx->CreateRTVAndDSV();
-	dx->CreateShaders();
-	dx->CreateInputLayout();
-	dx->CreateRasterizerStates();
+	dx.CreateDevice();
+	dx.CreateSwapchain();
+	dx.CreateRTVAndDSV();
+	dx.CreateShaders();
+	dx.CreateInputLayout();
+	dx.CreateRasterizerStates();
 
 	Camera camera = {};
 	camera.focusPoint = XMVectorSet(0.f, 0.f, 0.f, 1.f);
@@ -40,21 +44,20 @@ int __stdcall WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine,
 	matrices.model = XMMatrixIdentity();
 	camera.view = XMMatrixLookAtLH(camera.location, camera.focusPoint, camera.worldUp);
 	matrices.view = camera.view;
-	matrices.proj = XMMatrixPerspectiveFovLH(XM_PI / 3, win32->GetAspectRatio(), 0.01f, 1000.f);
+	matrices.proj = XMMatrixPerspectiveFovLH(XM_PI / 3, win32.GetAspectRatio(), 0.01f, 1000.f);
 	matrices.mvp = matrices.model * matrices.view * matrices.proj;
 
-	ID3D11Buffer* cbMatrices = dx->CreateDefaultBuffer(sizeof(Matrices), D3D11_BIND_CONSTANT_BUFFER, &matrices);
-	dx->context->VSSetConstantBuffers(0, 1, &cbMatrices);
+	ID3D11Buffer* cbMatrices = dx.CreateDefaultBuffer(sizeof(Matrices), D3D11_BIND_CONSTANT_BUFFER, &matrices);
+	dx.context->VSSetConstantBuffers(0, 1, &cbMatrices);
 
-	ui->init(dx->swapchain);
-
+	ui.init(dx.swapchain);
 
 	//Temp model loading
 	OBJData model;
 	if (loadOBJFile("Models/sphere.obj", model))
 	{
 		UINT byteWidth = model.GetByteWidth();
-		dx->CreateVertexBuffer(byteWidth, model.verts.data());
+		dx.CreateVertexBuffer(byteWidth, model.verts.data());
 	}
 
 	AudioContext* ac = new AudioContext;
@@ -63,7 +66,7 @@ int __stdcall WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine,
 	//MAIN LOOP
 	while (msg.message != WM_QUIT) 
 	{
-		win32->StartTimer();
+		win32.StartTimer();
 
 		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
 		{
@@ -71,64 +74,65 @@ int __stdcall WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine,
 			DispatchMessage(&msg);
 		}
 
-		ui->update();
-		dx->Render();
+		ui.update();
+		dx.Render();
 
 		if (GetAsyncKeyState('1'))
 		{
-			dx->context->RSSetState(dx->rastStateWireframe);
+			dx.context->RSSetState(dx.rastStateWireframe);
 		}
 		if (GetAsyncKeyState('2'))
 		{
-			dx->context->RSSetState(dx->rastStateSolid);
+			dx.context->RSSetState(dx.rastStateSolid);
 		}
 
 
 		if (GetAsyncKeyState('W'))
 		{
-			camera.MoveForward(5.f * win32->delta);
+			camera.MoveForward(5.f * win32.delta);
 		}
 		if (GetAsyncKeyState('S'))
 		{
-			camera.MoveForward(-5.f * win32->delta);
+			camera.MoveForward(-5.f * win32.delta);
 		}
 		if (GetAsyncKeyState('D'))
 		{
-			camera.Strafe(5.f * win32->delta);
+			camera.Strafe(5.f * win32.delta);
 		}
 		if (GetAsyncKeyState('A'))
 		{
-			camera.Strafe(-5.f * win32->delta);
+			camera.Strafe(-5.f * win32.delta);
 		}
 		if (GetAsyncKeyState('Q'))
 		{
-			camera.MoveUp(-5.f * win32->delta);
+			camera.MoveUp(-5.f * win32.delta);
 		}
 		if (GetAsyncKeyState('E'))
 		{
-			camera.MoveUp(5.f * win32->delta);
+			camera.MoveUp(5.f * win32.delta);
 		}
 
-		camera.MouseMove(ui->mousePos.x, ui->mousePos.y);
+		camera.MouseMove(ui.mousePos.x, ui.mousePos.y);
 
 		camera.UpdateViewMatrix();
 		//matrices.view = XMMatrixLookAtLH(camera.location, camera.focusPoint, camera.worldUp);
 		matrices.view = camera.view;
 		matrices.mvp = matrices.model * matrices.view * matrices.proj;
-		dx->context->UpdateSubresource(cbMatrices, 0, nullptr, &matrices, 0, 0);
+		dx.context->UpdateSubresource(cbMatrices, 0, nullptr, &matrices, 0, 0);
 
-		ui->d2dRenderTarget->BeginDraw();
+		//TODO: Put render and d2d stuff into func for profiling
+		ui.d2dRenderTarget->BeginDraw();
 
-		ui->d2dRenderTarget->EndDraw();
+		ui.d2dRenderTarget->EndDraw();
 
-		dx->context->Draw(model.verts.size(), 0);
+		dx.context->Draw(model.verts.size(), 0);
 
-		HR(dx->swapchain->Present(1, 0));
+		HR(dx.swapchain->Present(1, 0));
 
-		win32->EndTimer();
+		win32.EndTimer();
 	}
 
-	ui->cleanup();
+	ui.cleanup();
 
 	return (int)msg.wParam;
 }
