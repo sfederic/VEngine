81bf1cd9c3b9ef792a7059e094d5f482c0d5abc0
sfederic <furshazzle@gmail.com>
2021-04-11 19:54:34 +1000

    Tiny fix to normal transformation in default shader (assumes uniform scaling)

--- VEngine/Shaders/shaders.hlsl
+++ VEngine/Shaders/shaders.hlsl
@@ -5,7 +5,7 @@ VS_OUT VSMain(VS_IN i)
 	VS_OUT o;
 	o.pos = mul(mvp, float4(i.pos, 1.0f));
 	o.uv = i.uv;
-	o.normal = mul(i.normal, (float3x3)model);
+	o.normal = mul((float3x3)model, i.normal);
 
 	return o;
 }
@@ -13,7 +13,7 @@ VS_OUT VSMain(VS_IN i)
 float4 PSMain(VS_OUT i) : SV_Target
 {
 	float4 texColour = t.Sample(s, i.uv);
-	float3 lightDir = float3(0.f, 1.f, 1.f);
+	float3 lightDir = float3(0.f, 0.f, 1.f);
 	float diffuse = dot(-lightDir, i.normal);
 
 	//clip(texColour.a - 0.1f);
