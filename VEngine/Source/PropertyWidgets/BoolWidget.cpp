13213d2befdf2d3365254480ff42666ead10979b
sfederic <furshazzle@gmail.com>
2021-05-04 00:43:47 +1000

    BoolWidget value set in constructor

--- VEngine/Source/PropertyWidgets/BoolWidget.cpp
+++ VEngine/Source/PropertyWidgets/BoolWidget.cpp
@@ -3,6 +3,15 @@
 BoolWidget::BoolWidget(bool* value, const char* name, QWidget* parent) : QCheckBox(parent)
 {
 	_value = value;
+	if (*_value == true)
+	{
+		setCheckState(Qt::CheckState::Checked);
+	}
+	else
+	{
+		setCheckState(Qt::CheckState::Unchecked);
+	}
+
 	connect(this, &QCheckBox::stateChanged, this, &BoolWidget::SetValue);
 
 	setText(name);
