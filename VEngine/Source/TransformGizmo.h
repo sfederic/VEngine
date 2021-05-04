2964555f5b98946e6750677e6e164063bf6b6472
sfederic <furshazzle@gmail.com>
2021-05-04 23:19:29 +1000

    Imguizmo reverse click detection on rotation gizmo fix

--- VEngine/Source/TransformGizmo.h
+++ VEngine/Source/TransformGizmo.h
@@ -3,6 +3,10 @@
 #include "imgui/imgui.h"
 #include "Imguizmo/ImGuizmo.h"
 
+//NOTE: There was an issue with ImGuizmo where the rotation gizmo would raycast in the opposite direction
+//(with LH matrices?) and only hit the back of the gizmo. The fix was in ImGuizmo.cpp at line 1777.
+//https://github.com/CedricGuillemet/ImGuizmo/issues/144
+
 //Using ImGuizmo for transform, scale and rotation. Press W, E or R for Translation, Scale or Rotation.
 //NOTE: Keep in mind that ImGuizmo draws its gizmos inside an ImGui window, and not directly onto the viewport.
 class TransformGizmo
