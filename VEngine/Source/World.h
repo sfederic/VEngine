c1849bb5da1c68dfcafd5c588ebcc43370cc4fc3
sfederic <furshazzle@gmail.com>
2021-05-14 00:27:23 +1000

    Added SpawnActor functions to World.h

--- VEngine/Source/World.h
+++ VEngine/Source/World.h
@@ -4,6 +4,7 @@
 #include "Actor.h"
 #include <unordered_map>
 #include <string>
+#include "ActorSystemFactory.h"
 
 class World
 {
@@ -12,6 +13,27 @@ public:
 	void TickAllActorSystems(float deltaTime);
 	void CleaupAllActors();
 	void AddActorSystem(ActorSystem* actorSystem);
+
+	template <class ActorType>
+	Actor* SpawnActor(Transform transform)
+	{
+		ActorSystem* actorSystem = ActorSystemFactory::GetActorSystem(typeid(ActorType));
+		if (actorSystem)
+		{
+			actorSystem->SpawnActor(transform);
+		}
+	}
+
+	template <class ActorType>
+	void SpawnActors(std::vector<Transform>& transforms, uint64_t numActorsToSpawn)
+	{
+		ActorSystem* actorSystem = ActorSystemFactory::GetActorSystem(typeid(ActorType));
+		if (actorSystem)
+		{
+			actorSystem->SpawnActors(transforms, numActorsToSpawn);
+		}
+	}
+
 	Actor* FindActorByString(std::wstring name);
 	ActorSystem* GetActorSystem(unsigned int id);
 	Actor* GetActor(unsigned int systemId, unsigned int actorId);
