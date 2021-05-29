e80db084822e24f2d37202b49203057d7891c3ce
sfederic <furshazzle@gmail.com>
2021-05-29 17:24:48 +1000

    EditorSystem Print call in debug.cpp fix

--- VEngine/Source/Debug.cpp
+++ VEngine/Source/Debug.cpp
@@ -8,7 +8,6 @@ void DXTrace(HRESULT hr, const char* filename, const char* func, int line)
 	_com_error err(hr);
 	char errmsg[1024];
 	snprintf(errmsg, sizeof(errmsg), "HR: %s\nFile: %s\nFunction: %s\nLine: %d", err.ErrorMessage(), filename, func, line);
-	//gEditorMainWindow->Print(errmsg);
 	MessageBox(0, errmsg, "Error", 0);
 }
 
@@ -16,8 +15,10 @@ int Print(const char* format, va_list args)
 {
 	char buff[1024];
 	int charCount = vsnprintf(buff, sizeof(buff), format, args);
+
 	OutputDebugString(buff);
-	//gEditorMainWindow->Print(buff);
+	gEditorSystem->Print(buff);
+
 	return charCount;
 }
 
@@ -26,6 +27,5 @@ int DebugPrint(const char* format, ...)
 	va_list argList;
 	va_start(argList, format);
 	int charsWritten = Print(format, argList);
-	//gEditorMainWindow->Print(format);
 	return charsWritten;
 }
