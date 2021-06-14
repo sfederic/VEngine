9ac2de025f0d562684797433ff08c8ace3bf73a0
sfederic <furshazzle@gmail.com>
2021-06-14 12:08:31 +1000

    MaterialSystem:CreateMaterialFromFile testing

--- VEngine/Source/Material.cpp
+++ VEngine/Source/Material.cpp
@@ -1,1 +1,14 @@
 #include "Material.h"
+#include <fstream>
+
+Material* MaterialSystem::CreateMaterialFromFile(const std::string& filename)
+{
+	Material newMaterial = {};
+
+	std::filebuf fb;
+	fb.open(filename.c_str(), std::ios_base::in);
+	std::istream is(&fb);
+
+	materials.push_back(newMaterial);
+	return &materials.back();
+}
--- VEngine/Source/Material.h
+++ VEngine/Source/Material.h
@@ -1,17 +1,22 @@
 #pragma once
 
 #include <DirectXMath.h>
+#include <string>
+#include <vector>
 
 using namespace DirectX;
 
 class Material
 {
 public:
+	std::string name;
 	XMFLOAT4 colour;
 };
 
 class MaterialSystem
 {
 public:
-	Material* CreateMaterial();
+	Material* CreateMaterialFromFile(const std::string& filename);
+
+	std::vector<Material> materials;
 };
