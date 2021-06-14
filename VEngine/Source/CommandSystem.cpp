fecf2f6cd4d4b46d3b041cdcf65de1c59356a927
sfederic <furshazzle@gmail.com>
2021-06-14 23:48:45 +1000

    Command system functions

--- VEngine/Source/CommandSystem.cpp
+++ VEngine/Source/CommandSystem.cpp
@@ -1,1 +1,35 @@
 #include "CommandSystem.h"
+#include <assert.h>
+
+CommandSystem gCommandSystem;
+
+void CommandSystem::AddCommand(ICommand* command)
+{
+	assert(command);
+	commands.push_back(command);
+	currentCommandIndex = commands.size() - 1;
+}
+
+void CommandSystem::Undo()
+{
+	if (currentCommandIndex <= 0)
+	{
+		commands.front()->Undo();
+		return; //Reached start of list
+	}
+
+	commands[currentCommandIndex]->Undo();
+	currentCommandIndex--;
+}
+
+void CommandSystem::Redo()
+{
+	if (currentCommandIndex >= commands.size() - 1)
+	{
+		commands.back()->Execute();
+		return; //Reached end of list
+	}
+
+	currentCommandIndex++;
+	commands[currentCommandIndex]->Execute();
+}
--- VEngine/Source/CommandSystem.h
+++ VEngine/Source/CommandSystem.h
@@ -1,5 +1,20 @@
 #pragma once
+
+#include "ICommand.h"
+#include <vector>
+
+//currently just for editor undo/redo, but I guess the pattern is generic enough that it 
+//could be used in other spots.
 class CommandSystem
 {
+public:
+	void AddCommand(ICommand* command);
+	void Undo();
+	void Redo();
+
+private:
+	std::vector<ICommand*> commands;
+	uint32_t currentCommandIndex = 0;
 };
 
+extern CommandSystem gCommandSystem;
