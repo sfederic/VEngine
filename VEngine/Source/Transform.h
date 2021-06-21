5ac47ae7322927c21c255a7f6c0c4f83651db3ac
sfederic <furshazzle@gmail.com>
2021-06-21 22:43:31 +1000

    Added world and local mats to Transform

--- VEngine/Source/Transform.h
+++ VEngine/Source/Transform.h
@@ -12,6 +12,9 @@ public:
 	XMMATRIX GetAffineRotationOrigin(XMVECTOR rotOrigin);
 	void Decompose(XMMATRIX m);
 
+	XMMATRIX world;
+	XMMATRIX local;
+
 	XMFLOAT3 position;
 	XMFLOAT3 scale;
 	XMFLOAT3 euler;
