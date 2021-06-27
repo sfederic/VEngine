9939db496eeadccc59d5cc3ed0d2207a6ec08dce
sfederic <45758254+sfederic@users.noreply.github.com>
2021-06-28 09:35:58 +1000

    Update TransformGizmo.h

--- VEngine/Source/TransformGizmo.h
+++ VEngine/Source/TransformGizmo.h
@@ -3,6 +3,9 @@
 #include "imgui/imgui.h"
 #include "Imguizmo/ImGuizmo.h"
 
+//REF: Blog about building custom imgui gizmos.
+//https://maxliani.wordpress.com/2021/06/26/offline-to-realtime-gesture-graphics/
+
 //NOTE: There was an issue with ImGuizmo where the rotation gizmo would raycast in the opposite direction
 //(with LH matrices?) and only hit the back of the gizmo. The fix was in ImGuizmo.cpp at line 1777.
 //https://github.com/CedricGuillemet/ImGuizmo/issues/144
