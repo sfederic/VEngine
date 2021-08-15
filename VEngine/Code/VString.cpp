0b5aaee9a53325c269c9a9b99bcb317206a2b0cf
sfederic <furshazzle@gmail.com>
2021-08-15 13:03:09 +1000

    added vstring back in

---
+++ VEngine/Code/VString.cpp
@@ -0,0 +1,10 @@
+#include "VString.h"
+#include <Windows.h>
+
+std::wstring stows(const std::string& str)
+{
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+    std::wstring wstrTo(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+    return wstrTo;
+}
---
+++ VEngine/Code/VString.h
@@ -0,0 +1,4 @@
+#include <string>
+
+//Stolen from: https://stackoverflow.com/questions/10737644/convert-const-char-to-wstring
+std::wstring stows(const std::string& str);
