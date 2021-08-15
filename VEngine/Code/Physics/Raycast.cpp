80d87ed1aeb0156c2ae26521f27652e33eb469be
sfederic <furshazzle@gmail.com>
2021-08-15 22:23:56 +1000

    Boundingbox fix for spatialcomponent extent in raycast

--- VEngine/Code/Physics/Raycast.cpp
+++ VEngine/Code/Physics/Raycast.cpp
@@ -69,7 +69,9 @@ bool Raycast(Ray& ray, XMVECTOR origin, XMVECTOR direction, IActorSystem* actorS
 
 				boundingBox.Center = spatialComponent->transform.position;
 				boundingBox.Orientation = spatialComponent->transform.rotation;
-				boundingBox.Extents = spatialComponent->transform.scale;
+				boundingBox.Extents.x *= spatialComponent->transform.scale.x;
+				boundingBox.Extents.y *= spatialComponent->transform.scale.y;
+				boundingBox.Extents.z *= spatialComponent->transform.scale.z;
 
 				if (boundingBox.Intersects(ray.origin, ray.direction, ray.distance))
 				{
