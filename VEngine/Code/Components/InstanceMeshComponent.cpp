a011b188b6849b6e8db4215727c2c17627026b3f
sfederic <furshazzle@gmail.com>
2021-08-22 23:59:27 +1000

    More setup for InstanceMeshComponent

--- VEngine/Code/Components/InstanceMeshComponent.cpp
+++ VEngine/Code/Components/InstanceMeshComponent.cpp
@@ -1,11 +1,36 @@
 #include "InstanceMeshComponent.h"
+#include "Render/Renderer.h"
 
-InstanceMeshComponent::InstanceMeshComponent(const char* filename, const wchar_t* shader)
+InstanceMeshComponent::InstanceMeshComponent(
+	uint32_t meshInstanceRenderCount_, const char* filename, const wchar_t* shader)
 	: MeshComponent(filename, shader)
 {
+	meshInstanceRenderCount = meshInstanceRenderCount_;
+}
+
+InstanceMeshComponent::~InstanceMeshComponent()
+{
+	structuredBuffer->Release();
+	srv->Release();
 }
 
 void InstanceMeshComponent::Create()
 {
 	MeshComponent::Create();
+
+	//Populate instance data
+	instanceData.reserve(meshInstanceRenderCount);
+
+	for (uint32_t i = 0; i < meshInstanceRenderCount; i++)
+	{
+		instanceData.push_back(InstanceData());
+	}
+
+	//Setup shader buffers
+	structuredBuffer = renderer.CreateStructuredBuffer(sizeof(InstanceData) * instanceData.size(),
+		sizeof(InstanceData), instanceData.data());
+	assert(structuredBuffer);
+
+	srv = renderer.CreateSRVForMeshInstance(structuredBuffer);
+	assert(srv);
 }
--- VEngine/Code/Components/InstanceMeshComponent.h
+++ VEngine/Code/Components/InstanceMeshComponent.h
@@ -1,12 +1,24 @@
 #pragma once
 #include "Components/MeshComponent.h"
 
+struct ID3D11Buffer;
+struct ID3D11ShaderResourceView;
+
 struct InstanceMeshComponent : MeshComponent
 {
 	COMPONENT_SYSTEM(InstanceMeshComponent)
 
+	ID3D11Buffer* structuredBuffer = nullptr;
+	ID3D11ShaderResourceView* srv = nullptr;
+
+	std::vector<InstanceData> instanceData;
+
+private:
 	uint32_t meshInstanceRenderCount = 0;
 
-	InstanceMeshComponent(const char* filename, const wchar_t* shader = L"InstanceShader.hlsl");
+public:
+	InstanceMeshComponent(
+		uint32_t meshInstanceRenderCount_, const char* filename, const wchar_t* shader = L"InstanceShader.hlsl");
+	~InstanceMeshComponent();
 	virtual void Create();
 };
