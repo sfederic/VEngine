b8193a50e6d8623bcf36e93a60ce24efe0d01c79
sfederic <furshazzle@gmail.com>
2021-09-04 11:24:15 +1000

    MeshData comments

--- VEngine/Code/Render/RenderTypes.h
+++ VEngine/Code/Render/RenderTypes.h
@@ -12,12 +12,15 @@ struct Vertex
 	XMFLOAT2 uv;
 };
 
+//The actual data for each loaded mesh. Each loaded mesh file will have one of these per its filename.
 struct MeshData
 {
 	std::vector<Vertex> vertices;
 	std::vector<uint32_t> indices;
 };
 
+//A pointer structure to a MeshData struct in memory. Each rendered component will have one of these pointing
+//to the mesh data on a per-filename basis.
 struct MeshDataProxy
 {
 	std::vector<Vertex>* vertices = nullptr;
