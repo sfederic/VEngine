ac9a9fac883acc601072978e6ad151c37f658635
sfederic <furshazzle@gmail.com>
2021-09-10 17:22:55 +1000

    more spotlight component props

--- VEngine/Code/Components/Lights/SpotLightComponent.cpp
+++ VEngine/Code/Components/Lights/SpotLightComponent.cpp
@@ -18,5 +18,8 @@ Properties SpotLightComponent::GetProps()
     Properties props("SpotLightComponent");
     props.Add("Colour", &lightData.colour);
     props.Add("Spot Angle", &lightData.spotAngle);
+    props.Add("Const Atten", &lightData.constantAtten);
+    props.Add("Linear Atten", &lightData.linearAtten);
+    props.Add("Quad Atten", &lightData.quadraticAtten);
     return props;
 }
