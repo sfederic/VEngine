a6117b2462ae71ad951ddc602b75158aeca24da0
sfederic <furshazzle@gmail.com>
2021-09-12 22:08:42 +1000

    added comments and references to TransformGizmo about grid layering and whatever

--- VEngine/Code/Editor/TransformGizmo.h
+++ VEngine/Code/Editor/TransformGizmo.h
@@ -5,12 +5,17 @@
 //REF: Blog about building custom imgui gizmos.
 //https://maxliani.wordpress.com/2021/06/26/offline-to-realtime-gesture-graphics/
 
+//REF: OurMachinery guys also have a 3 part blog about gizmo related stuff
+//https://ourmachinery.com/post/borderland-between-rendering-and-editor-part-1/
+
 //NOTE: There was an issue with ImGuizmo where the rotation gizmo would raycast in the opposite direction
 //(with LH matrices?) and only hit the back of the gizmo. The fix was in ImGuizmo.cpp at line 1777.
 //https://github.com/CedricGuillemet/ImGuizmo/issues/144
 
 //Using ImGuizmo for transform, scale and rotation. Press W, E or R for Translation, Scale or Rotation.
-//NOTE: Keep in mind that ImGuizmo draws its gizmos inside an ImGui window, and not directly onto the viewport.
+//Keep in mind that ImGuizmo draws its gizmos inside an ImGui window, and not directly onto the viewport.
+//Also, Imguizmo's grid renders over the top of actors in the world, but trying to fix it just brings
+//up all sorts of mouse input logic that isn't worth having.
 struct TransformGizmo
 {
 	ImGuizmo::OPERATION currentTransformOperation = ImGuizmo::TRANSLATE;
