17baf4703174d2041b48bf473111f6602096afc5
sfederic <furshazzle@gmail.com>
2021-09-30 00:02:59 +1000

    InteractTrigger 3D widget testing

--- VEngine/Code/Actors/InteractTrigger.cpp
+++ VEngine/Code/Actors/InteractTrigger.cpp
@@ -1,29 +1,38 @@
 #include "InteractTrigger.h"
 #include "Components/BoxTriggerComponent.h"
 #include "GameUtils.h"
-
-#include "Editor/Editor.h"
+#include "UI/Widget.h"
 
 InteractTrigger::InteractTrigger()
 {
 	trigger = BoxTriggerComponent::system.Add(this);
 	rootComponent = trigger;
+
+	widget = new Widget();
 }
 
 void InteractTrigger::Start()
 {
 	trigger->target = (Actor*)GameUtils::GetPlayer();
+
+	widget->pos = GetHomogeneousPositionVector();
 }
 
 void InteractTrigger::Tick(double deltaTime)
 {
 	if (trigger->ContainsTarget())
 	{
-		editor->Log("inside");
+		widget->AddToViewport();
+	}
+	else
+	{
+		widget->RemoveFromViewport();
 	}
 }
 
 Properties InteractTrigger::GetProps()
 {
-	return Actor::GetProps();
+	auto props = Actor::GetProps();
+	props.Add("Interact Text", &interactText);
+	return props;
 }
--- VEngine/Code/Actors/InteractTrigger.h
+++ VEngine/Code/Actors/InteractTrigger.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "Actor.h"
 #include "ActorSystem.h"
+#include <string>
 
 struct BoxTriggerComponent;
+struct Widget;
 
 //Used to open up a quick prompt/action when the player overlaps
 struct InteractTrigger : Actor
@@ -10,6 +12,9 @@ struct InteractTrigger : Actor
 	ACTOR_SYSTEM(InteractTrigger)
 
 	BoxTriggerComponent* trigger = nullptr;
+	Widget* widget = nullptr;
+
+	std::string interactText;
 
 	InteractTrigger();
 	virtual void Start() override;
