0f33208276114b667931185d5dd3b9bb25c40647
sfederic <furshazzle@gmail.com>
2021-10-02 15:19:03 +1000

    created EmptyComponent

---
+++ VEngine/Code/Components/EmptyComponent.cpp
@@ -0,0 +1,18 @@
+#include "EmptyComponent.h"
+
+EmptyComponent::EmptyComponent()
+{
+}
+
+void EmptyComponent::Tick(double deltaTime)
+{
+}
+
+void EmptyComponent::Create()
+{
+}
+
+Properties EmptyComponent::GetProps()
+{
+    return Properties();
+}
---
+++ VEngine/Code/Components/EmptyComponent.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "SpatialComponent.h"
+#include "ComponentSystem.h"
+
+//An empty spatial component. Used mainly as a basic root compontent for an actor.
+struct EmptyComponent : SpatialComponent
+{
+	COMPONENT_SYSTEM(EmptyComponent)
+
+	EmptyComponent();
+	virtual void Tick(double deltaTime) override;
+	virtual void Create() override;
+	virtual Properties GetProps() override;
+};
