699e1f1b209316da726517768ccce06737467939
sfederic <furshazzle@gmail.com>
2021-10-07 22:50:19 +1100

    created UI shader file for basic viewport rendering

---
+++ VEngine/Code/Render/Shaders/ui.hlsl
@@ -0,0 +1,30 @@
+#include "Include/CommonTypes.hlsli"
+
+struct TransformOut
+{
+	VS_OUT Transform(VS_IN i)
+	{
+		VS_OUT o;
+
+		o.pos = mul(mvp, float4(i.pos, 1.0f));
+		o.posWS = mul(model, float4(i.pos, 1.0f));
+		float4 newUv = mul(texMatrix, float4(i.uv, 0.f, 1.0f));
+		o.uv = float2(newUv.x, newUv.y);
+		o.normal = mul((float3x3)model, i.normal);
+
+		o.shadowPos = float4(1.f, 1.f, 1.f, 1.f);
+
+		return o;
+	}
+};
+
+VS_OUT VSMain(VS_IN i)
+{
+	TransformOut t;
+	return t.Transform(i);
+}
+
+float4 PSMain(VS_OUT i) : SV_Target
+{
+	return float4(1.f, 1.f, 1.f, 1.f);
+}
