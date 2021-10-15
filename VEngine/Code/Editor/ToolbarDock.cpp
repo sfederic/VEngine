aa10f61281b9c2c2c7c260388a9f132bc027e9e8
sfederic <furshazzle@gmail.com>
2021-10-15 21:14:09 +1100

    Toolbardock transformgizmo snap inputs

--- VEngine/Code/Editor/ToolbarDock.cpp
+++ VEngine/Code/Editor/ToolbarDock.cpp
@@ -1,7 +1,10 @@
 #include "ToolbarDock.h"
 #include <qpushbutton.h>
 #include <qboxlayout.h>
+#include <qlabel.h>
+#include "PropertyWidgets/SpinBox.h"
 #include "Core.h"
+#include "TransformGizmo.h"
 
 ToolbarDock::ToolbarDock() : QDockWidget("Toolbar")
 {
@@ -9,12 +12,41 @@ ToolbarDock::ToolbarDock() : QDockWidget("Toolbar")
 	hLayout->setSpacing(10);
 	hLayout->setAlignment(Qt::AlignHCenter);
 
+
+	//Play buttons
 	playButton = new QPushButton("Play");
 	connect(playButton, &QPushButton::clicked, this, &ToolbarDock::StartPlay);
 	hLayout->addWidget(playButton);
 
 	pauseButton = new QPushButton("Pause");
 	hLayout->addWidget(pauseButton);
+
+
+	//TRANSFORMGIZMO SNAP INPUTS
+	//Translation
+	translationSnapSpinBox = new SpinBox(this);
+	translationSnapSpinBox->setValue(transformGizmo.translateSnapValues[0]);
+	connect(translationSnapSpinBox, &SpinBox::editingFinished, this, &ToolbarDock::SetTranslationSnapValue);
+
+	hLayout->addWidget(new QLabel("Translation:"));
+	hLayout->addWidget(translationSnapSpinBox);
+
+	//Scale
+	scaleSnapSpinBox = new SpinBox(this);
+	scaleSnapSpinBox->setValue(transformGizmo.scaleSnapValues[0]);
+	connect(scaleSnapSpinBox, &SpinBox::editingFinished, this, &ToolbarDock::SetScaleSnapValue);
+
+	hLayout->addWidget(new QLabel("Scale:"));
+	hLayout->addWidget(scaleSnapSpinBox);
+
+	//Rotation
+	rotationSnapSpinBox = new SpinBox(this);
+	rotationSnapSpinBox->setValue(transformGizmo.rotationSnapValues[0]);
+	connect(rotationSnapSpinBox, &SpinBox::editingFinished, this, &ToolbarDock::SetRotationSnapValue);
+
+	hLayout->addWidget(new QLabel("Rotation:"));
+	hLayout->addWidget(rotationSnapSpinBox);
+
 
 	auto toolbarWidget = new QWidget();
 	toolbarWidget->setLayout(hLayout);
@@ -34,4 +66,28 @@ void ToolbarDock::StartPlay()
 	{
 		playButton->setText("Play");
 	}
+}
+
+void ToolbarDock::SetTranslationSnapValue()
+{
+	float value = (float)translationSnapSpinBox->value();
+	transformGizmo.translateSnapValues[0] = value;
+	transformGizmo.translateSnapValues[1] = value;
+	transformGizmo.translateSnapValues[2] = value;
+}
+
+void ToolbarDock::SetScaleSnapValue()
+{
+	float value = (float)scaleSnapSpinBox->value();
+	transformGizmo.scaleSnapValues[0] = value;
+	transformGizmo.scaleSnapValues[1] = value;
+	transformGizmo.scaleSnapValues[2] = value;
+}
+
+void ToolbarDock::SetRotationSnapValue()
+{
+	float value = (float)rotationSnapSpinBox->value();
+	transformGizmo.rotationSnapValues[0] = value;
+	transformGizmo.rotationSnapValues[1] = value;
+	transformGizmo.rotationSnapValues[2] = value;
 }
--- VEngine/Code/Editor/ToolbarDock.h
+++ VEngine/Code/Editor/ToolbarDock.h
@@ -2,12 +2,20 @@
 #include <qdockwidget.h>
 
 class QPushButton;
+struct SpinBox;
 
 struct ToolbarDock : public QDockWidget
 {
 	QPushButton* playButton;
 	QPushButton* pauseButton;
 
+	SpinBox* translationSnapSpinBox;
+	SpinBox* scaleSnapSpinBox;
+	SpinBox* rotationSnapSpinBox;
+
 	ToolbarDock();
 	void StartPlay();
+	void SetTranslationSnapValue();
+	void SetScaleSnapValue();
+	void SetRotationSnapValue();
 };
