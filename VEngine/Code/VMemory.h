f48449434c30927edbae2367b96928deb98f6f95
sfederic <furshazzle@gmail.com>
2021-10-16 13:25:11 +1100

    created VMemory.h (only contains a small todo and example)

---
+++ VEngine/Code/VMemory.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//TODO: need to come back here and make a custom allocator to be able to track mem usage and display
+//it in debug menus. Not sure how to go about logging the size of buffers belonging to third party libs
+//like d3d11 or xaudio2, but I'm sure there's a way.
+
+//namespace VMemory
+//{
+// void alloc();
+//}
