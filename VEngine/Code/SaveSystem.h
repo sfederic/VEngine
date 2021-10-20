ec856ba1b778954b8ae5c14fe2c265ef8440a996
sfederic <furshazzle@gmail.com>
2021-10-20 20:34:53 +1100

    created SaveSystem and System

---
+++ VEngine/Code/SaveSystem.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+struct SaveSystem
+{
+	std::string saveFileNumber = "save01";
+};
---
+++ VEngine/Code/System.h
@@ -0,0 +1,9 @@
+#pragma once
+//#include "SystemStates.h"
+//
+////TODO: see if systems that aren't component/actor based can inherit from a base class like this
+////so that debug tools can be built from it.
+//struct System
+//{
+//	SystemStates systemState = SystemStates::Unloaded;
+//};
