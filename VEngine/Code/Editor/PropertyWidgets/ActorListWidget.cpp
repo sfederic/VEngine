fa5b17b062671e3124c39c4b8dae3fddda908ec8
sfederic <furshazzle@gmail.com>
2021-10-21 23:11:33 +1100

    ActorListWidget changes to interface with the value vector better

--- VEngine/Code/Editor/PropertyWidgets/ActorListWidget.cpp
+++ VEngine/Code/Editor/PropertyWidgets/ActorListWidget.cpp
@@ -3,6 +3,8 @@
 #include <qlistwidget.h>
 #include <qpushbutton.h>
 #include <qlayout.h>
+#include "World.h"
+#include "Editor/Editor.h"
 
 ActorListWidget::ActorListWidget(Property value_)
 {
@@ -10,9 +12,10 @@ ActorListWidget::ActorListWidget(Property value_)
 
 	value = (std::vector<Actor*>*)value_.data;
 
-	actorNamelistWidget = new QListWidget(this);
-	actorNamelistWidget->setSelectionMode(QAbstractItemView::SelectionMode::MultiSelection);
-	SetValue();
+	actorNameListWidget = new QListWidget(this);
+	actorNameListWidget->setSelectionMode(QAbstractItemView::SelectionMode::MultiSelection);
+	connect(actorNameListWidget, &QListWidget::itemChanged, this, &ActorListWidget::SetValue);
+	ResetValue();
 
 	addButton = new QPushButton("+", this);
 	connect(addButton, &QPushButton::clicked, this, &ActorListWidget::AddItem);
@@ -20,36 +23,64 @@ ActorListWidget::ActorListWidget(Property value_)
 	connect(deleteButton, &QPushButton::clicked, this, &ActorListWidget::DeleteItems);
 
 	auto vLayout = new QVBoxLayout(this);
-	vLayout->addWidget(actorNamelistWidget);
+	vLayout->addWidget(actorNameListWidget);
 	vLayout->addWidget(addButton);
 	vLayout->addWidget(deleteButton);
 
 	setLayout(vLayout);
 }
 
-void ActorListWidget::SetValue()
+void ActorListWidget::SetValue(QListWidgetItem* item)
 {
-	for (auto actor : *value)
+	std::string actorName = item->text().toStdString();
+	Actor* foundActor = world.FindActorByName(actorName);
+	if (foundActor)
 	{
-		QString actorName = QString::fromStdString(actor->name);
-		auto item = new QListWidgetItem(actorName, actorNamelistWidget);
-		item->setFlags(item->flags() | Qt::ItemFlag::ItemIsEditable);
+		for (auto actor : *value)
+		{
+			if (actor == foundActor)
+			{
+				editor->Log("Actor already in list widget");
+				actorNameListWidget->removeItemWidget(item);
+				return;
+			}
+		}
+
+		value->push_back(foundActor);
 	}
 }
 
 void ActorListWidget::ResetValue()
 {
-	
+	actorNameListWidget->clear();
+
+	for (auto actor : *value)
+	{
+		QString actorName = QString::fromStdString(actor->name);
+		auto item = new QListWidgetItem(actorName, actorNameListWidget);
+		item->setFlags(item->flags() | Qt::ItemFlag::ItemIsEditable);
+	}
 }
 
 void ActorListWidget::AddItem()
 {
-	auto item = new QListWidgetItem("empty", actorNamelistWidget);
+	auto item = new QListWidgetItem("empty", actorNameListWidget);
 	item->setFlags(item->flags() | Qt::ItemFlag::ItemIsEditable);
 }
 
 void ActorListWidget::DeleteItems()
 {
-	auto items = actorNamelistWidget->selectedItems();
+	auto items = actorNameListWidget->selectedItems();
+	for (auto item : items)
+	{
+		for (int i = 0; i < value->size(); i++)
+		{
+			if (value->at(i)->name == item->text().toStdString())
+			{
+				value->erase(value->begin() + i);
+			}
+		}
+	}
+
 	qDeleteAll(items);
 }
--- VEngine/Code/Editor/PropertyWidgets/ActorListWidget.h
+++ VEngine/Code/Editor/PropertyWidgets/ActorListWidget.h
@@ -6,18 +6,19 @@
 struct Actor;
 class QListWidget;
 class QPushButton;
+class QListWidgetItem;
 
 //TODO: can probably make this sort of widget a template so it can work with all types.
 struct ActorListWidget : public QWidget, IPropertyWidget
 {
-	QListWidget* actorNamelistWidget;
+	QListWidget* actorNameListWidget;
 	QPushButton* addButton;
 	QPushButton* deleteButton;
 
 	std::vector<Actor*>* value;
 
 	ActorListWidget(Property value_);
-	void SetValue();
+	void SetValue(QListWidgetItem* item);
 	virtual void ResetValue() override;
 
 	void AddItem();
