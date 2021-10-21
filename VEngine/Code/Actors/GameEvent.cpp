5d0fb946f2fa0cf4737d8ced456d5490a57a44f6
sfederic <furshazzle@gmail.com>
2021-10-21 21:29:18 +1100

    created GameEvent (for time of day events in-game)

---
+++ VEngine/Code/Actors/GameEvent.cpp
@@ -0,0 +1,35 @@
+#include "GameEvent.h"
+#include "Components/EmptyComponent.h"
+#include "GameInstance.h"
+
+GameEvent::GameEvent()
+{
+}
+
+void GameEvent::Start()
+{
+	//if (GameInstance::currentMinute  startMinute && GameInstance::currentMinute <= endMinute)
+	{
+		if (GameInstance::currentHour < startHour && GameInstance::currentHour > endHour)
+		{
+			for (auto actor : actorsToActivate)
+			{
+				actor->active = false;
+			}
+		}
+	}
+}
+
+void GameEvent::Tick(float deltaTime)
+{
+}
+
+Properties GameEvent::GetProps()
+{
+	auto props = Actor::GetProps();
+	props.Add("Start Hour", &startHour);
+	props.Add("End Hour", &endHour);
+	props.Add("Start Minute", &startMinute);
+	props.Add("End Minute", &endMinute);
+	return props;
+}
---
+++ VEngine/Code/Actors/GameEvent.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Actor.h"
+#include "ActorSystem.h"
+#include <vector>
+
+struct EmptyComponent;
+struct Actor;
+
+//Game events are an in-world instance that map to a 'time of day' during gameplay,
+//linking actors to be activated in a game-time timeframe.
+struct GameEvent : Actor
+{
+	ACTOR_SYSTEM(GameEvent)
+
+	EmptyComponent* root = nullptr;
+
+	//Times to start and end event
+	int startHour = 0;
+	int endHour = 0;
+	int startMinute = 0;
+	int endMinute = 0;
+
+	std::vector<Actor*> actorsToActivate;
+
+	GameEvent();
+	virtual void Start() override;
+	virtual void Tick(float deltaTime) override;
+	virtual Properties GetProps() override;
+};
