819d575735dc3bd3e1b58eb702d9bfc334c27497
sfederic <furshazzle@gmail.com>
2021-10-23 22:48:07 +1100

    Commented out all of VMemory.h, kept for reference

--- VEngine/Code/VMemory.cpp
+++ VEngine/Code/VMemory.cpp
@@ -1,27 +1,27 @@
-#include "VMemory.h"
-
-uint64_t AllocSystem::GetAllocationSystemMemoryUsage()
-{
-	uint64_t total = 0;
-
-	for (auto memIt : memoryMap)
-	{
-		MemoryItem& item = memIt.second;
-		total += sizeof(MemoryItem);
-		total += item.allocFunction.size();
-		total += item.allocFile.size();
-	}
-
-	return total;
-}
-
-double AllocationPerType::GetTotalSizeInKB()
-{
-	return static_cast<double>(totalSize) / 1024.0;
-}
-
-double AllocationPerType::GetTotalSizeInMB()
-{
-	double kbSize = GetTotalSizeInKB();
-	return kbSize / 1024.0;
-}
+//#include "VMemory.h"
+//
+//uint64_t AllocSystem::GetAllocationSystemMemoryUsage()
+//{
+//	uint64_t total = 0;
+//
+//	for (auto memIt : memoryMap)
+//	{
+//		MemoryItem& item = memIt.second;
+//		total += sizeof(MemoryItem);
+//		total += item.allocFunction.size();
+//		total += item.allocFile.size();
+//	}
+//
+//	return total;
+//}
+//
+//double AllocationPerType::GetTotalSizeInKB()
+//{
+//	return static_cast<double>(totalSize) / 1024.0;
+//}
+//
+//double AllocationPerType::GetTotalSizeInMB()
+//{
+//	double kbSize = GetTotalSizeInKB();
+//	return kbSize / 1024.0;
+//}
