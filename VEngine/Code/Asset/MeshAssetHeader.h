5971b79c39435ce3a22136b2826b831277adc3fb
sfederic <furshazzle@gmail.com>
2021-10-30 10:57:50 +1100

    added SourceMeshFormat enum to MeshAssetHeader

--- VEngine/Code/Asset/MeshAssetHeader.h
+++ VEngine/Code/Asset/MeshAssetHeader.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <cstdint>
 
+enum class SourceMeshFormat : uint8_t
+{
+	FBX
+};
+
 //Binary header structure for mesh asset files
 struct MeshAssetHeader
 {
 	uint64_t vertexCount = 0;
 	uint64_t indexCount = 0;
+
+	SourceMeshFormat sourceMeshFormat;
 };
