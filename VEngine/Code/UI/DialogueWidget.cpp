5cef9ac2c592313cc2bb0eb4710a84dda1992be6
sfederic <furshazzle@gmail.com>
2021-11-27 12:36:12 +1100

    added UI to DialogueWidget::Tick

--- VEngine/Code/UI/DialogueWidget.cpp
+++ VEngine/Code/UI/DialogueWidget.cpp
@@ -2,5 +2,9 @@
 
 void DialogueWidget::Tick(float deltaTime)
 {
-	//MapToScreenSpace();
+	int sx, sy;
+	GetScreenSpaceCoords(sx, sy);
+
+	Image("speech_bubble.png", (float)sx - 50.f, (float)sy - 50.f, 200.f, 200.f);
+	Text(displayText, { (float)sx, (float)sy, (float)sx + 150.f, (float)sy + 150.f });
 }
