9c4f5c38028d28c6ffefda9fa97b5f9074b1f9e4
sfederic <furshazzle@gmail.com>
2021-11-27 20:02:45 +1100

    fixed up dialoguewidget layout

--- VEngine/Code/UI/DialogueWidget.cpp
+++ VEngine/Code/UI/DialogueWidget.cpp
@@ -2,9 +2,18 @@
 
 void DialogueWidget::Tick(float deltaTime)
 {
+	//TODO: this whole function looks fucking wonky.
+
 	int sx, sy;
 	GetScreenSpaceCoords(sx, sy);
 
-	Image("speech_bubble.png", (float)sx - 50.f, (float)sy - 50.f, 200.f, 200.f);
-	Text(displayText, { (float)sx, (float)sy, (float)sx + 150.f, (float)sy + 150.f });
+	sy -= 75.f;
+	D2D1_RECT_F textRect = CenterLayoutOnScreenSpaceCoords(100.f, 100.f, sx, sy);
+
+	D2D1_RECT_F imageRect = {
+		textRect.left - 50.f, textRect.top - 50.f, textRect.right + 50.f, textRect.bottom + 50.f 
+	};
+
+	Image("speech_bubble.png", imageRect);
+	Text(displayText, textRect);
 }
--- VEngine/Code/UI/DialogueWidget.h
+++ VEngine/Code/UI/DialogueWidget.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "Widget.h"
 
-//TODO: keep this widget around for now, NPCs are just using Widget for now, see if these is needed later.
-
 //Shows dialogue text for actors
 struct DialogueWidget : Widget
 {
