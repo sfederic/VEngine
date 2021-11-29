18266514290dce0b33f4562d28e5ef17ec2596cd
sfederic <furshazzle@gmail.com>
2021-11-30 00:06:52 +1100

    Added more world information to Intuition

--- VEngine/Code/Gameplay/Intuition.h
+++ VEngine/Code/Gameplay/Intuition.h
@@ -7,6 +7,15 @@ struct Intuition
 {
 	std::string name;
 	std::string description;
+	
+	//Debug menu information
+	std::string conditionFuncName;
+	std::string actorAquiredFrom;
+	std::string worldAquiredFrom;
+
+	//Time information
+	int hourAquired = 0;
+	int minuteAquired = 0;
 
 	//Function to call in order to add Intuition to Player.
 	std::function<bool()> conditionFunc;
