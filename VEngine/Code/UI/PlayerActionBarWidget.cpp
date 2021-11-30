d9e08c6600eb4091e1b2ccce70e61591ee7efbee
sfederic <furshazzle@gmail.com>
2021-11-30 21:51:44 +1100

    fixed PlayerActionBarWidget padding

--- VEngine/Code/UI/PlayerActionBarWidget.cpp
+++ VEngine/Code/UI/PlayerActionBarWidget.cpp
@@ -2,13 +2,18 @@
 
 void PlayerActionBarWidget::Tick(float deltaTime)
 {
-	D2D1_RECT_F rect = AlignLayout(150.f, 25.f, Align::Bottom);
+	D2D1_RECT_F rect = AlignLayout(150.f, 15.f, Align::Bottom);
 	D2D1_RECT_F iconRect = rect;
-	iconRect.right = rect.left;
+	iconRect.right = rect.left + 20.f;
 
 	for (int i = 0; i < *actionPoints; i++)
 	{
-		iconRect.right += 20.f;
-		Rect(iconRect);
+		iconRect.left += 20.f;
+ 		iconRect.right += 20.f;
+		FillRect(iconRect);
+
+		//Padding
+		iconRect.left += 5.f;
+		iconRect.right += 5.f;
 	}
 }
