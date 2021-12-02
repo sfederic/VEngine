2337b8784a6aa744fe1a2c8e687117f605241f76
sfederic <furshazzle@gmail.com>
2021-12-02 22:03:15 +1100

    changed rect colour in InteractWidget

--- VEngine/Code/UI/InteractWidget.cpp
+++ VEngine/Code/UI/InteractWidget.cpp
@@ -4,6 +4,6 @@ void InteractWidget::Tick(float deltaTime)
 {
 	auto rect = AlignLayout(200.f, 100.f, Align::Bottom);
 
-	Rect(rect);
+	FillRect(rect, {0.7f, 0.7f, 0.7f, 1.f}, 0.7f);
 	Text(interactText, rect);
 }
