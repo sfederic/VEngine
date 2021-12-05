3e7169f07e13a03a3e87c075953c2392c3f68310
sfederic <furshazzle@gmail.com>
2021-12-05 12:21:30 +1100

    Timeofdaywidget tick

--- VEngine/Code/UI/TimeOfDayWidget.cpp
+++ VEngine/Code/UI/TimeOfDayWidget.cpp
@@ -1,7 +1,10 @@
 #include "TimeOfDayWidget.h"
 #include "Gameplay/GameInstance.h"
+#include "VString.h"
 
 void TimeOfDayWidget::Tick(float deltaTime)
 {
-	
+	D2D1_RECT_F layout = AlignLayout(200.f, 200.f, Align::BottomLeft);
+	Text(VString::wformat(L"Hour: %d | Minute: %d",
+		GameInstance::currentHour, GameInstance::currentMinute), layout);
 }
