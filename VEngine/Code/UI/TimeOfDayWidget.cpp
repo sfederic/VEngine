2233039bb9208cc8c784bc96268f6a074a6654e8
sfederic <furshazzle@gmail.com>
2021-12-05 12:31:29 +1100

    changed TimeOfDayWidget alignment and colour

--- VEngine/Code/UI/TimeOfDayWidget.cpp
+++ VEngine/Code/UI/TimeOfDayWidget.cpp
@@ -4,7 +4,8 @@
 
 void TimeOfDayWidget::Tick(float deltaTime)
 {
-	D2D1_RECT_F layout = AlignLayout(200.f, 200.f, Align::BottomLeft);
+	D2D1_RECT_F layout = AlignLayout(200.f, 200.f, Align::TopLeft);
 	Text(VString::wformat(L"Hour: %d | Minute: %d",
-		GameInstance::currentHour, GameInstance::currentMinute), layout);
+		GameInstance::currentHour, GameInstance::currentMinute), layout, TextAlign::Center,
+		{1.f, 1.f, 1.f, 1.f});
 }
