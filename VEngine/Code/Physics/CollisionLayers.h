d78512dcf602dccb6fea78e41c6154cc7fe3f1b6
sfederic <furshazzle@gmail.com>
2021-12-07 21:35:00 +1100

    created bitwise operators for CollisionLayers enum

--- VEngine/Code/Physics/CollisionLayers.h
+++ VEngine/Code/Physics/CollisionLayers.h
@@ -1,9 +1,19 @@
 #pragma once
 
-enum class CollisionLayers
+enum class CollisionLayers : int
 {
-	Ignore,
-	Editor,
-	Game,
-	All
+	None = 0x00,
+	Editor = 0x01,
+	All = 0x02
 };
+
+//bro just define your own enum operators yeah sick
+inline CollisionLayers operator|(CollisionLayers a, CollisionLayers b)
+{
+	return static_cast<CollisionLayers>(static_cast<int>(a) | static_cast<int>(b));
+}
+
+inline CollisionLayers operator&(CollisionLayers a, CollisionLayers b)
+{
+	return static_cast<CollisionLayers>(static_cast<int>(a) & static_cast<int>(b));
+}
