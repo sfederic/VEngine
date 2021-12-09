93969c4a9809ddf0ee4f071f32d97ae7f4e0263a
sfederic <furshazzle@gmail.com>
2021-12-09 22:42:14 +1100

    changed starting values for hour/minute in GameInstance

--- VEngine/Code/Gameplay/GameInstance.h
+++ VEngine/Code/Gameplay/GameInstance.h
@@ -5,8 +5,8 @@
 //Instance holding data over the entirety of the game
 struct GameInstance
 {
-	inline static int currentHour;
-	inline static int currentMinute;
+	inline static int currentHour = 8;
+	inline static int currentMinute = 0;
 
 	inline static const std::string startingMap = "church_room1.vmap";
 	inline static std::string previousMapMovedFrom = startingMap;
