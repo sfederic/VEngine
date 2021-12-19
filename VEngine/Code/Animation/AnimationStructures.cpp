3c20e12e15eec7030a70fe226656865c3907cedb
sfederic <furshazzle@gmail.com>
2021-12-20 10:00:29 +1100

    changed lerping value names in joint interpolation

--- VEngine/Code/Animation/AnimationStructures.cpp
+++ VEngine/Code/Animation/AnimationStructures.cpp
@@ -3,26 +3,27 @@
 //@Todo: the args here feel shitty. Find a way to put in all in the joint.
 void Animation::Interpolate(float t, Joint& joint, Skeleton* skeleton)
 {
-	for (int i = 0; i < (frames.size() - 1); i++)
+	for (size_t i = 0; i < (frames.size() - 1); i++)
 	{
 		if (t >= frames[i].time && t <= frames[i + 1].time)
 		{
 			//Get the interpolation difference between input time and frame time.
 			float lerpPercent = (t - frames[i].time) / (frames[i + 1].time - frames[i].time);
 
-			XMVECTOR pos1 = XMLoadFloat3(&frames[i].pos);
-			XMVECTOR pos2 = XMLoadFloat3(&frames[i + 1].pos);
+			XMVECTOR currentPos = XMLoadFloat3(&frames[i].pos);
+			XMVECTOR nextPos = XMLoadFloat3(&frames[i + 1].pos);
 
-			XMVECTOR scale1 = XMLoadFloat3(&frames[i].scale);
-			XMVECTOR scale2 = XMLoadFloat3(&frames[i + 1].scale);
+			XMVECTOR currentScale = XMLoadFloat3(&frames[i].scale);
+			XMVECTOR nextScale = XMLoadFloat3(&frames[i + 1].scale);
 
-			XMVECTOR rot1 = XMLoadFloat4(&frames[i].rot);
-			XMVECTOR rot2 = XMLoadFloat4(&frames[i + 1].rot);
+			XMVECTOR currentRot = XMLoadFloat4(&frames[i].rot);
+			XMVECTOR nextRot = XMLoadFloat4(&frames[i + 1].rot);
 
-			XMVECTOR lerpedPos = XMVectorLerp(pos1, pos2, lerpPercent);
-			XMVECTOR lerpedScale = XMVectorLerp(scale1, scale2, lerpPercent);
-			XMVECTOR lerpedRot = XMQuaternionSlerp(rot1, rot2, lerpPercent);
+			XMVECTOR lerpedPos = XMVectorLerp(currentPos, nextPos, lerpPercent);
+			XMVECTOR lerpedScale = XMVectorLerp(currentScale, nextScale, lerpPercent);
+			XMVECTOR lerpedRot = XMQuaternionSlerp(currentRot, nextRot, lerpPercent);
 			XMVECTOR zero = XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f);
+
 			joint.currentPose = XMMatrixAffineTransformation(lerpedScale, zero, lerpedRot, lerpedPos);
 
 			int parentIndex = joint.parentIndex;
