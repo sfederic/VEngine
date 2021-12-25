081d3f2d1a26a405202e5895b78521244ad08b60
sfederic <furshazzle@gmail.com>
2021-12-25 22:22:18 +1100

    added @todo to ActorWidget about it not even working

--- VEngine/Code/Editor/PropertyWidgets/ActorWidget.h
+++ VEngine/Code/Editor/PropertyWidgets/ActorWidget.h
@@ -4,6 +4,9 @@
 
 struct Actor;
 
+//@Todo: actorwidget doesn't really work because it's trying to attribute an existing actor name to
+//a pointer, but actor names are deserialised at the same time actor pointers are, so it does nothing.
+
 //Is essentially a StringWidget that uses that finds the actor by its name.
 struct ActorWidget : QLineEdit, IPropertyWidget
 {
