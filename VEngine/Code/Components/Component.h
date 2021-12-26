567e52a75cb779ce6d8f7b28a5f22ab167c4024d
sfederic <furshazzle@gmail.com>
2021-12-26 20:37:51 +1100

    -added UID to component -added UID generation to ComponentSystem::Add

--- VEngine/Code/Components/Component.h
+++ VEngine/Code/Components/Component.h
@@ -8,10 +8,9 @@ struct Component
 {
 	//TODO: there's something wrong with owner here. It always ends up crashing with junk data.
 	Actor* owner = nullptr; 
-
+	UID uid;
 	std::string name;
 	int index = -1;
-
 	bool active = true;
 
 	virtual void Tick(float deltaTime) {}
