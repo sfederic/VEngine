06f86227cf0cb4cc10e34b1daecc7ea17281928e
sfederic <furshazzle@gmail.com>
2021-12-28 18:45:57 +1100

    added basic content to TitleScreenWidget

--- VEngine/Code/UI/TitleScreenWidget.cpp
+++ VEngine/Code/UI/TitleScreenWidget.cpp
@@ -1,6 +1,19 @@
 #include "TitleScreenWidget.h"
+#include "Gameplay/GameUtils.h"
+#include "Gameplay/GameInstance.h"
 
 void TitleScreenWidget::Tick(float deltaTime)
 {
-	Image("bush.png", 0, 0, 300, 350);
+	Text(L"Vagrant Tactics", AlignLayout(300.f, 100.f, Align::Center), TextAlign::Center, Colours::White);
+
+	if (Button(L"Begin", AlignLayout(100.f, 50.f, Align::BottomLeft), 1.f, TextAlign::Center, Colours::White))
+	{
+		GameUtils::LoadWorld("church_hall.vmap");
+	}
+
+	if (Button(L"Continue", AlignLayout(100.f, 50.f, Align::BottomRight), 1.f, TextAlign::Center, Colours::White))
+	{
+		GameInstance::useGameSaves = true;
+		//@Todo: load world to continue from 
+	}
 }
