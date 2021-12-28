304efe4e0cabc3c9e2710389c5bcf38971559dac
sfederic <furshazzle@gmail.com>
2021-12-28 11:21:08 +1100

    added workflow comment to DestructibleMeshComponent.h

--- VEngine/Code/Components/DestructibleMeshComponent.h
+++ VEngine/Code/Components/DestructibleMeshComponent.h
@@ -1,7 +1,14 @@
 #pragma once
 #include "MeshComponent.h"
 
+//Destructible mesh workflow:
+// 1. Blender -> Edit -> Preferences : turn on cell fracture
+// 2. F3 on the selected mesh, search 'Cell Fracture'
+// 3. fiddle around with the settings, make it random
+// 4. FBXLoader::ImportFracturedMesh() is only working with meshes that have no children in the fbx
 //Ref: https://80.lv/articles/creating-a-destructible-object-in-unreal-engine-4-blender/
+
+//@Todo: destructible meshes are "working", but not well. Maybe come back and fix it all up later.
 
 struct DestructibleMeshComponent : MeshComponent
 {
