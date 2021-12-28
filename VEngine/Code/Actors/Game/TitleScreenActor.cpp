ed400cda29316183c6204c4c40e51a75d5ffc762
sfederic <furshazzle@gmail.com>
2021-12-28 18:07:52 +1100

    created TitleScreenActor

---
+++ VEngine/Code/Actors/Game/TitleScreenActor.cpp
@@ -0,0 +1,19 @@
+#include "TitleScreenActor.h"
+#include "UI/TitlescreenWidget.h"
+#include "Components/EmptyComponent.h"
+
+TitleScreenActor::TitleScreenActor()
+{
+    rootComponent = EmptyComponent::system.Add();
+}
+
+void TitleScreenActor::Start()
+{
+    titlescreenWidget = CreateWidget<TitleScreenWidget>();
+    titlescreenWidget->AddToViewport();
+}
+
+Properties TitleScreenActor::GetProps()
+{
+    return Properties();
+}
