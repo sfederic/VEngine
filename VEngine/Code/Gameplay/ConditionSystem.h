9108e4c4a6d5b573dfba8ec3fde51b98c3bfca45
sfederic <furshazzle@gmail.com>
2021-12-30 18:09:20 +1100

    rewrote condition system functions comment

--- VEngine/Code/Gameplay/ConditionSystem.h
+++ VEngine/Code/Gameplay/ConditionSystem.h
@@ -4,9 +4,13 @@
 #include <string>
 #include <iostream>
 
-//Evaluates and contains all in-game event conditions.
+//CONDITION FUNCTIONS
 //Condition functions need to be created as free functions in Conditions.cpp and then added to the system.
-//through the ADD_CONDITION macro.
+//through the ADD_CONDITION macro in the ConditionSystem constructor.
+//When a condition returns true, that means the goto line in the dialogueline will be the next line.
+//If the condition just needs to be used as an event (play sound, add intuiton), return false.
+
+//Evaluates and contains all in-game event conditions.
 struct ConditionSystem
 {
 	std::map<std::string, std::function<bool(std::string)>> conditions;
