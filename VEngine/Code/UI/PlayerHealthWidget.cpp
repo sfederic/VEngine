6a681c2a61d0c30f4da6658a5134a0c9a6a0bd1a
sfederic <furshazzle@gmail.com>
2021-12-30 16:43:25 +1100

    playerhealthwiget image layout fix

--- VEngine/Code/UI/PlayerHealthWidget.cpp
+++ VEngine/Code/UI/PlayerHealthWidget.cpp
@@ -5,7 +5,7 @@ void PlayerHealthWidget::Draw()
 	Layout layout = AlignLayout(25.f, 25.f, Align::BottomRight);
 	for (int i = 0; i < healthPoints; i++)
 	{
-		layout.AddHorizontalSpace(50.f * i);
+		layout.AddHorizontalSpace(50.f);
 		Image("heart_icon.png", layout);
 	}
 }
