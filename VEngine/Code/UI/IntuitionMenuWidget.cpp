98cbaad38fddf8791a07034aeb0e8d1e6fe0c240
sfederic <furshazzle@gmail.com>
2021-12-31 13:01:55 +1100

    IntuitonMenuWidget seperating out intuiton details into two layouts

--- VEngine/Code/UI/IntuitionMenuWidget.cpp
+++ VEngine/Code/UI/IntuitionMenuWidget.cpp
@@ -6,21 +6,54 @@
 
 void IntuitionMenuWidget::Draw()
 {
-	Layout layout = PercentAlignLayout(0.1f, 0.1f, 0.9f, 0.9f);
-	FillRect(layout, {0.5f, 0.5f, 0.5f, 0.75f});
-
-	//Set text rect to begining of layoutrect, then increment in forloop
-	Layout textLayout = layout;
-	textLayout.PushToTop();
-
-	for (auto& intuitionPair : GameInstance::playerIntuitions)
+	//Intuiton select scroll menu
 	{
-		Intuition& intuition = intuitionPair.second;
+		Layout layout = PercentAlignLayout(0.1f, 0.1f, 0.4f, 0.9f);
+		FillRect(layout, { 0.5f, 0.5f, 0.5f, 0.75f });
 
-		textLayout.AddVerticalSpace(30.f);
-		Text(VString::stows(intuition.name), textLayout);
+		//Set text rect to begining of layoutrect, then increment in forloop
+		Layout textLayout = layout;
+		textLayout.PushToTop();
+		textLayout.rect.bottom += 30.f;
 
-		textLayout.AddVerticalSpace(30.f);
-		Text(VString::stows(intuition.description), textLayout);
+		Text(L"Intuitions", textLayout);
+
+		for (auto& intuitionPair : GameInstance::playerIntuitions)
+		{
+			Intuition& intuition = intuitionPair.second;
+
+			textLayout.AddVerticalSpace(30.f);
+			if (Button(VString::stows(intuition.name), textLayout))
+			{
+				selectedIntuition = &intuition;
+			}
+		}
+	}
+
+	//Selected intuition details
+	{
+		if (selectedIntuition)
+		{
+			Layout layout = PercentAlignLayout(0.5f, 0.1f, 0.9f, 0.9f);
+			FillRect(layout, { 0.5f, 0.5f, 0.5f, 0.75f });
+
+			Text(VString::wformat(L"Name: %S", selectedIntuition->name.c_str()), layout, TextAlign::Leading);
+			layout.AddVerticalSpace(30.f);
+
+			Text(VString::wformat(L"Desc: %S", selectedIntuition->description.c_str()), layout, TextAlign::Leading);
+			layout.AddVerticalSpace(90.f);
+
+			Text(VString::wformat(L"Actor GET: %S", selectedIntuition->actorAquiredFrom.c_str()), layout, TextAlign::Leading);
+			layout.AddVerticalSpace(30.f);
+
+			Text(VString::wformat(L"Hour GET: %d", selectedIntuition->hourAquired), layout, TextAlign::Leading);
+			layout.AddVerticalSpace(30.f);
+
+			Text(VString::wformat(L"Minute GET: %d", selectedIntuition->minuteAquired), layout, TextAlign::Leading);
+			layout.AddVerticalSpace(30.f);
+
+			Text(VString::wformat(L"World GET: %S", selectedIntuition->worldAquiredFrom.c_str()), layout, TextAlign::Leading);
+			layout.AddVerticalSpace(30.f);
+		}
 	}
 }
--- VEngine/Code/UI/IntuitionMenuWidget.h
+++ VEngine/Code/UI/IntuitionMenuWidget.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "Widget.h"
 
+struct Intuition;
+
 //Draws all current known intuitons the player has in a game setting.
 struct IntuitionMenuWidget : Widget
 {
+	Intuition* selectedIntuition = nullptr;
+
 	virtual void Draw() override;
 };
