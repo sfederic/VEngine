45a117e1021b696ae73bab04bb6c2d7937bafb87
sfederic <furshazzle@gmail.com>
2021-12-31 12:39:42 +1100

    check on intuiton menu toggle if weapon drawn

--- VEngine/Code/Actors/Game/Player.cpp
+++ VEngine/Code/Actors/Game/Player.cpp
@@ -351,6 +351,8 @@ void Player::PrimaryAction()
 
 void Player::ToggleIntuitionMenu()
 {
+	if (isWeaponDrawn) return;
+
 	if (Input::GetKeyUp(Keys::Enter))
 	{
 		intuitionWidgetToggle = !intuitionWidgetToggle;
