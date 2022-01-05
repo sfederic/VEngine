de72e2746a7a99b2621122c9bc337ba6db8445b6
sfederic <furshazzle@gmail.com>
2022-01-05 17:00:04 +1100

    GainIntuition() condition function

--- VEngine/Code/Gameplay/ConditionSystem.cpp
+++ VEngine/Code/Gameplay/ConditionSystem.cpp
@@ -5,6 +5,10 @@
 #include "UI/UISystem.h"
 #include "UI/IntuitionRecalledWidget.h"
 #include "Gameplay/BattleSystem.h"
+#include "World.h"
+#include "Log.h"
+#include "UI/UISystem.h"
+#include "UI/IntuitionGainedWidget.h"
 
 ConditionSystem conditionSystem;
 
@@ -33,12 +37,41 @@ bool StartBattle(std::string arg)
 	return true;
 }
 
+//Define the intuition as "Name|Description" in the dialogue dock
+bool GainIntuition(std::string arg)
+{
+	std::string intuitionName = arg.substr(0, arg.find("|"));
+	std::string intuitionDesc = arg.substr(arg.find("|") + 1);
+
+	auto intuition = Intuition();
+	intuition.name = intuitionName;
+	intuition.description = intuitionDesc;
+
+	intuition.actorAquiredFrom = "Aquired from dialogue.";
+	intuition.worldAquiredFrom = world.worldFilename;
+
+	intuition.hourAquired = GameInstance::currentHour;
+	intuition.minuteAquired = GameInstance::currentMinute;
+
+	GameInstance::playerIntuitions.emplace(intuition.name, intuition);
+	Log("%s Intuition created.", intuition.name.c_str());
+
+	uiSystem.intuitionGainedWidget->intuitionToDisplay = &GameInstance::playerIntuitions[intuition.name];
+	uiSystem.intuitionWidgetInViewport = true;
+	uiSystem.intuitionGainedWidget->AddToViewport();
+
+	GameUtils::PlayAudioOneShot("purchase.wav");
+
+	return true;
+}
+
 //END CONDITION FUNCTIONS
 
 ConditionSystem::ConditionSystem()
 {
 	ADD_CONDITION(IntuitionCheck);
 	ADD_CONDITION(StartBattle);
+	ADD_CONDITION(GainIntuition);
 }
 
 void ConditionSystem::AddCondition(std::string functionName, ConditionFunction conditionFunction)
