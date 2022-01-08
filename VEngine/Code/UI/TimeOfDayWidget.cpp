d91aa26e1551c49361cda7bf221d5667c03e8750
sfederic <furshazzle@gmail.com>
2022-01-08 11:53:34 +1100

    Gave TimeOfDataWidget a rect to actually be able to see it

--- VEngine/Code/UI/TimeOfDayWidget.cpp
+++ VEngine/Code/UI/TimeOfDayWidget.cpp
@@ -4,7 +4,8 @@
 
 void TimeOfDayWidget::Draw(float deltaTime)
 {
-	Layout layout = PercentAlignLayout(0.f, 0.f, 0.25f, 0.25f);
+	Layout layout = PercentAlignLayout(0.f, 0.f, 0.2f, 0.1f);
+	FillRect(layout);
 	Text(VString::wformat(L"Hour: %d | Minute: %d",
 		GameInstance::currentHour, GameInstance::currentMinute), layout, TextAlign::Center,
 		{1.f, 1.f, 1.f, 1.f});
