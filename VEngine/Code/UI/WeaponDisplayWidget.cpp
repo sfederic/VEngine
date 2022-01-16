3b37f7dd3ba8b308e17d6193b55ce7d6dd49f391
sfederic <furshazzle@gmail.com>
2022-01-17 00:59:26 +1100

    Changed WeaponDisplayLayout to show test data

--- VEngine/Code/UI/WeaponDisplayWidget.cpp
+++ VEngine/Code/UI/WeaponDisplayWidget.cpp
@@ -1,17 +1,33 @@
 #include "WeaponDisplayWidget.h"
+#include "Gameplay/GameUtils.h"
+#include "Actors/Game/Player.h"
+#include "VString.h"
 
 void WeaponDisplayWidget::Draw(float deltaTime)
 {
-	FillRect(PercentAlignLayout(0.1f, 0.8f, 0.5f, 0.9f));
+	auto layout = PercentAlignLayout(0.1f, 0.7f, 0.3f, 0.9f);
 
-	auto layout = PercentAlignLayout(0.1f, 0.8f, 0.2f, 0.9f);
+	FillRect(layout);
 
-	Button(L"Spear", layout);
-	layout.AddHorizontalSpace(120.f);
+	//Button(L"Spear", layout);
+	//layout.AddHorizontalSpace(120.f);
 
-	Button(L"Sword", layout);
-	layout.AddHorizontalSpace(120.f);
+	//Button(L"Sword", layout);
+	//layout.AddHorizontalSpace(120.f);
 
-	Button(L"Shield", layout);
-	layout.AddHorizontalSpace(120.f);
+	//Button(L"Shield", layout);
+	//layout.AddHorizontalSpace(120.f);
+
+	auto player = GameUtils::GetPlayer();
+	auto aw = player->activeWeapon;
+	if (aw)
+	{
+		Text(VString::wformat(L"ATK: %d", aw->attackPoints), layout);
+		layout.AddVerticalSpace(30.f);
+
+		Text(VString::wformat(L"ENRG: %d", aw->energyCost), layout);
+		layout.AddVerticalSpace(30.f);
+
+		Text(VString::wformat(L"WGHT: %d", aw->weight), layout);
+	}
 }
