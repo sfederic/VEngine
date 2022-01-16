65789941325db7f7cc54e6e7eb3f9a03c32d300a
sfederic <furshazzle@gmail.com>
2022-01-16 22:28:21 +1100

    BoxTrigger and InteractWidget logic for MemoryWeapon

--- VEngine/Code/Actors/Game/MemoryWeapon.cpp
+++ VEngine/Code/Actors/Game/MemoryWeapon.cpp
@@ -1,22 +1,55 @@
 #include "MemoryWeapon.h"
+#include "Components/BoxTriggerComponent.h"
+#include "Components/MemoryComponent.h"
+#include "UI/InteractWidget.h"
+#include "Input.h"
 
 MemoryWeapon::MemoryWeapon()
 {
+	memoryTrigger = BoxTriggerComponent::system.Add(this);
+	rootComponent->AddChild(memoryTrigger);
 }
 
 void MemoryWeapon::Start()
 {
 	__super::Start();
+
+	memoryTrigger->SetTargetAsPlayer();
+
+	interactWidget = CreateWidget<InteractWidget>();
+	interactWidget->interactText = L"Gain weapon from memory";
 }
 
 void MemoryWeapon::Tick(float deltaTime)
 {
 	__super::Tick(deltaTime);
+
+	if (memoryTrigger->ContainsTarget()) 
+	{
+		interactWidget->AddToViewport();
+
+		if (Input::GetKeyUp(Keys::Down) && !isBeingInteractedWith) 
+		{
+			isBeingInteractedWith = true;
+			memoryComponent->CreateMemory(this->name);
+
+			interactWidget->RemoveFromViewport();
+		}
+		else if (isBeingInteractedWith)
+		{
+			isBeingInteractedWith = false;
+		}
+	} 
+	else 
+	{
+		interactWidget->RemoveFromViewport();
+	}
 }
 
 Properties MemoryWeapon::GetProps()
 {
 	auto props = __super::GetProps();
+	props.title = "MemoryWeapon";
 	props.Add("WeaponEnergy", &weaponData.energyCost);
 	props.Add("WeaponAttack", &weaponData.attackPoints);
 	props.Add("WeaponDefend", &weaponData.defendPoints);
--- VEngine/Code/Actors/Game/MemoryWeapon.h
+++ VEngine/Code/Actors/Game/MemoryWeapon.h
@@ -2,14 +2,25 @@
 #include "GridActor.h"
 #include "Gameplay/WeaponData.h"
 
+struct BoxTriggerComponent;
+struct InteractWidget;
+
 //A GridActor that needs to have weapon/item stats so that InteractTriggers
 //can spawn the right data for Memories.
 struct MemoryWeapon : GridActor
 {
 	ACTOR_SYSTEM(MemoryWeapon);
 
+	BoxTriggerComponent* memoryTrigger = nullptr;
+
+	InteractWidget* interactWidget = nullptr;
+
 	WeaponData weaponData;
 
+private:
+	bool isBeingInteractedWith = false;
+
+public:
 	MemoryWeapon();
 	virtual void Start() override;
 	virtual void Tick(float deltaTime) override;
