fc2195bebeccb2a10671b4e98a9ecfd9066de632
sfederic <furshazzle@gmail.com>
2022-01-17 23:27:12 +1100

    Commented out WeaponDisplayWidget stuff because if crashes ALL DAY

--- VEngine/Code/UI/WeaponDisplayWidget.cpp
+++ VEngine/Code/UI/WeaponDisplayWidget.cpp
@@ -22,15 +22,15 @@ void WeaponDisplayWidget::Draw(float deltaTime)
 	auto aw = player->activeWeapon;
 	if (aw)
 	{
-		Text(VString::wformat(L"ATK: %S", aw->name.c_str()), layout);
-		layout.AddVerticalSpace(30.f);
+		//Text(VString::wformat(L"ATK: %S", aw->name.c_str()), layout);
+		//layout.AddVerticalSpace(30.f);
 
-		Text(VString::wformat(L"ATK: %d", aw->attackPoints), layout);
-		layout.AddVerticalSpace(30.f);
+		//Text(VString::wformat(L"ATK: %d", aw->attackPoints), layout);
+		//layout.AddVerticalSpace(30.f);
 
-		Text(VString::wformat(L"ENRG: %d", aw->energyCost), layout);
-		layout.AddVerticalSpace(30.f);
+		//Text(VString::wformat(L"ENRG: %d", aw->energyCost), layout);
+		//layout.AddVerticalSpace(30.f);
 
-		Text(VString::wformat(L"WGHT: %d", aw->weight), layout);
+		//Text(VString::wformat(L"WGHT: %d", aw->weight), layout);
 	}
 }
