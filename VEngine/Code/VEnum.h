0a2defdd3d3e45749bfc505caa9a7d04650da082
sfederic <furshazzle@gmail.com>
2022-01-23 01:29:17 +1100

    Updated VEnum header comment

--- VEngine/Code/VEnum.h
+++ VEngine/Code/VEnum.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <unordered_map>
 
-//Because of how rough serialisation and editor UI widgets are with C++'s enums without reflection,
-//this struct acts as a makeshift enum to help with that. It also acts as a base class, meaning
-//you can inherit from it and define the enum's values as static strings in the struct.
+//Serialisation and editor UI widgets are with C++'s enums without reflection is rough,
+//this struct acts as a makeshift enum. It also acts as a base class, meaning
+//you can inherit from it and define the enum's values as static strings in the struct, or just define
+//static string values in a seperate struct and pass them into VEnum given that inheritance
+//isn't supported in the engine's serialisation.
 struct VEnum
 {
 private:
