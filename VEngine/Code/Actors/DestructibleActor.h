31abd0ffba5e9677ce3c974e766db5defd5cb0bd
sfederic <furshazzle@gmail.com>
2022-01-23 23:14:59 +1100

    Added todo to DestructibleActor header

--- VEngine/Code/Actors/DestructibleActor.h
+++ VEngine/Code/Actors/DestructibleActor.h
@@ -4,6 +4,9 @@
 
 struct DestructibleMeshComponent;
 
+//@Todo: because this the destructible component uses convex bounds, you can't
+//click on this actor in the editor, or delete it. To fix this, you'd have to
+//move raycasts and collision completely to PhysX.
 struct DestructibleActor : Actor
 {
 	ACTOR_SYSTEM(DestructibleActor);
