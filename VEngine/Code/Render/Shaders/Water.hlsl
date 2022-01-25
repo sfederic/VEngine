5b813db21f0ad898a93544453765466a1767762b
sfederic <furshazzle@gmail.com>
2022-01-26 01:12:27 +1100

    Create Water.hlsl
    Simple wave formula

---
+++ VEngine/Code/Render/Shaders/Water.hlsl
@@ -0,0 +1,25 @@
+#include "Include/CommonTypes.hlsli"
+#include "Include/TransformOut.hlsli"
+
+VS_OUT VSMain(VS_IN i) 
+{
+	VS_OUT o;
+
+	float4 newUv = mul(texMatrix, float4(i.uv, 0.f, 1.0f));
+	o.uv = float2(newUv.x, newUv.y);
+	o.normal = mul((float3x3)model, i.normal);
+	o.posWS = mul(model, float4(i.pos, 1.0f));
+	o.shadowPos = mul(lightMVP, o.posWS);
+	o.instanceID = i.instanceID;
+
+	i.pos.y = sin((newUv.x + newUv.y) * timeSinceStartup) * 0.1f;
+
+	o.pos = mul(mvp, float4(i.pos, 1.0f));
+
+	return o;
+}
+
+float4 PSMain(VS_OUT i) : SV_Target 
+{
+	return t.Sample(s, i.uv);
+}
