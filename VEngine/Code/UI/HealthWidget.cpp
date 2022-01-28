543883b499beb47264a01270e8a6e764db78763f
sfederic <furshazzle@gmail.com>
2022-01-29 00:18:40 +1100

    Small healthwidget latout change

--- VEngine/Code/UI/HealthWidget.cpp
+++ VEngine/Code/UI/HealthWidget.cpp
@@ -6,7 +6,7 @@ void HealthWidget::Draw(float deltaTime)
 	int sx, sy;
 	GetScreenSpaceCoords(sx, sy);
 
-	Layout layout = CenterLayoutOnScreenSpaceCoords(25.f, 25.f, (float)sx, (float)sy);
+	Layout layout = CenterLayoutOnScreenSpaceCoords(50.f, 25.f, (float)sx, (float)sy);
 
 	FillRect(layout);
 	Text(VString::wformat(L"%d/%d", healthPoints, maxHealthPoints), layout);
