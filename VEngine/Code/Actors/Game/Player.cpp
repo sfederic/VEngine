30f902f25ca20707917728635c9ccfbd2c077c77
sfederic <furshazzle@gmail.com>
2022-01-31 00:00:43 +1100

    Moved player camera back a bit

--- VEngine/Code/Actors/Game/Player.cpp
+++ VEngine/Code/Actors/Game/Player.cpp
@@ -42,7 +42,7 @@ Player::Player()
 	mesh = MeshComponent::system.Add(this, MeshComponent("character_test.fbx", "test.png"));
 	rootComponent->AddChild(mesh);
 
-	camera = CameraComponent::system.Add(this, CameraComponent(XMFLOAT3(1.5f, 1.5f, -2.5f), false));
+	camera = CameraComponent::system.Add(this, CameraComponent(XMFLOAT3(2.f, 2.f, -3.f), false));
 
 	rootComponent->AddChild(camera);
 
