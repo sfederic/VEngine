6a5544ac8d642399b193228c106355a1a408006a
sfederic <furshazzle@gmail.com>
2022-01-31 23:52:09 +1100

    Created QuestSystem

--- VEngine/Code/Quests/QuestSystem.cpp
+++ VEngine/Code/Quests/QuestSystem.cpp
@@ -1,1 +1,38 @@
 #include "QuestSystem.h"
+#include <map>
+#include <string>
+#include "Quest.h"
+#include "Gameplay/GameInstance.h"
+
+std::map<std::string, Quest*>* quests = nullptr;
+
+void AddQuest(const char* questName, Quest* quest)
+{
+	if (quests == nullptr)
+	{
+		quests = new std::map<std::string, Quest*>();
+	}
+
+	quests->emplace(questName, quest);
+}
+
+Quest* FindQuest(const char* questName)
+{
+	return quests->find(questName)->second;
+}
+
+void ExecuteQuestsForCurrentHour()
+{
+	int currentHour = GameInstance::currentHour;
+
+	for (auto& questPair : *quests)
+	{
+		Quest* quest = questPair.second;
+
+		switch (currentHour)
+		{
+		case 0:
+			quest->Hour0();
+		}
+	}
+}
--- VEngine/Code/Quests/QuestSystem.h
+++ VEngine/Code/Quests/QuestSystem.h
@@ -1,6 +1,13 @@
 #pragma once
-#include <map>
 
-struct QuestSystem
-{
-};
+//The typical "System" structure looks very C-ish here, it's just to keep compiles down to a minimum.
+//Not sure how many quests there might be at the end of it all.
+
+//Quests only need to be defined in .cpp files, they don't need headers. This is to get around static
+//include bullshit.
+
+struct Quest;
+
+void AddQuest(const char* questName, Quest* quest);
+Quest* FindQuest(const char* questName);
+void ExecuteQuestsForCurrentHour();
