ecff7415f161d3e86662a9fe8df80f38d1c1f940
sfederic <furshazzle@gmail.com>
2022-01-31 23:51:39 +1100

    Quest virtual function returns

--- VEngine/Code/Quests/Quest.h
+++ VEngine/Code/Quests/Quest.h
@@ -3,8 +3,8 @@
 //Quest data that quests inherit from and define their events via the HourN() calls.
 struct Quest
 {
-	virtual bool Hour0() {}
-	virtual bool Hour1() {}
-	virtual bool Hour2() {}
-	virtual bool Hour3() {}
+	virtual bool Hour0() { return true; }
+	virtual bool Hour1() { return true; }
+	virtual bool Hour2() { return true; }
+	virtual bool Hour3() { return true; }
 };
