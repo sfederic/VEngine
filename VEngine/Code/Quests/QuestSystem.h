2dc42e33711f778335c52bab8400a1465755d10e
sfederic <furshazzle@gmail.com>
2022-01-31 23:56:59 +1100

    QuestSystem todo

--- VEngine/Code/Quests/QuestSystem.h
+++ VEngine/Code/Quests/QuestSystem.h
@@ -6,6 +6,10 @@
 //Quests only need to be defined in .cpp files, they don't need headers. This is to get around static
 //include bullshit.
 
+//@Todo: it might be better to make a QuestComponent and call the quest based on the name input per actor
+//instead of what this system is doing now, where it's iterating over every single quest in the game
+//and activating their effects based on the current in-game hour
+
 struct Quest;
 
 void AddQuest(const char* questName, Quest* quest);
