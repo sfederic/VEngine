26b917f44c3aedebda9bde3bd809a2d5fc57acbd
sfederic <furshazzle@gmail.com>
2022-02-07 21:58:20 +1100

    ParticleSystem todo about TransformGizmo

--- VEngine/Code/Particle/ParticleSystem.h
+++ VEngine/Code/Particle/ParticleSystem.h
@@ -4,6 +4,9 @@
 
 struct ParticleEmitter;
 
+//@Todo: there's something wrong with the rootcomponent transform in ParticleSystem. ImGuizmo isn't showing
+//the transform gizmo at the actor's position in world, defaults to [0, 0, 0]
+
 struct ParticleSystem : Actor
 {
 	ACTOR_SYSTEM(ParticleSystem)
