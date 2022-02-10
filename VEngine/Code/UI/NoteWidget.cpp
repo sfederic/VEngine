67d4f0fc430b3dd9cb0e116b28057f54639aa494
sfederic <furshazzle@gmail.com>
2022-02-10 23:40:56 +1100

    Added draw() code to NoteWidget

--- VEngine/Code/UI/NoteWidget.cpp
+++ VEngine/Code/UI/NoteWidget.cpp
@@ -2,4 +2,11 @@
 
 void NoteWidget::Draw(float deltaTime)
 {
+	int sx, sy;
+	GetScreenSpaceCoords(sx, sy);
+
+	Layout layout = CenterLayoutOnScreenSpaceCoords(100.f, 100.f, sx, sy);
+
+	FillRect(layout);
+	Text(noteText, layout);
 }
--- VEngine/Code/UI/NoteWidget.h
+++ VEngine/Code/UI/NoteWidget.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "Widget.h"
 
-//Note that is meant to be a debug-esque thing viewed from the editor,
-//rendered in-world for development purposes. Got the idea from Breath of the Wild's engine from that GDC talk.
 struct NoteWidget : Widget
 {
+	std::wstring noteText;
+
 	virtual void Draw(float deltaTime) override;
 };
