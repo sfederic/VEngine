560e7cf031591384ec7a7ab596a6b504b8ee29ef
sfederic <furshazzle@gmail.com>
2022-02-10 23:50:21 +1100

    NoteWidget header comment

--- VEngine/Code/UI/NoteWidget.h
+++ VEngine/Code/UI/NoteWidget.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Widget.h"
 
+//Displays a note in world position. Mainly used for debugging notes in levels, not for actual gameplay.
 struct NoteWidget : Widget
 {
 	std::wstring noteText;
