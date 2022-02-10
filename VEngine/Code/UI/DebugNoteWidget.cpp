d21c26e90390da284b5c9c9f06824034b0a0e0a1
sfederic <furshazzle@gmail.com>
2022-02-11 00:00:30 +1100

    Changed DebugNoteWidget layout

--- VEngine/Code/UI/DebugNoteWidget.cpp
+++ VEngine/Code/UI/DebugNoteWidget.cpp
@@ -5,7 +5,7 @@ void DebugNoteWidget::Draw(float deltaTime)
 	int sx, sy;
 	GetScreenSpaceCoords(sx, sy);
 
-	Layout layout = CenterLayoutOnScreenSpaceCoords(100.f, 100.f, sx, sy);
+	Layout layout = CenterLayoutOnScreenSpaceCoords(175.f, 75.f, sx, sy);
 
 	FillRect(layout);
 	Text(noteText, layout);
