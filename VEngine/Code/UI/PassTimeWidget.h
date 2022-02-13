6569bdd8d876857f42987a17e66ddffd4f98184b
sfederic <furshazzle@gmail.com>
2022-02-13 12:58:53 +1100

    Updated PassTimeWidget header comment

--- VEngine/Code/UI/PassTimeWidget.h
+++ VEngine/Code/UI/PassTimeWidget.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "Widget.h"
 
-//Let's player change time of day via UI input
+//Let's player change time of day via UI input. Only hours are allowed to be changed, minute
+//input was too difficult to do, and GridActors only change based on the hour anyway.
 struct PassTimeWidget : Widget
 {
 	int hoursToPass = 0;
