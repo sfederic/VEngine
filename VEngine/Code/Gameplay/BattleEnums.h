5c68826576f148ca14b06b0d873424ea1374f141
sfederic <furshazzle@gmail.com>
2022-02-16 22:27:15 +1100

    Created BattleEnums.h
    Contains attack and defend directions

---
+++ VEngine/Code/Gameplay/BattleEnums.h
@@ -0,0 +1,21 @@
+#pragma once
+
+//Use 'count' as a max to determine random attack/defend directions
+
+enum class AttackDirection
+{
+	Up,
+	Down,
+	Left,
+	Right,
+	Count
+};
+
+enum class DefendDirection
+{
+	Up,
+	Down,
+	Left,
+	Right,
+	Count
+};
