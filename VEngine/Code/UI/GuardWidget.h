6a35fb65e2418d9c73c161a8b17a4d699467008a
sfederic <furshazzle@gmail.com>
2022-02-16 23:07:18 +1100

    GuardWidget enum private and enum inline setters

--- VEngine/Code/UI/GuardWidget.h
+++ VEngine/Code/UI/GuardWidget.h
@@ -4,16 +4,17 @@
 //Shows guard information when enemies are attacking player
 struct GuardWidget : Widget
 {
+private:
 	enum class GuardState
 	{
 		None,
 		Success,
 		Fail
-	};
+	} guardState;
 
-	GuardState guardState;
-
-	bool guardSuccessful = false;
-
+public:
 	virtual void Draw(float deltaTime) override;
+	inline void SetGuardSuccess() { guardState = GuardState::Success; }
+	inline void SetGuardFail() { guardState = GuardState::Fail; }
+	inline void ResetGuard() { guardState = GuardState::None; }
 };
