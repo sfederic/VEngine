8c4830d40abe6e43c8922f36b3dfe3d7c80600f0
sfederic <furshazzle@gmail.com>
2022-03-05 14:01:31 +1100

    SpriteSheetEmitter cleanup and fixes

--- VEngine/Code/Particle/SpriteSheetEmitter.cpp
+++ VEngine/Code/Particle/SpriteSheetEmitter.cpp
@@ -1,23 +1,74 @@
 #include "SpriteSheetEmitter.h"
+#include "Render/SpriteSystem.h"
+#include "Render/TextureSystem.h"
+#include "Render/PipelineObjects.h"
 
 SpriteSheetEmitter::SpriteSheetEmitter()
 {
 }
 
+void SpriteSheetEmitter::Create()
+{
+	sprite.useSourceRect = true;
+	sprite.textureFilename = textureData.filename;
+	
+	Texture2D* texture = textureSystem.FindTexture2D(textureData.filename);
+
+	int w = texture->desc.Width / numSheetColumns;
+	int h = texture->desc.Height / numSheetRows;
+
+	int x = w * currentSheetColumn;
+	int y = h * currentSheetRow;
+
+	sprite.srcRect = { x, y, x + w, y + h }; //Only src rect matters for sprite sheets
+
+	spriteSystem.CreateSpriteSheet(sprite);
+}
+
 void SpriteSheetEmitter::Tick(float deltaTime)
 {
-	//__super::Tick(deltaTime);
+	__super::Tick(deltaTime);
 
+	animationTimer += deltaTime * animationSpeed;
+	if (animationTimer > 1.0f)
+	{
+		animationTimer = 0.f;
 
+		currentSheetColumn++;
+
+		if (currentSheetColumn >= numSheetColumns)
+		{
+			currentSheetColumn = 0;
+			currentSheetRow++;
+		}
+
+		//End of animation
+		if (currentSheetRow >= numSheetRows)
+		{
+			currentSheetRow = 0;
+			currentSheetColumn = 0;
+		}
+	}
 }
 
 Properties SpriteSheetEmitter::GetProps()
 {
 	auto props = __super::GetProps();
-	props.AddProp(sheetRows);
-	props.AddProp(sheetColumns);
+	props.AddProp(numSheetRows);
+	props.AddProp(numSheetColumns);
 	props.AddProp(animationSpeed);
-	props.AddProp(spriteWidth);
-	props.AddProp(spriteHeight);
 	return props;
+}
+
+void SpriteSheetEmitter::UpdateSprite()
+{
+	Texture2D* texture = textureSystem.FindTexture2D(textureData.filename);
+
+	int w = texture->desc.Width / numSheetColumns;
+	int h = texture->desc.Height / numSheetRows;
+
+	int x = w * currentSheetColumn;
+	int y = h * currentSheetRow;
+
+	sprite.srcRect = { x, y, x + w, y + h };
 }
--- VEngine/Code/Particle/SpriteSheetEmitter.h
+++ VEngine/Code/Particle/SpriteSheetEmitter.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "ParticleEmitter.h"
+#include "Render/SpriteSystem.h"
 
 struct SpriteSheetEmitter : ParticleEmitter
 {
 	COMPONENT_SYSTEM(SpriteSheetEmitter);
 
-	int sheetRows = 0;
-	int sheetColumns = 0;
+	Sprite sprite;
+
+	int numSheetRows = 0;
+	int numSheetColumns = 0;
 
 	float animationSpeed = 1.f;
 
@@ -14,12 +17,15 @@ private:
 	float animationTimer = 0.f;
 
 public:
-	int spriteWidth = 0;
-	int spriteHeight = 0;
+	int currentSheetRow = 0;
+	int currentSheetColumn = 0;
 
 	bool loopAnimation = false;
 
 	SpriteSheetEmitter();
 	virtual void Tick(float deltaTime) override;
+	virtual void Create() override;
 	virtual Properties GetProps() override;
+
+	void UpdateSprite();
 };
