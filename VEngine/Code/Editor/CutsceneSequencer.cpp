b90908ed8615b948bc64d45174f3726913469283
sfederic <furshazzle@gmail.com>
2022-04-03 21:53:42 +1000

    Moved CutsceneSequencer into its own files

---
+++ VEngine/Code/Editor/CutsceneSequencer.cpp
@@ -0,0 +1,60 @@
+#include "CutsceneSequencer.h"
+#include "imgui/imgui.h"
+
+void CutsceneSequencer::Tick()
+{
+	if (ImGui::CollapsingHeader("Sequencer"))
+	{
+		ImGui::PushItemWidth(130);
+		ImGui::InputInt("Frame ", &currentFrame);
+		ImGui::SameLine();
+		ImGui::InputInt("Frame Min", &frameMin);
+		ImGui::SameLine();
+		ImGui::InputInt("Frame Max", &frameMax);
+		ImGui::PopItemWidth();
+		ImSequencer::Sequencer(this, &currentFrame, &expanded, &selectedEntry, &firstFrame,
+			ImSequencer::SEQUENCER_EDIT_STARTEND | ImSequencer::SEQUENCER_ADD | ImSequencer::SEQUENCER_DEL | ImSequencer::SEQUENCER_COPYPASTE | ImSequencer::SEQUENCER_CHANGE_FRAME);
+		if (selectedEntry != -1)
+		{
+			const CutsceneSequenceItem& item = items[selectedEntry];
+			//...
+		}
+	}
+}
+
+void CutsceneSequencer::Get(int index, int** start, int** end, int* type, unsigned int* color)
+{
+	CutsceneSequenceItem& item = items[index];
+	if (color) *color = 0xFFAA8080;
+	if (start) *start = &item.frameStart;
+	if (end) *end = &item.frameEnd;
+	if (type) *type = item.type;
+}
+
+void CutsceneSequencer::Add(int type)
+{
+	//testing
+	CutsceneSequenceItem item;
+	item.expanded = false;
+	item.frameStart = 0;
+	item.frameEnd = 25;
+	item.type = 0;
+	item.name = "test" + std::to_string(type);
+	items.push_back(item);
+}
+
+void CutsceneSequencer::DoubleClick(int index)
+{
+	if (items[index].expanded)
+	{
+		items[index].expanded = false;
+		return;
+	}
+
+	for (auto& item : items)
+	{
+		item.expanded = false;
+	}
+
+	items[index].expanded = !items[index].expanded;
+}
---
+++ VEngine/Code/Editor/CutsceneSequencer.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "ImGuizmo/ImSequencer.h"
+
+struct CutsceneSequenceItem
+{
+	std::string name;
+	int type;
+	int frameStart, frameEnd;
+	bool expanded;
+};
+
+//Editor using ImGuizmo for timelining cutscene events
+struct CutsceneSequencer : ImSequencer::SequenceInterface
+{
+	bool focused = false;
+	int frameMin = 0;
+	int frameMax = 250;
+
+	int selectedEntry = -1;
+	int firstFrame = 0;
+	bool expanded = true;
+	int currentFrame = 0;
+
+	std::vector<CutsceneSequenceItem> items;
+
+	void Tick();
+
+	virtual int GetFrameMin() const { return frameMin; }
+	virtual int GetFrameMax() const { return frameMax; };
+	virtual int GetItemCount() const { return items.size(); };
+
+	virtual void BeginEdit(int index) {}
+	virtual void EndEdit() {}
+	virtual int GetItemTypeCount() const { return 0; }
+	virtual const char* GetItemTypeName(int typeIndex) const { return items[typeIndex].name.c_str(); }
+	virtual const char* GetItemLabel(int index) const { return items[index].name.c_str(); }
+
+	virtual void Get(int index, int** start, int** end, int* type, unsigned int* color);
+	virtual void Add(int type);
+
+	virtual void Del(int index) { items.erase(items.begin() + index); }
+	virtual void Duplicate(int index) { items.push_back(items[index]); }
+
+	virtual void Copy() {}
+	virtual void Paste() {}
+
+	virtual size_t GetCustomHeight(int index) { return 0; }
+	virtual void DoubleClick(int index);
+
+	virtual void CustomDraw(int index, ImDrawList* draw_list, const ImRect& rc, const ImRect& legendRect, const ImRect& clippingRect, const ImRect& legendClippingRect) {}
+	virtual void CustomDrawCompact(int index, ImDrawList* draw_list, const ImRect& rc, const ImRect& clippingRect) {}
+};
