d6489eaeaada98b82e65b8b0f06c0dff35993d00
sfederic <furshazzle@gmail.com>
2022-04-09 19:08:40 +1000

    Removed Renderer::RenderMeshComponents() todo about instancing

--- VEngine/Code/Render/Renderer.cpp
+++ VEngine/Code/Render/Renderer.cpp
@@ -465,7 +465,6 @@ void Renderer::Render()
 	PROFILE_END
 }
 
-//@Todo: I think you can do instancing here based on the mesh filename to up performance.
 void Renderer::RenderMeshComponents()
 {
 	PROFILE_START
