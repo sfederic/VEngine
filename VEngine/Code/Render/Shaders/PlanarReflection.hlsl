d00cdfbea86e9f40d6ed148528ccbbe12d4a2b6e
sfederic <furshazzle@gmail.com>
2022-04-09 19:36:28 +1000

    Created PlanarReflection.hlsl
    It's just DefaultShader.hlsl with the reflection texture

---
+++ VEngine/Code/Render/Shaders/PlanarReflection.hlsl
@@ -0,0 +1,27 @@
+#include "Include/Common.hlsli"
+#include "Include/TransformOut.hlsli"
+
+VS_OUT VSMain(VS_IN i)
+{
+    TransformOut o;
+    return o.Transform(i);
+}
+
+float4 PSMain(VS_OUT i) : SV_Target
+{
+    float4 texColour = reflectionMap.Sample(s, i.uv);
+    clip(texColour.a - 0.10f);
+
+    float3 normal = normalize(i.normal);
+    float4 position = i.posWS;
+
+    float3 V = normalize(eyePosition - position).xyz;
+
+    LightingResult endResult = CalcForwardLighting(V, position, normal);
+
+    endResult.diffuse *= material.ambient;
+    endResult.specular *= material.ambient;
+
+    float4 finalColour = (globalAmbient + endResult.diffuse + endResult.specular) * texColour;
+    return finalColour;
+}
