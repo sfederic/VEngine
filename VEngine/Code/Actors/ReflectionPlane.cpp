0048f0e8356fdf10cfb8e7f0361f0f969abd8d10
sfederic <furshazzle@gmail.com>
2022-04-10 13:19:47 +1000

    Main reflection plane logic

--- VEngine/Code/Actors/ReflectionPlane.cpp
+++ VEngine/Code/Actors/ReflectionPlane.cpp
@@ -1,6 +1,47 @@
 #include "ReflectionPlane.h"
+#include "Camera.h"
+#include "VMath.h"
+#include "Components/MeshComponent.h"
+#include "Gameplay/GameUtils.h"
+
+ReflectionPlane::ReflectionPlane()
+{
+    reflectDirection = XMFLOAT3(0.f, 1.f, 0.f);
+
+    //Keep this camera out of the component hierarchy
+    reflectionCamera = CameraComponent::system.Add(this, CameraComponent(XMFLOAT3(0.f, 0.f, 0.f), false));
+
+    planeMesh = MeshComponent::system.Add(this, MeshComponent("plane.fbx", "test.png", "PlanarReflection.hlsl"));
+    rootComponent = planeMesh;
+}
+
+void ReflectionPlane::Start()
+{
+}
+
+void ReflectionPlane::Tick(float deltaTime)
+{
+    auto activeCameraPos = activeCamera->GetWorldPositionV();
+    auto planePos = GetPositionVector();
+    auto reflectionCameraPos = reflectionCamera->GetWorldPositionV();
+
+    //testing on x-plane only
+    XMVECTOR newPos = activeCameraPos;
+    float diff =  planePos.m128_f32[0] - activeCameraPos.m128_f32[0];
+    newPos.m128_f32[0] = planePos.m128_f32[0] + diff;
+
+    reflectionCamera->SetPosition(newPos);
+}
 
 Properties ReflectionPlane::GetProps()
 {
     return __super::GetProps();
+}
+
+XMMATRIX ReflectionPlane::GetReflectionViewMatrix()
+{
+    auto player = (Actor*)GameUtils::GetPlayer();
+    XMMATRIX view = XMMatrixLookAtLH(reflectionCamera->GetPositionV(),
+        player->GetPositionVector(), VMath::XMVectorUp());
+    return view;
 }
--- VEngine/Code/Actors/ReflectionPlane.h
+++ VEngine/Code/Actors/ReflectionPlane.h
@@ -2,9 +2,23 @@
 #include "Actor.h"
 #include "ActorSystem.h"
 
+struct CameraComponent;
+struct MeshComponent;
+
 struct ReflectionPlane : Actor
 {
 	ACTOR_SYSTEM(ReflectionPlane);
 
+	CameraComponent* reflectionCamera = nullptr;
+
+	MeshComponent* planeMesh = nullptr;
+
+	XMFLOAT3 reflectDirection;
+
+	ReflectionPlane();
+	virtual void Start() override;
+	virtual void Tick(float deltaTime) override;
 	virtual Properties GetProps() override;
+
+	XMMATRIX GetReflectionViewMatrix();
 };
