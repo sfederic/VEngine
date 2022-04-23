0f0d0d88e5cee7bcd763280aaee583ee56927727
sfederic <furshazzle@gmail.com>
2022-04-23 13:36:41 +1000

    DiffuseProbeMap instance mesh testing setup

--- VEngine/Code/Actors/DiffuseProbeMap.cpp
+++ VEngine/Code/Actors/DiffuseProbeMap.cpp
@@ -1,9 +1,14 @@
 #include "DiffuseProbeMap.h"
-#include "Components/EmptyComponent.h"
+#include "Components/InstanceMeshComponent.h"
 
 DiffuseProbeMap::DiffuseProbeMap()
 {
-	rootComponent = EmptyComponent::system.Add(this);
+	int meshCount = sizeX * sizeY * sizeZ;
+	instanceMeshComponent = InstanceMeshComponent::system.Add(this, InstanceMeshComponent(meshCount, "cube.fbx", "test.png"));
+	instanceMeshComponent->SetShaderName("InstanceShader.hlsl");
+	rootComponent = instanceMeshComponent;
+
+	SetInstanceMeshData();
 }
 
 Properties DiffuseProbeMap::GetProps()
@@ -14,4 +19,24 @@ Properties DiffuseProbeMap::GetProps()
 	props.AddProp(sizeY);
 	props.AddProp(sizeZ);
 	return props;
+}
+
+void DiffuseProbeMap::SetInstanceMeshData()
+{
+	std::vector<InstanceData> instanceData;
+
+	for (int x = 0; x < sizeX; x++)
+	{
+		for (int y = 0; y < sizeY; y++)
+		{
+			for (int z = 0; z < sizeZ; z++)
+			{
+				InstanceData data = {};
+				data.world = XMMatrixTranslation((float)x, (float)y, (float)z);
+				instanceData.push_back(data);
+			}
+		}
+	}
+
+	instanceMeshComponent->SetInstanceData(instanceData);
 }
--- VEngine/Code/Actors/DiffuseProbeMap.h
+++ VEngine/Code/Actors/DiffuseProbeMap.h
@@ -2,6 +2,8 @@
 #include "Actor.h"
 #include "ActorSystem.h"
 
+struct InstanceMeshComponent;
+
 struct ProbeData
 {
 	float radiance;
@@ -11,10 +13,13 @@ struct DiffuseProbeMap : Actor
 {
 	ACTOR_SYSTEM(DiffuseProbeMap);
 
-	int sizeX;
-	int sizeY;
-	int sizeZ;
+	InstanceMeshComponent* instanceMeshComponent = nullptr;
+
+	int sizeX = 1;
+	int sizeY = 1;
+	int sizeZ = 1;
 
 	DiffuseProbeMap();
 	virtual Properties GetProps() override;
+	void SetInstanceMeshData();
 };
