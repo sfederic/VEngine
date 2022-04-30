bd0a7061da9723f9a7f7a868d057ce8bde42a3cd
sfederic <furshazzle@gmail.com>
2022-05-01 00:26:30 +1000

    Editor todo about minimzing and ImGui

--- VEngine/Code/Editor/QtEditor.h
+++ VEngine/Code/Editor/QtEditor.h
@@ -5,6 +5,10 @@
 //@Todo: want a qt editor widget that is a scrollable listview with a searchbar on top so that
 //you can search through string values like Memory names or VEnum values or whatever.
 
+//@Todo: there's a 'bug' with the Qt editor where minimizing the window will cause the renderer's
+//swapchain to not resize or something and the ImGui/ImGuizmo & D2D widgets will be smaller with maxizing
+//the window again after minimizing.
+
 struct QtEditor : IEditor, QObject
 {
 	struct QApplication* app;
