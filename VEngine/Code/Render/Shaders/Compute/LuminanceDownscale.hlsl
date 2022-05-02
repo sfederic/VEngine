2548fbc0566fb912bd40ada8b4ddca39f0f9d43e
sfederic <furshazzle@gmail.com>
2022-05-02 22:44:13 +1000

    Uncommented out code from LuminanceDownscale.hlsl

--- VEngine/Code/Render/Shaders/Compute/LuminanceDownscale.hlsl
+++ VEngine/Code/Render/Shaders/Compute/LuminanceDownscale.hlsl
@@ -1,5 +1,4 @@
-Texture2D HDRTex : register(t0);
-RWStructuredBuffer<float> AverageValues1D : register(u0);
+#include "DownScale.hlsli"
 
 #define MAX_GROUPS 64
 
@@ -10,64 +9,64 @@ groupshared float SharedAvgFinal[MAX_GROUPS];
 void CSMain(uint3 groupId : SV_GroupID, uint3 groupThreadId : SV_GroupThreadID,
     uint3 dispatchThreadId : SV_DispatchThreadID)
 {
-    //// Fill the shared memory with the 1D values
-    //float avgLum = 0.0;
-    //if (dispatchThreadId.x < GroupSize)
-    //{
-    //    avgLum = AverageValues1D[dispatchThreadId.x];
-    //}
+    // Fill the shared memory with the 1D values
+    float avgLum = 0.0;
+    if (dispatchThreadId.x < GroupSize)
+    {
+        avgLum = AverageValues1D[dispatchThreadId.x];
+    }
     
-    //SharedAvgFinal[dispatchThreadId.x] = avgLum;
-    //GroupMemoryBarrierWithGroupSync(); // Sync before next step
+    SharedAvgFinal[dispatchThreadId.x] = avgLum;
+    GroupMemoryBarrierWithGroupSync(); // Sync before next step
     
-    //// Downscale from 64 to 16
-    //if (dispatchThreadId.x % 4 == 0)
-    //{
-    //    // Calculate the luminance sum for this step
-    //    float stepAvgLum = avgLum;
-    //    stepAvgLum += dispatchThreadId.x + 1 < GroupSize ?
-    //        SharedAvgFinal[dispatchThreadId.x + 1] : avgLum;
-    //    stepAvgLum += dispatchThreadId.x + 2 < GroupSize ?
-    //        SharedAvgFinal[dispatchThreadId.x + 2] : avgLum;
-    //    stepAvgLum += dispatchThreadId.x + 3 < GroupSize ?
-    //        SharedAvgFinal[dispatchThreadId.x + 3] : avgLum;
+    // Downscale from 64 to 16
+    if (dispatchThreadId.x % 4 == 0)
+    {
+        // Calculate the luminance sum for this step
+        float stepAvgLum = avgLum;
+        stepAvgLum += dispatchThreadId.x + 1 < GroupSize ?
+            SharedAvgFinal[dispatchThreadId.x + 1] : avgLum;
+        stepAvgLum += dispatchThreadId.x + 2 < GroupSize ?
+            SharedAvgFinal[dispatchThreadId.x + 2] : avgLum;
+        stepAvgLum += dispatchThreadId.x + 3 < GroupSize ?
+            SharedAvgFinal[dispatchThreadId.x + 3] : avgLum;
 
-    //    // Store the results
-    //    avgLum = stepAvgLum;
-    //    SharedAvgFinal[dispatchThreadId.x] = stepAvgLum;
-    //}
+        // Store the results
+        avgLum = stepAvgLum;
+        SharedAvgFinal[dispatchThreadId.x] = stepAvgLum;
+    }
     
-    //GroupMemoryBarrierWithGroupSync(); // Sync before next step
+    GroupMemoryBarrierWithGroupSync(); // Sync before next step
     
-    //// Downscale from 16 to 4
-    //if (dispatchThreadId.x % 16 == 0)
-    //{
-    //    // Calculate the luminance sum for this step
-    //    float stepAvgLum = avgLum;
-    //    stepAvgLum += dispatchThreadId.x + 4 < GroupSize ?
-    //        SharedAvgFinal[dispatchThreadId.x + 4] : avgLum;
-    //    stepAvgLum += dispatchThreadId.x + 8 < GroupSize ?
-    //        SharedAvgFinal[dispatchThreadId.x + 8] : avgLum;
-    //    stepAvgLum += dispatchThreadId.x + 12 < GroupSize ?
-    //        SharedAvgFinal[dispatchThreadId.x + 12] : avgLum;
-    //    // Store the results
-    //    avgLum = stepAvgLum;
-    //    SharedAvgFinal[dispatchThreadId.x] = stepAvgLum;
-    //}
-    //GroupMemoryBarrierWithGroupSync(); // Sync before next step
+    // Downscale from 16 to 4
+    if (dispatchThreadId.x % 16 == 0)
+    {
+        // Calculate the luminance sum for this step
+        float stepAvgLum = avgLum;
+        stepAvgLum += dispatchThreadId.x + 4 < GroupSize ?
+            SharedAvgFinal[dispatchThreadId.x + 4] : avgLum;
+        stepAvgLum += dispatchThreadId.x + 8 < GroupSize ?
+            SharedAvgFinal[dispatchThreadId.x + 8] : avgLum;
+        stepAvgLum += dispatchThreadId.x + 12 < GroupSize ?
+            SharedAvgFinal[dispatchThreadId.x + 12] : avgLum;
+        // Store the results
+        avgLum = stepAvgLum;
+        SharedAvgFinal[dispatchThreadId.x] = stepAvgLum;
+    }
+    GroupMemoryBarrierWithGroupSync(); // Sync before next step
     
-    //// Downscale from 4 to 1
-    //if (dispatchThreadId.x == 0)
-    //{
-    //    // Calculate the average luminace
-    //    float fFinalLumValue = avgLum;
-    //    fFinalLumValue += dispatchThreadId.x + 16 < GroupSize ?
-    //        SharedAvgFinal[dispatchThreadId.x + 16] : avgLum;
-    //    fFinalLumValue += dispatchThreadId.x + 32 < GroupSize ?
-    //        SharedAvgFinal[dispatchThreadId.x + 32] : avgLum;
-    //    fFinalLumValue += dispatchThreadId.x + 48 < GroupSize ?
-    //        SharedAvgFinal[dispatchThreadId.x + 48] : avgLum;
-    //    fFinalLumValue /= 64.0;
-    //    AverageLum[0] = fFinalLumValue;
-    //}
+    // Downscale from 4 to 1
+    if (dispatchThreadId.x == 0)
+    {
+        // Calculate the average luminace
+        float fFinalLumValue = avgLum;
+        fFinalLumValue += dispatchThreadId.x + 16 < GroupSize ?
+            SharedAvgFinal[dispatchThreadId.x + 16] : avgLum;
+        fFinalLumValue += dispatchThreadId.x + 32 < GroupSize ?
+            SharedAvgFinal[dispatchThreadId.x + 32] : avgLum;
+        fFinalLumValue += dispatchThreadId.x + 48 < GroupSize ?
+            SharedAvgFinal[dispatchThreadId.x + 48] : avgLum;
+        fFinalLumValue /= 64.0;
+        AverageLum[0] = fFinalLumValue;
+    }
 }
