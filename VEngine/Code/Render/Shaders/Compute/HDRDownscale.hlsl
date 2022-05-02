726021cf2b418a739708d40dc8a9d96ab5abf8c0
sfederic <furshazzle@gmail.com>
2022-05-02 22:43:25 +1000

    Create DownScale.hlsli
    Removed similar code from HDRDownScale.hlsl

--- VEngine/Code/Render/Shaders/Compute/HDRDownscale.hlsl
+++ VEngine/Code/Render/Shaders/Compute/HDRDownscale.hlsl
@@ -1,18 +1,4 @@
-Texture2D HDRTex : register(t0);
-RWStructuredBuffer<float> AverageLum : register(u0);
-groupshared float SharedPositions[1024];
-
-static const float4 LUM_FACTOR = float4(0.299, 0.587, 0.114, 0);
-
-cbuffer DownScaleConstants : register(b0)
-{
-    // Resolution of the down scaled target: x - width, y - height
-    uint2 Res : packoffset(c0);
-    // Total pixel in the downscaled image
-    uint Domain : packoffset(c0.z);
-    // Number of groups dispached on the first pass
-    uint GroupSize : packoffset(c0.w);
-}
+#include "DownScale.hlsli"
 
 float DownScale4x4(uint2 CurPixel, uint groupThreadId)
 {
