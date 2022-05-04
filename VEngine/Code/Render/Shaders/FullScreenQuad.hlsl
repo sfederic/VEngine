18186bfff29550f9df5949183645d1bc704ed9ee
sfederic <furshazzle@gmail.com>
2022-05-05 00:23:46 +1000

    Cleanedup FullscrennQuad.hlsl

--- VEngine/Code/Render/Shaders/FullScreenQuad.hlsl
+++ VEngine/Code/Render/Shaders/FullScreenQuad.hlsl
@@ -1,42 +1,50 @@
-static const float2 arrBasePos[6] =
+static const float2 positions[6] =
 {
-    float2(-1.0, 1.0),
-    float2(1.0, 1.0),
-    float2(-1.0, -1.0),
-    float2(-1.0, -1.0),
-    float2(1.0, -1.0),
-    float2(1.0, 1.0)
+    float2(-1.f, -1.f),
+	float2(-1.f, 1.f),
+	float2(1.f, 1.f),
+	float2(1.f, 1.f),
+	float2(1.f, -1.f),
+    float2(-1.f, -1.f)
 };
 
-static const float2 arrUV[6] =
+static const float2 uvs[6] =
 {
     float2(0.0, 0.0),
-    float2(1.0, 0.0),
     float2(0.0, 1.0),
-    float2(0.0, 1.0),
-    float2(1.0, 0.0),
     float2(1.0, 1.0),
+    float2(1.0, 1.0),
+    float2(1.0, 0.0),
+    float2(0.0, 0.0)
 };
+
+Texture2D<float4> HDRTexture : register(t0);
+SamplerState s : register(s0);
 
 struct VS_OUTPUT
 {
-    float4 Position : SV_Position;
-    float2 UV : TEXCOORD0;
+    float4 position : SV_Position;
+    float2 uv : TEXCOORD0;
 };
 
-VS_OUTPUT VSMain(uint VertexID : SV_VertexID)
+VS_OUTPUT VSMain(uint vertexID : SV_VertexID)
 {
-    VS_OUTPUT Output;
-    Output.Position = float4(arrBasePos[VertexID].xy, 0.0, 1.0);
-    Output.UV = arrUV[VertexID].xy;
-
-    return Output;
+    VS_OUTPUT o;
+    o.position = float4(positions[vertexID].xy, 0.0, 1.0);
+    o.uv = float2(uvs[vertexID].x, -uvs[vertexID].y);
+    return o;
 }
 
-Texture2D<float4> HDRTexture : register(t0);
-StructuredBuffer<float> AvgLum : register(t1);
-
-SamplerState PointSampler : register(s0);
+float3 Rec709ToRec2020(float3 color)
+{
+    static const float3x3 conversion =
+    {
+        0.627402, 0.329292, 0.043306,
+		0.069095, 0.919544, 0.011360,
+		0.016394, 0.088028, 0.895578
+    };
+    return mul(conversion, color);
+}
 
 cbuffer FinalPassConstants : register(b0)
 {
@@ -48,24 +56,30 @@ static const float4 LUM_FACTOR = float4(0.299, 0.587, 0.114, 0);
 
 float3 ToneMapping(float3 HDRColor)
 {
-    // Find the luminance scale for the current pixel
-    float LScale = dot(HDRColor, LUM_FACTOR.xyz);
+    const float gamma = 1.0;
+    const float exposure = 5.0;
     
-    LScale *= MiddleGrey / AvgLum[0];
-    LScale = (LScale + LScale * LScale / LumWhiteSqr) / (1.0 + LScale);
+    // reinhard tone mapping
+    float3 mapped = HDRColor / (HDRColor + float3(1.f, 1.f, 1.f));
     
-    // Apply the luminance scale to the pixels color
-    return HDRColor * LScale;
+    // exposure tone mapping
+    //float3 mapped = float3(1.0, 1.0, 1.0) - exp(-HDRColor * exposure);
+    
+    // gamma correction 
+    mapped = pow(mapped, float3(1.0 / gamma, 1.0 / gamma, 1.0 / gamma));
+  
+    return mapped;
 }
 
 float4 PSMain(VS_OUTPUT i) : SV_Target
 {
     // Get the color sample
-    float3 color = HDRTexture.Sample(PointSampler, i.UV.xy).xyz;
+    float3 color = HDRTexture.Sample(s, i.uv).xyz;
     
     // Tone mapping
-    color = ToneMapping(color);
+    //color = ToneMapping(color);
     
-    // Output the LDR value
+    //color = Rec709ToRec2020(color);
+    
     return float4(color, 1.0);
 }
