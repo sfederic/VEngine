a9bcd2a16e982ccdc57ec8fa65ffc1f7cdd01602
sfederic <furshazzle@gmail.com>
2022-05-23 22:37:44 +1000

    ConstantBuffer update

--- VEngine/Code/Render/ConstantBuffer.h
+++ VEngine/Code/Render/ConstantBuffer.h
@@ -1,19 +1,44 @@
 #pragma once
 #include "RenderUtils.h"
+#include "Debug.h"
 
 struct ID3D11Buffer;
 
 template <typename T>
 class ConstantBuffer
 {
-	ID3D11Buffer* data = nullptr;
+	ID3D11Buffer* buffer = nullptr;
+	uint32_t shaderRegister = 0;
 
 public:
-	ConstantBuffer(const void* initData)
+	ConstantBuffer(const void* initData, uint32_t shaderRegister_) : shaderRegister(shaderRegister_)
 	{
-		data = RenderUtils::CreateDynamicBuffer(sizeof(T), D3D11_BIND_CONSTANT_BUFFER, initData);
-		assert(data);
+		buffer = RenderUtils::CreateDynamicBuffer(sizeof(T), D3D11_BIND_CONSTANT_BUFFER, initData);
 	}
 
-	ID3D11Buffer* GetBuffer() { return data; }
+	ID3D11Buffer* GetBuffer() { return buffer; }
+
+	void Map(T* shaderData)
+	{
+		D3D11_MAPPED_SUBRESOURCE mapped = {};
+		HR(RenderUtils::context->Map(buffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mapped));
+		std::memcpy(mapped.pData, &shaderData, sizeof(T));
+		RenderUtils::context->Unmap(buffer, 0);
+	}
+
+	void SetVS()
+	{
+		RenderUtils::context->VSSetConstantBuffers(shaderRegister, 1, &buffer);
+	}
+
+	void SetPS()
+	{
+		RenderUtils::context->PSSetConstantBuffers(shaderRegister, 1, &buffer);
+	}
+
+	void SetVSAndPS()
+	{
+		SetVS();
+		SetPS();
+	}
 };
