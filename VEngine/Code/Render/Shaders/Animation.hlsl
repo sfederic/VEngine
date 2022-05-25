467f54b5acdf8e5dd663b1a49dcc771c406e1f0b
sfederic <furshazzle@gmail.com>
2022-05-25 22:00:16 +1000

    Copied DefaultShader's PSMain() into Animation's

--- VEngine/Code/Render/Shaders/Animation.hlsl
+++ VEngine/Code/Render/Shaders/Animation.hlsl
@@ -10,26 +10,39 @@ VS_OUT VSMain(VS_IN i)
 //@Todo: need to consolidate DefaultShader's PSMain and this one eventually
 float4 PSMain(VS_OUT i) : SV_Target
 {
-	float4 texColour = t.Sample(s, i.uv);
-	clip(texColour.a - 0.10f);
+    float4 texColour = float4(0.f, 0.f, 0.f, 0.f);
+    
+    if (material.useTexture)
+    {
+        texColour = t.Sample(s, i.uv);
+        clip(texColour.a - 0.10f);
+    }
+    else
+    {
+        texColour = material.ambient;
+    }
 
-	float3 normal = normalize(i.normal);
-	float4 position = i.posWS;
+    float3 normal = normalize(i.normal);
+    float4 position = i.posWS;
 
-	float3 V = normalize(eyePosition - position).xyz;
+    float3 V = normalize(eyePosition - position).xyz;
 
-	LightingResult endResult = CalcForwardLighting(V, position, normal);
+    LightingResult endResult = CalcForwardLighting(V, position, normal);
 
-	endResult.diffuse = saturate(endResult.diffuse);
+    endResult.diffuse *= material.ambient;
+    endResult.specular *= material.ambient;
 
-	float shadowColour = CalcShadowFactor(i.shadowPos);
-
-	float4 localAmbient = float4(0.3f, 0.3f, 0.3f, 1.0f);
-
-	//grey colour 
-	//float4 finalColour = endResult.diffuse * shadowColour;
-
-	float4 finalColour = ((endResult.diffuse + localAmbient) * shadowColour) * texColour;
-
-	return finalColour;
+    //LIGHT PROBES AND SHADOWS COMMENTED OUT FOR NOW
+    
+    //float4 shadowColour = float4(0.f, 0.f, 0.f, 0.f);
+    //if (shadowsEnabled)
+    //{
+    //    shadowColour = CalcShadowFactor(i.shadowPos);
+    //}
+    
+    //float4 shIrradiance = float4(GetSHIrradiance(i.normal, SH), 1.0f);
+    
+    float4 finalColour = (globalAmbient + endResult.diffuse + endResult.specular) * texColour;
+    finalColour.a = material.ambient.a;
+    return finalColour;
 }
