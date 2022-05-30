b23ee90cb8b0a259ac1b981a67d3c9f9baa9a39c
sfederic <furshazzle@gmail.com>
2022-05-31 00:07:45 +1000

    SystemStates todo

--- VEngine/Code/SystemStates.h
+++ VEngine/Code/SystemStates.h
@@ -1,5 +1,10 @@
 #pragma once
 
+//@Todo: there's an interesting todo here where Loaded is only set when the engine is initially loaded.
+//However, the enum here was originally for creating System resources in batches. 
+//e.g. Create the structure for all the Textures and then on world load, create the actual D3D11 data at once
+//for every texture.
+//What's needed is for systems to be set back to Unloaded when their Cleanup() happens.
 enum class SystemStates
 {
 	Unloaded,
