89a839310e5cb5b4fc61635f558401d7415f8d2e
sfederic <furshazzle@gmail.com>
2022-07-05 23:48:08 +1000

    Removed an ImGuizmo error comment
    Must have been fixed in a later commit in ImGuizmo

--- VEngine/Code/Editor/TransformGizmo.h
+++ VEngine/Code/Editor/TransformGizmo.h
@@ -8,10 +8,6 @@
 //REF: OurMachinery guys also have a 3 part blog about gizmo related stuff
 //https://ourmachinery.com/post/borderland-between-rendering-and-editor-part-1/
 
-//NOTE: There was an issue with ImGuizmo where the rotation gizmo would raycast in the opposite direction
-//(with LH matrices?) and only hit the back of the gizmo. The fix was in ImGuizmo.cpp at line 1777.
-//https://github.com/CedricGuillemet/ImGuizmo/issues/144
-
 //Using ImGuizmo for transform, scale and rotation. Press W, E or R for Translation, Scale or Rotation.
 //Keep in mind that ImGuizmo draws its gizmos inside an ImGui window, and not directly onto the viewport.
 //Also, Imguizmo's grid renders over the top of actors in the world, but trying to fix it just brings
