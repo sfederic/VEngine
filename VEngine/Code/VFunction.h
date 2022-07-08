614492001b7f60694ea2d84d093847a43f108ac1
sfederic <furshazzle@gmail.com>
2022-07-08 21:40:36 +1000

    Had to capitalise typename in VFunction because code autism

--- VEngine/Code/VFunction.h
+++ VEngine/Code/VFunction.h
@@ -3,16 +3,16 @@
 #include <vector>
 
 //Acts as a defined information for a function, holding types via packed params.
-template <typename... args>
+template <typename... Args>
 class VFunction
 {
 private:
 	std::string name;
-	std::function<void(args...)> function;
+	std::function<void(Args...)> function;
 	std::vector<std::type_index> argTypes;
 
 public:
-	VFunction(std::string name_, std::function<void(args...)> function_)
+	VFunction(std::string name_, std::function<void(Args...)> function_)
 		: name(name_), function(function_)
 	{
 		IterateTypes();
