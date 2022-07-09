80cb1425f6796f503f84c340f6f8ac0da7c6df73
sfederic <furshazzle@gmail.com>
2022-07-09 11:00:34 +1000

    IFunction constructor calls in VFunction

--- VEngine/Code/VFunction.h
+++ VEngine/Code/VFunction.h
@@ -1,21 +1,21 @@
 #pragma once
+#include "IFunction.h"
 #include <functional>
-#include <vector>
 
 //Acts as a defined information for a function, holding types via packed params.
 template <typename... Args>
 class VFunction : public IFunction
 {
 private:
-	std::string name;
 	std::function<void(Args...)> function;
 
 public:
-	VFunction(std::string name_, std::function<void(Args...)> function_)
-		: name(name_), function(function_)
+	VFunction(std::string name_, std::function<void(Args...)> function_, std::vector<std::string> argNames_)
+		: IFunction(name_)
 	{
+		argNames = argNames_;
+		function = function_;
 		IterateTypes();
-		functionSystem.AddFunction(name, this);
 	}
 
 	void Call(Args... arg)
