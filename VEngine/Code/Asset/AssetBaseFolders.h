1a4b3782ee0a154e0b7f795bf66e94c9bd667b25
sfederic <furshazzle@gmail.com>
2022-07-12 22:48:30 +1000

    More paths to AssetBaseFolders

--- VEngine/Code/Asset/AssetBaseFolders.h
+++ VEngine/Code/Asset/AssetBaseFolders.h
@@ -9,4 +9,7 @@ struct AssetBaseFolders
 	inline static std::string worldMap = "WorldMaps/";
 	inline static std::string actorTemplate = "ActorTemplates/";
 	inline static std::string shader = "Code/Render/Shaders/";
+	inline static std::string audio = "Audio/";
+	inline static std::string audio = "Cutscenes/";
+	inline static std::string audio = "Dialogues/";
 };
