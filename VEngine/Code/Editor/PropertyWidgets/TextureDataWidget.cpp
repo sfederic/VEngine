6309f445007793b6153e28c736465fc2eb9498ef
sfederic <furshazzle@gmail.com>
2022-07-12 22:05:14 +1000

    Fixed up subdirectory filepathing in TextureDataWidget

--- VEngine/Code/Editor/PropertyWidgets/TextureDataWidget.cpp
+++ VEngine/Code/Editor/PropertyWidgets/TextureDataWidget.cpp
@@ -2,6 +2,8 @@
 #include "TextureDataWidget.h"
 #include <qfiledialog.h>
 #include "Render/RenderTypes.h"
+#include "VString.h"
+#include "Asset/AssetBaseFolders.h"
 
 TextureDataWidget::TextureDataWidget(Property& prop_)
 {
@@ -17,16 +19,19 @@ void TextureDataWidget::SetValue()
 	//Note: You have to call CoInitializeEx(NULL, COINIT_APARTMENTTHREADED) for the native file
 	//dialog to work properly.
 	QString filepath = QFileDialog::getOpenFileName(this,
-		tr("Set Texture"), "Textures/", tr("Image Files (*.png *.jpg *.bmp)"));
+		tr("Set Texture"),
+		QString::fromStdString(AssetBaseFolders::texture),
+		tr("Image Files (*.png *.jpg *.bmp)"));
 
 	QFileInfo info(filepath);
 	QString filename = info.fileName();
 
 	if (!filename.isEmpty())
 	{
-		value->filename.assign(filepath.toStdString());
+		value->filename.assign(
+			VString::GetSubStringAtFoundOffset(filepath.toStdString(), AssetBaseFolders::texture));
 
-		setToolTip(QString::fromStdString(value->filename));
+		setToolTip(filepath);
 
 		if (prop.change)
 		{
