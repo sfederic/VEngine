ca27fd21dbd6dbd9d7abcb0d0df8b47c94fe2385
sfederic <furshazzle@gmail.com>
2022-07-12 22:07:52 +1000

    Added shader filepath to assetbasefolders

--- VEngine/Code/Asset/AssetBaseFolders.h
+++ VEngine/Code/Asset/AssetBaseFolders.h
@@ -8,4 +8,5 @@ struct AssetBaseFolders
 	inline static std::string texture = "Textures/";
 	inline static std::string worldMap = "WorldMaps/";
 	inline static std::string actorTemplate = "ActorTemplates/";
+	inline static std::string shader = "Code/Render/Shaders/";
 };
