01c7dea2d4e5dbd7633075e6506c75d5d92d4ff6
sfederic <furshazzle@gmail.com>
2022-07-12 21:59:55 +1000

    Created AssetBaseFolders.h

---
+++ VEngine/Code/Asset/AssetBaseFolders.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+//Base folder starting paths for corresponding assets.
+struct AssetBaseFolders
+{
+	inline static std::string mesh = "Meshes/";
+	inline static std::string texture = "Textures/";
+	inline static std::string worldMap = "WorldMaps/";
+	inline static std::string actorTemplate = "ActorTemplates/";
+};
