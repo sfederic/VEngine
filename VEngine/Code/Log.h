2047c74f65213719f7aa502a1906de197759d678
sfederic <furshazzle@gmail.com>
2022-07-28 12:11:12 +1000

    Removed <format> todo in Log.h

--- VEngine/Code/Log.h
+++ VEngine/Code/Log.h
@@ -1,8 +1,6 @@
 #pragma once
-#include <string>
 
-//@Todo: C++'s std::format is good, but it's fucked up currently.
-//Soon as it's green, think about replacing va_args here.
+#include <string>
 
 void Log(std::string msg, ...);
 void Log(std::wstring msg, ...);
