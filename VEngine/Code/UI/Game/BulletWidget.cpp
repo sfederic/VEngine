77983296dc7181987c6d3f8291940476235a915b
sfederic <furshazzle@gmail.com>
2022-07-30 23:28:07 +1000

    BulletWidget image filepath fix

--- VEngine/Code/UI/Game/BulletWidget.cpp
+++ VEngine/Code/UI/Game/BulletWidget.cpp
@@ -6,7 +6,7 @@ void BulletWidget::Draw(float deltaTime)
 	Layout layout = AlignLayout(25.f, 25.f, Align::BottomLeft);
 	for (int i = 0; i < numBulletsPlayerHas; i++)
 	{
-		Image("bullet.png", layout);
+		Image("UI/bullet.png", layout);
 		layout.AddHorizontalSpace(30.f);
 	}
 }
