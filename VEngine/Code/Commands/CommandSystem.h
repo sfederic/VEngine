ca43d9b8c28e226caa48a5ca816b994985c6d675
sfederic <furshazzle@gmail.com>
2022-08-05 15:53:07 +1000

    Encapsulated CommandSystem properly

--- VEngine/Code/Commands/CommandSystem.h
+++ VEngine/Code/Commands/CommandSystem.h
@@ -7,17 +7,22 @@ struct ICommand;
 //that it could be used during gameplay and for other systems.
 struct CommandSystem
 {
-	std::vector<ICommand*> commands;
+private:
 	uint32_t commandIndex = 0;
+
+public:
+	std::vector<ICommand*> commands;
 
 	void Tick();
 	void Add(ICommand* command);
-	void Undo();
-	void Redo();
 	void Reset();
 
 	//Wind back or forward to a selected command
 	void WindToCommand(uint32_t windCommandIndex);
+
+private:
+	void Undo();
+	void Redo();
 };
 
 extern CommandSystem commandSystem;
