ef4be84cacda9aa84732a0d826689e1ad1ca9b42
sfederic <furshazzle@gmail.com>
2022-08-08 23:36:12 +1000

    GuardWidget image filename fixes

--- VEngine/Code/UI/Game/GuardWidget.cpp
+++ VEngine/Code/UI/Game/GuardWidget.cpp
@@ -4,7 +4,7 @@
 void GuardWidget::Draw(float deltaTime)
 {
 	Layout layout = AlignLayout(100.f, 100.f, Align::Center);
-	Image("shield_icon.png", layout);
+	Image("UI/shield_icon.png", layout);
 
 	//Show aura 'hint' based on memory of attacking unit (think player character remembering movesets)
 	{
@@ -14,19 +14,19 @@ void GuardWidget::Draw(float deltaTime)
 		{
 		case AttackDirection::Up:
 			auraLayout = AlignLayout(100.f, 100.f, Align::Top);
-			Image("aura_window.png", auraLayout);
+			Image("UI/aura_window.png", auraLayout);
 			break;
 		case AttackDirection::Down:
 			auraLayout = AlignLayout(100.f, 100.f, Align::Bottom);
-			Image("aura_window.png", auraLayout);
+			Image("UI/aura_window.png", auraLayout);
 			break;
 		case AttackDirection::Left:
 			auraLayout = AlignLayout(100.f, 100.f, Align::Left);
-			Image("aura_window.png", auraLayout);
+			Image("UI/aura_window.png", auraLayout);
 			break;
 		case AttackDirection::Right:
 			auraLayout = AlignLayout(100.f, 100.f, Align::Right);
-			Image("aura_window.png", auraLayout);
+			Image("UI/aura_window.png", auraLayout);
 			break;
 		}
 	}
@@ -34,10 +34,10 @@ void GuardWidget::Draw(float deltaTime)
 	switch(guardState)
 	{
 	case GuardState::Success:
-		Image("check_mark.png", layout);
+		Image("UI/check_mark.png", layout);
 		break;
 	case GuardState::Fail:
-		Image("cross_mark.png", layout);
+		Image("UI/cross_mark.png", layout);
 		break;
 	}
 }
