512c0d43b878f82aef20abfa5eec269c6f7eef56
sfederic <furshazzle@gmail.com>
2022-08-08 23:37:28 +1000

    Created UI vertex shader
    Old one went missing, meaning it was using default shader and not passing in the non-transformed position to the pixel shader, making the UI images draw offscreen

---
+++ VEngine/Code/Render/Shaders/Vertex/UI_vs.hlsl
@@ -0,0 +1,13 @@
+#include "../Include/Common.hlsli"
+
+VS_OUT main(VS_IN i)
+{
+    VS_OUT o;
+    o.pos = float4(i.pos, 1.0f);
+    o.posWS = mul(model, float4(i.pos, 1.0f));
+    o.uv = i.uv;
+    o.normal = mul((float3x3) model, i.normal);
+    o.instanceID = i.instanceID;
+    o.shadowPos = float4(1.f, 1.f, 1.f, 1.f);
+    return o;
+}
