3418429fddbf6b01fcffc61ee2f9b491bb5fc549
sfederic <furshazzle@gmail.com>
2022-08-14 13:19:55 +1000

    -Removed header comment from VEnum (because it was wrong) -Changed data to std::map

--- VEngine/Code/VEnum.h
+++ VEngine/Code/VEnum.h
@@ -1,16 +1,12 @@
 #pragma once
-#include <unordered_map>
 
-//Serialisation and editor UI widgets are with C++'s enums without reflection is rough,
-//this struct acts as a makeshift enum. It also acts as a base class, meaning
-//you can inherit from it and define the enum's values as static strings in the struct, or just define
-//static string values in a seperate struct and pass them into VEnum given that inheritance
-//isn't supported in the engine's serialisation.
+#include <map>
+
 struct VEnum
 {
 private:
 	int dataIndex = 0;
-	std::unordered_map<std::string, int> data;
+	std::map<std::string, int> data;
 	std::string value;
 
 public:
