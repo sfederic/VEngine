404a16a0a1536e0217b7ba17c0782a410ac4a23f
sfederic <furshazzle@gmail.com>
2022-08-30 21:23:23 +1000

    Removed ref from VEnum string func args because scared of some random string crash probably linked to Visual studio's debug iterator macro

--- VEngine/Code/VEnum.h
+++ VEngine/Code/VEnum.h
@@ -15,7 +15,7 @@ public:
 	std::string GetValue() { return value; }
 
 	void SetValue(const std::string newValue);
-	void Add(const std::string& name);
+	void Add(const std::string name);
 	std::vector<std::string> GetAllNames();
 	bool Compare(const std::string valueToCompare);
 };
