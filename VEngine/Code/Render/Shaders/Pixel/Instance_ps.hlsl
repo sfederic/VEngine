d6b7cc35c3b31efafea55b1bf04bc2477d44f98c
sfederic <furshazzle@gmail.com>
2022-10-10 23:25:26 +1100

    Texture to output colour in Instance shader

--- VEngine/Code/Render/Shaders/Pixel/Instance_ps.hlsl
+++ VEngine/Code/Render/Shaders/Pixel/Instance_ps.hlsl
@@ -3,5 +3,11 @@
 float4 main(VS_OUT i) : SV_Target
 {
     float4 finalColour = instanceData[i.instanceID].colour;
+    
+    if (material.useTexture)
+    {
+        finalColour *= t.Sample(s, i.uv);
+    }
+    
     return finalColour;
 }
