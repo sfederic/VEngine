96ec24dc6f1e588d7f362d11e32aacad0ffddfb2
sfederic <furshazzle@gmail.com>
2022-10-13 23:24:00 +1100

    Created PopupWidget
    Needed it for tutorial messages in the catacombs

---
+++ VEngine/Code/UI/Game/PopupWidget.cpp
@@ -0,0 +1,9 @@
+#include "vpch.h"
+#include "PopupWidget.h"
+
+void PopupWidget::Draw(float deltaTime)
+{
+	auto layout = PercentAlignLayout(0.3f, 0.3f, 0.7f, 0.7f);
+	FillRect(layout);
+	Text(popupText, layout);
+}
