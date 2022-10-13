479a551f50360cdb44a770dd001c86bdccc5431f
sfederic <furshazzle@gmail.com>
2022-10-13 23:51:08 +1100

    Added pause to game world and 'continue' input to PopupWidget

--- VEngine/Code/UI/Game/PopupWidget.cpp
+++ VEngine/Code/UI/Game/PopupWidget.cpp
@@ -1,9 +1,25 @@
 #include "vpch.h"
 #include "PopupWidget.h"
+#include "Input.h"
+#include "Core.h"
 
 void PopupWidget::Draw(float deltaTime)
 {
+	Core::gameWorldPaused = true;
+
 	auto layout = PercentAlignLayout(0.3f, 0.3f, 0.7f, 0.7f);
+
 	FillRect(layout);
+
 	Text(popupText, layout);
+
+	layout.AddVerticalSpace(75.f);
+	Text(L"Push Down to Continue...", layout);
+
+	if (Input::GetKeyUp(Keys::Down))
+	{
+		Core::gameWorldPaused = false;
+
+		RemoveFromViewport();
+	}
 }
