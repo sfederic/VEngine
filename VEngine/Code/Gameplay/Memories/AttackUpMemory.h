893805a2bc99795ee6347fe3a57c56138f6a08b7
sfederic <furshazzle@gmail.com>
2022-10-15 22:00:27 +1100

    Created AttackUpMemory

---
+++ VEngine/Code/Gameplay/Memories/AttackUpMemory.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../Memory.h"
+
+struct AttackUpMemory : public Memory
+{
+	AttackUpMemory() : 
+		Memory("Attack Up",
+		"Increases attack by 1",
+		"",
+		"",
+		1, 0) {}
+};
