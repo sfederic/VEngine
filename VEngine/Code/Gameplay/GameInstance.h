3625021370ee66409bc17a8e62230bb0a460d9c6
sfederic <furshazzle@gmail.com>
2022-10-15 14:55:08 +1100

    Various updates around church and catacomb levels/dialog

--- VEngine/Code/Gameplay/GameInstance.h
+++ VEngine/Code/Gameplay/GameInstance.h
@@ -15,7 +15,7 @@ struct GameInstance
 	inline static int currentHour = 0;
 	inline static int currentMinute = 0;
 
-	inline static std::string startingMap = "catacomb_3.vmap";
+	inline static std::string startingMap = "church.vmap";
 	inline static std::string previousMapMovedFrom = startingMap;
 
 	//Used when continuing from game save files
