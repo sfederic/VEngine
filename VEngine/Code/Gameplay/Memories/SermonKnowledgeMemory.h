3df002b5081e3a484381fd3699a6833138031c92
sfederic <furshazzle@gmail.com>
2022-10-18 23:03:02 +1100

    Created Memory for Sermon knowledge

---
+++ VEngine/Code/Gameplay/Memories/SermonKnowledgeMemory.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../Memory.h"
+
+struct SermonKnowledgeMemory : public Memory
+{
+	SermonKnowledgeMemory() : Memory(
+		"Sermen Knowledge",
+		"Aden now knows how the funeral will be conducted for the Bestowed.",
+		"",
+		"",
+		0, 0
+	) {}
+};
