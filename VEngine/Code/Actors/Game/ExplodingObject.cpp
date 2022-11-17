f2d97ab9cd9b10cdb811719fbbb1846fed62453a
sfederic <furshazzle@gmail.com>
2022-11-17 22:36:44 +1100

    Created ExplodingObject actor

---
+++ VEngine/Code/Actors/Game/ExplodingObject.cpp
@@ -0,0 +1,27 @@
+#include "vpch.h"
+#include "ExplodingObject.h"
+#include "Gameplay/GameUtils.h"
+#include "Actors/Game/Grid.h"
+#include "Actors/Game/Unit.h"
+
+//Explode in a cross pattern.
+void ExplodingObject::Attacked()
+{
+	GameUtils::SpawnSpriteSheet("Sprites/explosion.png", GetPosition(), false, 4, 4);
+
+	auto grid = Grid::system.GetFirstActor();
+
+	std::vector<GridNode*> explodingNodes;
+	grid->GetNeighbouringNodesForceful(GetCurrentNode(), explodingNodes);
+
+	for (auto node : explodingNodes)
+	{
+		GameUtils::SpawnSpriteSheet("Sprites/explosion.png", node->worldPosition, false, 4, 4);
+
+		auto unit = grid->GetUnitAtNode(node);
+		if (unit)
+		{
+			unit->InflictDamage(1);
+		}
+	}
+}
