79725dcb9623fa05d3dafeb0dd9ee31924f19453
sfederic <furshazzle@gmail.com>
2022-11-18 00:15:51 +1100

    Small map updates

--- VEngine/Code/Gameplay/GameInstance.h
+++ VEngine/Code/Gameplay/GameInstance.h
@@ -12,7 +12,7 @@ struct GameInstance
 	//this is to set in the editor to know whether to use map files from WorldMaps/ vs GameSaves/
 	inline static bool useGameSaves = false;
 
-	inline static std::string startingMap = "test.vmap";
+	inline static std::string startingMap = "church.vmap";
 	inline static std::string previousMapMovedFrom = startingMap;
 
 	//Used when continuing from game save files
