2631fb227e7c01850336affc41e2f604e8934895
sfederic <furshazzle@gmail.com>
2022-11-26 17:58:44 +1100

    Static MemoryMerger function fix

--- VEngine/Code/Gameplay/Memories/MergedMemories/MemoryMerger.h
+++ VEngine/Code/Gameplay/Memories/MergedMemories/MemoryMerger.h
@@ -4,7 +4,7 @@
 
 namespace MemoryMerger
 {
-	static void Init();
-	static void MergeMemories(const std::string memoryName1, const std::string memoryName2);
+	void Init();
+	void MergeMemories(const std::string memoryName1, const std::string memoryName2);
 };
 
