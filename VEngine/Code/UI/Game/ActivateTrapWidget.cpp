a8e00f3b1b106b0dec8c9a1c02f58ecd2dcf800e
sfederic <furshazzle@gmail.com>
2022-12-04 13:04:36 +1100

    Created ActivateTrapWidget

---
+++ VEngine/Code/UI/Game/ActivateTrapWidget.cpp
@@ -0,0 +1,9 @@
+#include "vpch.h"
+#include "ActivateTrapWidget.h"
+
+void ActivateTrapWidget::Draw(float deltaTime)
+{
+	auto layout = PercentAlignLayout(0.4f, 0.7f, 0.6f, 0.8f);
+	FillRect(layout);
+	Text(L"Activate Trap?", layout);
+}
---
+++ VEngine/Code/UI/Game/ActivateTrapWidget.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../Widget.h"
+
+class ActivateTrapWidget : public Widget
+{
+public:
+	virtual void Draw(float deltaTime) override;
+};
