01ab8c8dfb909e4cc2ae7a2fe10c0b73d277fca2
sfederic <furshazzle@gmail.com>
2022-12-05 21:30:42 +1100

    Test UI visuals for BattleCards in hand widget

--- VEngine/Code/UI/Game/BattleCardHandWidget.cpp
+++ VEngine/Code/UI/Game/BattleCardHandWidget.cpp
@@ -1,7 +1,16 @@
 #include "vpch.h"
 #include "BattleCardHandWidget.h"
+#include "Actors/Game/Player.h"
+#include "Gameplay/BattleCard.h"
 
 void BattleCardHandWidget::Draw(float deltaTime)
 {
+	auto layout = PercentAlignLayout(0.1f, 0.8f, 0.2f, 0.9f);
 
+	for (auto& battleCard : Player::system.GetFirstActor()->battleCardsInHand)
+	{
+		FillRect(layout);
+
+		layout.AddHorizontalSpace(50.f);
+	}
 }
--- VEngine/Code/UI/Game/BattleCardHandWidget.h
+++ VEngine/Code/UI/Game/BattleCardHandWidget.h
@@ -2,6 +2,7 @@
 
 #include "../Widget.h"
 
+//Shows and allows selection from all battle cards player has.
 class BattleCardHandWidget : public Widget
 {
 public:
