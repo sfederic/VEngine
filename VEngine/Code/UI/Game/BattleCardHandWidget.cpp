ed334552a88de27be43f8a78ddbf48626784e044
sfederic <furshazzle@gmail.com>
2022-12-05 21:44:38 +1100

    Fix battlecardhandwidget layout spacing

--- VEngine/Code/UI/Game/BattleCardHandWidget.cpp
+++ VEngine/Code/UI/Game/BattleCardHandWidget.cpp
@@ -5,12 +5,12 @@
 
 void BattleCardHandWidget::Draw(float deltaTime)
 {
-	auto layout = PercentAlignLayout(0.1f, 0.8f, 0.2f, 0.9f);
+	auto layout = PercentAlignLayout(0.1f, 0.8f, 0.15f, 0.9f);
 
 	for (auto& battleCard : Player::system.GetFirstActor()->battleCardsInHand)
 	{
 		FillRect(layout);
 
-		layout.AddHorizontalSpace(50.f);
+		layout.AddHorizontalSpace(75.f);
 	}
 }
