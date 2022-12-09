c9b44b2f0feb034779c25bbf18c1ee121a933b16
sfederic <furshazzle@gmail.com>
2022-12-09 22:42:57 +1100

    Deleted all EnemyUnit classes

--- VEngine/Code/Actors/Game/EnemyUnits/LineAttackerUnit.cpp
+++
@@ -1,8 +0,0 @@
-#include "vpch.h"
-#include "LineAttackerUnit.h"
-#include "Gameplay/UnitSkills/LineAttackSkill.h"
-
-LineAttackUnit::LineAttackUnit()
-{
-	skills.emplace("area", std::make_unique<LineAttackSkill>());
-}
--- VEngine/Code/Actors/Game/EnemyUnits/ShieldUnit.cpp
+++
@@ -1,7 +0,0 @@
-#include "vpch.h"
-#include "ShieldUnit.h"
-
-ShieldUnit::ShieldUnit()
-{
-	attackDirections = AttackDirection::Back | AttackDirection::Left | AttackDirection::Right;
-}
