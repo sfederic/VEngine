a62272d7602205a4c28871ade617f9e331c570ff
sfederic <furshazzle@gmail.com>
2022-12-20 23:27:31 +1100

    TurretEnemy to shoot turret bullet

--- VEngine/Code/Actors/Game/TurretEnemy.cpp
+++ VEngine/Code/Actors/Game/TurretEnemy.cpp
@@ -3,11 +3,27 @@
 #include "Components/MeshComponent.h"
 #include "Actors/Game/TurretBullet.h"
 #include "Gameplay/GameplayTags.h"
+#include "VMath.h"
 
 void TurretEnemy::Create()
 {
     auto mesh = CreateComponent(MeshComponent("shield.fbx", "test.png"), "Mesh");
     rootComponent->AddChild(mesh);
+}
+
+void TurretEnemy::Start()
+{
+    shootTimer = VMath::RandomRange(0.f, SHOOT_TIMER_MAX);
+}
+
+void TurretEnemy::Tick(float deltaTime)
+{
+    shootTimer += deltaTime;
+    if (shootTimer > SHOOT_TIMER_MAX)
+    {
+        Shoot();
+        shootTimer = 0.f;
+    }
 }
 
 Properties TurretEnemy::GetProps()
@@ -19,6 +35,8 @@ Properties TurretEnemy::GetProps()
 
 void TurretEnemy::Shoot()
 {
-    auto bullet = TurretBullet::system.Add(GetTransform());
-    bullet->SetMovementDirection(GetForwardVectorV());
+    auto transform = GetTransform();
+    transform.position.y += 0.55f;
+    auto bullet = TurretBullet::system.Add(transform);
+    bullet->SetMovementDirection(VMath::GlobalUpVector());
 }
--- VEngine/Code/Actors/Game/TurretEnemy.h
+++ VEngine/Code/Actors/Game/TurretEnemy.h
@@ -9,8 +9,13 @@ public:
 	ACTOR_SYSTEM(TurretEnemy);
 
 	void Create() override;
+	void Start() override;
+	void Tick(float deltaTime) override;
 	Properties GetProps() override;
 
 private:
 	void Shoot();
+
+	float shootTimer = 0.f;
+	inline static const float SHOOT_TIMER_MAX = 1.5f;
 };
