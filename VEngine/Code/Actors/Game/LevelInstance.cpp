25754e47e74dc29ed5a6bffb15d4de0c7ed80e15
sfederic <furshazzle@gmail.com>
2022-12-23 10:03:59 +1100

    Added small offset when checking if point inside level size

--- VEngine/Code/Actors/Game/LevelInstance.cpp
+++ VEngine/Code/Actors/Game/LevelInstance.cpp
@@ -18,15 +18,16 @@ bool LevelInstance::CheckIfPointInsideLevelSize(XMVECTOR point)
 	XMFLOAT3 pos;
 	XMStoreFloat3(&pos, point);
 
-	if (pos.x < 0.f || pos.x > levelSize.x)
+	//The '0.1f's here are just a small offset to work with to handle floating point accuracy.
+	if (pos.x < -0.1f || pos.x > levelSize.x + 0.1f)
 	{
 		return false;
 	}
-	else if (pos.y < 0.f || pos.y > levelSize.y)
+	else if (pos.y < -0.1f || pos.y > levelSize.y + 0.1f)
 	{
 		return false;
 	}
-	else if (pos.z < 0.f || pos.z > levelSize.z)
+	else if (pos.z < -0.1f || pos.z > levelSize.z + 0.1f)
 	{
 		return false;
 	}
