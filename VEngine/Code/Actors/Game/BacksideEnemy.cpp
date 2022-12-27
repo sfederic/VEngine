b2f8b15b8670807e6b9ef7d27235413f7f0dc83b
sfederic <furshazzle@gmail.com>
2022-12-27 11:12:08 +1100

    Created BacksideEnemy

---
+++ VEngine/Code/Actors/Game/BacksideEnemy.cpp
@@ -0,0 +1,28 @@
+#include "vpch.h"
+#include "BacksideEnemy.h"
+#include "Components/MeshComponent.h"
+#include "Actors/Game/Player.h"
+#include "VMath.h"
+
+void BacksideEnemy::Create()
+{
+	auto mesh = CreateComponent(MeshComponent("turret.fbx", "test.png"), "Mesh");
+	rootComponent->AddChild(mesh);
+}
+
+bool BacksideEnemy::CanBeHit()
+{
+	auto player = Player::system.GetFirstActor();
+	auto playerForward = player->GetForwardVectorV();
+	auto forward = GetForwardVectorV();
+
+	VMath::RoundVector(playerForward);
+	VMath::RoundVector(forward);
+
+	if (XMVector4Equal(playerForward, forward))
+	{
+		return true;
+	}
+
+	return false;
+}
---
+++ VEngine/Code/Actors/Game/BacksideEnemy.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Enemy.h"
+
+//Enemy the player has to attack at its back by comparing player's forward vector.
+class BacksideEnemy : public Enemy
+{
+public:
+	ACTOR_SYSTEM(BacksideEnemy);
+
+	void Create() override;
+	bool CanBeHit() override;
+};
