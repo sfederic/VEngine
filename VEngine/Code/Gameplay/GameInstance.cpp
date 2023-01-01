5c83152d612908fba12b6b47bfd261a8586dca50
sfederic <furshazzle@gmail.com>
2023-01-01 17:33:29 +1100

    Misc. commit

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -1,7 +1,7 @@
 #include "vpch.h"
 #include "GameInstance.h"
 
-std::string GameInstance::startingMap = "space_elevator.vmap";
+std::string GameInstance::startingMap = "GI_test.vmap";
 
 Properties GameInstance::GetInstanceSaveData()
 {
