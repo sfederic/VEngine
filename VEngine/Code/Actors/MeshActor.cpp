f493c69aaee5df0ea69ffd927047545771ec61f4
sfederic <furshazzle@gmail.com>
2023-01-22 15:19:23 +1100

    MeshActor ctor using .vmesh files

--- VEngine/Code/Actors/MeshActor.cpp
+++ VEngine/Code/Actors/MeshActor.cpp
@@ -5,14 +5,15 @@
 
 MeshActor::MeshActor()
 {
+	//Don't call Create() on these spawns as the actor spawn code in WorldEditor already calls it.
 	if (!spawnMeshFilename.empty())
 	{
-		mesh = MeshComponent::system.Add("Mesh", this, MeshComponent(spawnMeshFilename.c_str(), "test.png"));
+		mesh = MeshComponent::system.Add("Mesh", this, MeshComponent(spawnMeshFilename.c_str(), "test.png"), false);
 		rootComponent = mesh;
 	}
 	else
 	{
-		mesh = MeshComponent::system.Add("Mesh", this, MeshComponent("cube.fbx", "test.png"));
+		mesh = MeshComponent::system.Add("Mesh", this, MeshComponent("cube.vmesh", "test.png"), false);
 		rootComponent = mesh;
 	}
 }
