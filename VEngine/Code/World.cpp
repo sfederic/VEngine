901ee5ec4238ccb74467944e87d1b75dd1bb46ed
sfederic <furshazzle@gmail.com>
2023-01-24 23:42:30 +1100

    Material create todo in World::Start()

--- VEngine/Code/World.cpp
+++ VEngine/Code/World.cpp
@@ -50,6 +50,8 @@ void World::Init()
 
 void World::Start()
 {
+	//@Todo: creating all materials here isn't working. All meshes are creating materials twice on world loads,
+	//once here then again in MeshComponent::Create()
 	MaterialSystem::CreateAllMaterials();
 	TextureSystem::CreateAllTextures();
 
