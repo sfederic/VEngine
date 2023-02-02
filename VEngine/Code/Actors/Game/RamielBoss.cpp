406b4e9649d9d9e2027fffa58e97fb06c955f613
sfederic <furshazzle@gmail.com>
2023-02-02 23:03:03 +1100

    Ramiel boss charge beam attack

--- VEngine/Code/Actors/Game/RamielBoss.cpp
+++ VEngine/Code/Actors/Game/RamielBoss.cpp
@@ -1,2 +1,53 @@
 #include "vpch.h"
 #include "RamielBoss.h"
+#include "Actors/Game/Player.h"
+#include "Components/MeshComponent.h"
+#include "Physics/Raycast.h"
+#include "Gameplay/GameUtils.h"
+
+RamielBoss::RamielBoss()
+{
+    mesh = CreateComponent(MeshComponent("ramielboss.vmesh", ""), "Mesh");
+    rootComponent = mesh;
+}
+
+void RamielBoss::Create()
+{
+    mesh->SetUseTexture(false);
+    mesh->SetAmbientColour(ambientColour);
+}
+
+void RamielBoss::Tick(float deltaTime)
+{
+    chargeAttackTimer += deltaTime;
+    constexpr float chargeAttackMaxTime = 5.f;
+
+    //Change colour over time as attack indicator
+    mesh->SetAmbientColour(ambientColour);
+    ambientColour.x += deltaTime / chargeAttackMaxTime;
+
+    if (chargeAttackTimer > chargeAttackMaxTime)
+    {
+        FireChargeBeamAtPlayer();
+        chargeAttackTimer = 0.f;
+        ambientColour.x = 0.1f;
+    }
+}
+
+Properties RamielBoss::GetProps()
+{
+    auto props = __super::GetProps();
+    props.title = "RamielBoss";
+    return props;
+}
+
+void RamielBoss::FireChargeBeamAtPlayer()
+{
+    HitResult hit(this);
+    auto player = Player::system.GetFirstActor();
+    XMVECTOR playerPos = player->GetPositionV();
+    if (Raycast(hit, GetPositionV(), playerPos))
+    {
+        GameUtils::SpawnSpriteSheet("Sprites/blue_explosion.png", hit.GetHitPosV(), false, 5, 5);
+    }
+}
--- VEngine/Code/Actors/Game/RamielBoss.h
+++ VEngine/Code/Actors/Game/RamielBoss.h
@@ -2,6 +2,22 @@
 
 #include "Enemy.h"
 
+class MeshComponent;
+
 class RamielBoss : public Enemy
 {
+public:
+	ACTOR_SYSTEM(RamielBoss);
+
+	RamielBoss();
+	void Create() override;
+	void Tick(float deltaTime) override;
+	Properties GetProps() override;
+
+private:
+	void FireChargeBeamAtPlayer();
+
+	MeshComponent* mesh = nullptr;
+	XMFLOAT4 ambientColour = XMFLOAT4(0.1f, 0.1f, 0.9f, 1.f);
+	float chargeAttackTimer = 0.f;
 };
