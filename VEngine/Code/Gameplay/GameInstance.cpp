9a8567a6f353445c767ad7050685f01d30468fe0
sfederic <furshazzle@gmail.com>
2023-02-10 23:29:11 +1100

    Misc. commit

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -1,7 +1,7 @@
 #include "vpch.h"
 #include "GameInstance.h"
 
-std::string GameInstance::startingMap = "test3.vmap";
+std::string GameInstance::startingMap = "underwater.vmap";
 
 Properties GameInstance::GetInstanceSaveData()
 {
