dd485a2aef1cb9d0bdfb57635ef49e1e22451736
sfederic <furshazzle@gmail.com>
2023-02-11 15:56:06 +1100

    Created MapSelectionActor

---
+++ VEngine/Code/Actors/Game/MapSelectionActor.cpp
@@ -0,0 +1,26 @@
+#include "vpch.h"
+#include "MapSelectionActor.h"
+#include "Components/BoxTriggerComponent.h"
+#include "Actors/Game/MapScreenSelector.h"
+
+MapSelectionActor::MapSelectionActor()
+{
+    boxTrigger = CreateComponent<BoxTriggerComponent>("BoxTrigger");
+}
+
+void MapSelectionActor::Tick(float deltaTime)
+{
+    auto mapScreenSelector = MapScreenSelector::system.GetFirstActor();
+
+    if (boxTrigger->Contains(mapScreenSelector->GetPositionV()))
+    {
+
+    }
+}
+
+Properties MapSelectionActor::GetProps()
+{
+    auto props = __super::GetProps();
+    props.title = GetTypeName();
+    return props;
+}
---
+++ VEngine/Code/Actors/Game/MapSelectionActor.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../Actor.h"
+#include "../ActorSystem.h"
+
+class BoxTriggerComponent;
+
+//An actor the MapScreenSelector can select on the map, holding information for the level to enter via map.
+//Triggers when the MapScreenSelector is inside this actor's boxtrigger, that way you can place meshes around it.
+class MapSelectionActor : public Actor
+{
+public:
+	ACTOR_SYSTEM(MapSelectionActor);
+
+	MapSelectionActor();
+	void Tick(float deltaTime) override;
+	Properties GetProps() override;
+
+private:
+	BoxTriggerComponent* boxTrigger = nullptr;
+};
+
