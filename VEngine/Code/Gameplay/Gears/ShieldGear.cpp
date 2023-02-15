bf1bc3e6f5e3ad7559c84e08fecd55964bfa5666
sfederic <furshazzle@gmail.com>
2023-02-16 00:17:22 +1100

    -Created ShieldGear -New folder for Gear classes

---
+++ VEngine/Code/Gameplay/Gears/ShieldGear.cpp
@@ -0,0 +1,7 @@
+#include "vpch.h"
+#include "ShieldGear.h"
+
+void ShieldGear::Use()
+{
+
+}
---
+++ VEngine/Code/Gameplay/Gears/ShieldGear.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "PlayerGear.h"
+
+class ShieldGear : public PlayerGear
+{
+public:
+	void Use() override;
+};
