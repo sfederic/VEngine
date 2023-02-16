ecadaf5fc47bcbee4394ec2a024157006aeb3a09
sfederic <furshazzle@gmail.com>
2023-02-16 22:41:12 +1100

    Added equipped gears as global props

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -6,10 +6,18 @@ std::string GameInstance::startingMap = "map_screen.vmap";
 //Global properties
 static bool ramielDefeated = false;
 
+std::string equippedGear0;
+std::string equippedGear1;
+
 Properties GameInstance::GetGlobalProps()
 {
 	Properties props("GameInstance");
 	props.Add("Continue Map", &mapToLoadOnContinue);
+
 	props.Add("RamielDefeated", &ramielDefeated);
+
+	props.Add("EquippedGear0", &equippedGear0);
+	props.Add("EquippedGear1", &equippedGear1);
+
 	return props;
 }
