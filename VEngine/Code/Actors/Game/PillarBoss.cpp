c5c6a886eddb6ae8b468929d27d2ae6c4706bf56
sfederic <furshazzle@gmail.com>
2023-02-18 22:45:34 +1100

    Rough code to spawn lasers on PillarBoss

--- VEngine/Code/Actors/Game/PillarBoss.cpp
+++ VEngine/Code/Actors/Game/PillarBoss.cpp
@@ -1,6 +1,9 @@
 #include "vpch.h"
 #include "PillarBoss.h"
 #include "Components/MeshComponent.h"
+#include "Physics/Raycast.h"
+#include "Core/VMath.h"
+#include "Particle/Polyboard.h"
 
 PillarBoss::PillarBoss()
 {
@@ -13,7 +16,43 @@ void PillarBoss::Create()
 	baseMesh->SetMeshFilename("cylinder.vmesh");
 }
 
+void PillarBoss::Start()
+{
+	CalcLaserPositions();
+}
+
 void PillarBoss::Tick(float deltaTime)
 {
-	AddRotation(GetUpVectorV(), 10.f * deltaTime);
+	AddRotation(GetUpVectorV(), 15.f * deltaTime);
+}
+
+//Shoot a ray from the center of the base mesh to hit a backfacing triangle to denote a point to shoot a laser
+//from, then take the normal of that hit triangle and shoot the actual laser out along that normal's direction.
+void PillarBoss::CalcLaserPositions()
+{
+	const auto dir = VMath::RandomUnitDirectionVector();
+	XMVECTOR pos = GetPositionV();
+	HitResult hit; //Don't ignore this actor
+
+	if (Raycast(hit, pos, dir, 30.f))
+	{
+		LaserData data;
+		data.shootDirection = XMLoadFloat3(&hit.normal);
+		data.position = hit.GetHitPosV();
+
+		auto laserMesh = MeshComponent::system.Add("LaserMesh",
+			this,
+			MeshComponent("cube.vmesh", "test.png"),
+			true);
+		rootComponent->AddChild(laserMesh);
+		laserMesh->SetLocalScale(0.5f);
+		laserMesh->SetWorldRotation(XMQuaternionRotationAxis(dir, 0.f));
+		laserMesh->SetWorldPosition(data.position);
+
+		auto beam = Polyboard::system.Add("Beam", this, Polyboard(), true);
+
+		data.mesh = laserMesh;
+		data.beam = beam;
+		laserPositions.push_back(data);
+	}
 }
--- VEngine/Code/Actors/Game/PillarBoss.h
+++ VEngine/Code/Actors/Game/PillarBoss.h
@@ -2,6 +2,17 @@
 
 #include "Enemy.h"
 
+struct Polyboard;
+class MeshComponent;
+
+struct LaserData
+{
+	XMVECTOR position = XMVectorSet(0.f, 0.f, 0.f, 1.f);
+	XMVECTOR shootDirection = XMVectorZero();
+	Polyboard* beam = nullptr;
+	MeshComponent* mesh = nullptr;
+};
+
 //Pillar Enemy that spins around and shoots lasers.
 class PillarBoss : public Enemy
 {
@@ -10,8 +21,12 @@ public:
 
 	PillarBoss();
 	void Create() override;
+	void Start() override;
 	void Tick(float deltaTime) override;
 
 private:
+	void CalcLaserPositions();
+
 	MeshComponent* baseMesh = nullptr;
+	std::vector<LaserData> laserPositions;
 };
