4a0a5c27f2b960a32612a12437ed90598ca48b98
sfederic <furshazzle@gmail.com>
2023-02-26 22:25:38 +1100

    Created LevelCompleteWidget

---
+++ VEngine/Code/UI/Game/LevelCompleteWidget.cpp
@@ -0,0 +1,15 @@
+#include "vpch.h"
+#include "LevelCompleteWidget.h"
+#include "Gameplay/GameUtils.h"
+
+void LevelCompleteWidget::Draw(float deltaTime)
+{
+	auto rectLayout = PercentAlignLayout(0.3f, 0.3f, 0.7f, 0.7f);
+	FillRect(rectLayout);
+	
+	auto buttonLayout = PercentAlignLayout(0.45f, 0.5f, 0.55f, 0.6f);
+	if (Button(L"Done", buttonLayout))
+	{
+		GameUtils::LoadWorld("map_screen.vmap");
+	}
+}
---
+++ VEngine/Code/UI/Game/LevelCompleteWidget.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../Widget.h"
+
+class LevelCompleteWidget : public Widget
+{
+public:
+	virtual void Draw(float deltaTime) override;
+};
