bc6a4663d62e87a2fe82aa067bd18826dd057dbb
sfederic <furshazzle@gmail.com>
2023-02-27 21:28:32 +1100

    Removed polyboard general todo

--- VEngine/Code/Particle/Polyboard.h
+++ VEngine/Code/Particle/Polyboard.h
@@ -8,10 +8,6 @@
 struct Buffer;
 struct MeshDataProxy;
 
-//@Todo: the main idea behind the Polyboard was to show an NPC's intent (both in combat and out) in the
-//same way FFXII shows aggro from enemies and player NPCs while in combat. The code here is still extremely
-//rough and needs work with how buffers are handled and vertices are generated smoothly.
-
 //Taken from 'Mathematics for 3D Game Programming and Computer Graphics', Chapter 9.3.3: Polyboards.
 //Polyboard is a line of 'planes' facing the camera for line-like effects (beams, lightning, etc.)
 class Polyboard : public SpatialComponent
