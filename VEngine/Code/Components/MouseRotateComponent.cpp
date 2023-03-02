107b308a3bb62ba29a44eee7c1a47daa5fef27da
sfederic <furshazzle@gmail.com>
2023-03-02 20:53:36 +1100

    Creaetd MouseRotateComponent

---
+++ VEngine/Code/Components/MouseRotateComponent.cpp
@@ -0,0 +1,7 @@
+#include "vpch.h"
+#include "MouseRotateComponent.h"
+
+void MouseRotateComponent::Tick(float deltaTime)
+{
+	FPSCameraRotation();
+}
---
+++ VEngine/Code/Components/MouseRotateComponent.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "SpatialComponent.h"
+#include "ComponentSystem.h"
+
+//Component where mouse fps controls can be used to rotate it.
+//(Mainly for pairing cameras and whatnot to it to preserve child parent transforms)
+class MouseRotateComponent : public SpatialComponent
+{
+public:
+	COMPONENT_SYSTEM(MouseRotateComponent);
+
+	MouseRotateComponent() {}
+	void Tick(float deltaTime) override;
+};
