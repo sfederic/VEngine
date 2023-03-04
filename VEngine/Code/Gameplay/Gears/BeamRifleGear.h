f6c03980d9d1eed69ec5c21316c5099458461fda
sfederic <furshazzle@gmail.com>
2023-03-04 21:38:08 +1100

    Polyboard todo on player beam rifle

--- VEngine/Code/Gameplay/Gears/BeamRifleGear.h
+++ VEngine/Code/Gameplay/Gears/BeamRifleGear.h
@@ -2,6 +2,9 @@
 
 #include "Gear.h"
 
+//@Todo: when the beam rifle spawns its polyboard, the board isn't orienting itself correctly because
+//the player camera is set in a manner behind the player.
+
 //Beam rifle shoots through enemies like a skewer.
 class BeamRifleGear : public Gear
 {
