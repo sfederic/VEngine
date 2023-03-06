58880dc924aef50266f9245c0de4d8afeaaf0884
sfederic <furshazzle@gmail.com>
2023-03-06 23:58:13 +1100

    Removed &

--- VEngine/Code/Gameplay/WorldFunctions.cpp
+++ VEngine/Code/Gameplay/WorldFunctions.cpp
@@ -7,7 +7,7 @@ std::map<std::string, std::function<void()>> worldStartFunctionMap;
 
 void WorldFunctions::SetupWorldStartFunctions()
 {
-	worldStartFunctionMap.emplace("map_screen.vmap", [&]() {
+	worldStartFunctionMap.emplace("map_screen.vmap", []() {
 		GameUtils::LoadPlayerMapScreenPos();
 	});
 }
