22ad7eca5821bf753359e0c824ed1b92587e1f49
sfederic <furshazzle@gmail.com>
2023-03-07 23:15:53 +1100

    Misc. commit

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -1,7 +1,7 @@
 #include "vpch.h"
 #include "GameInstance.h"
 
-std::string GameInstance::startingMap = "map_screen.vmap";
+std::string GameInstance::startingMap = "test.vmap";
 
 //Global properties
 static bool ramielDefeated = false;
