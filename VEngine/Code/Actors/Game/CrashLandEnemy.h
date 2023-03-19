f615d48b141d84ad8667e2e4f5d71452e447c2d0
sfederic <furshazzle@gmail.com>
2023-03-19 13:06:44 +1100

    Removed CrashLandEnemy todo

--- VEngine/Code/Actors/Game/CrashLandEnemy.h
+++ VEngine/Code/Actors/Game/CrashLandEnemy.h
@@ -6,9 +6,6 @@ class MeshComponent;
 class BoxTriggerComponent;
 class ParticleEmitter;
 
-//@Todo: I'd love a visual effect like a growth/vines coming out of the enemy into the structure/floor
-//its landed on once it has landed.
-
 //Enemy that crash lands into shit from outside the map, basically a gimmick spawning enemy.
 //Think like the Sayians from DBZ.
 class CrashLandEnemy : public Enemy
