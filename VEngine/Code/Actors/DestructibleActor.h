26d37df11960cd829eef25e071afbb8a98d641c5
sfederic <furshazzle@gmail.com>
2023-03-19 13:22:49 +1100

    DescructibleActor encapsulate

--- VEngine/Code/Actors/DestructibleActor.h
+++ VEngine/Code/Actors/DestructibleActor.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "Actor.h"
 #include "ActorSystem.h"
 
@@ -7,14 +8,16 @@ struct DestructibleMeshComponent;
 //@Todo: because this the destructible component uses convex bounds, you can't
 //click on this actor in the editor, or delete it. To fix this, you'd have to
 //move raycasts and collision completely to PhysX.
-struct DestructibleActor : Actor
+class DestructibleActor : public Actor
 {
+public:
 	ACTOR_SYSTEM(DestructibleActor);
-
-	DestructibleMeshComponent* destructibleMesh = nullptr;
 
 	DestructibleActor();
 	void Start() override;
 	void Tick(float deltaTime) override;
 	Properties GetProps() override;
+
+private:
+	DestructibleMeshComponent* destructibleMesh = nullptr;
 };
