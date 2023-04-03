e3ae4ac54fb324c20d4ab835f613a01871f40d2c
sfederic <furshazzle@gmail.com>
2023-04-03 20:51:01 +1000

    Removed Vengen global prop variables

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -1,27 +1,12 @@
 #include "vpch.h"
 #include "GameInstance.h"
 
-std::string GameInstance::startingMap = "test.vmap";
+std::string GameInstance::startingMap = "library.vmap";
 
 //Global properties
-static bool ramielDefeated = false;
-
-static std::string primaryGear;
-static std::string secondaryGear;
-
-static DirectX::XMFLOAT3 playerMapScreenPos;
 
 Properties GameInstance::GetGlobalProps()
 {
 	Properties props("GameInstance");
-	props.Add("Continue Map", &mapToLoadOnContinue);
-
-	props.Add("RamielDefeated", &ramielDefeated);
-
-	props.Add("PrimaryGear", &primaryGear);
-	props.Add("SecondaryGear", &secondaryGear);
-
-	props.Add("PlayerMapScreenPos", &playerMapScreenPos);
-
 	return props;
 }
