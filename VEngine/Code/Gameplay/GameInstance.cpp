78ecf788ecb860e64ce41c13d49a73e2f6df582b
sfederic <furshazzle@gmail.com>
2023-04-16 16:23:36 +1000

    Misc. commit

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -1,7 +1,7 @@
 #include "vpch.h"
 #include "GameInstance.h"
 
-std::string GameInstance::startingMap = "test.vmap";
+std::string GameInstance::startingMap = "block_break.vmap";
 
 //Global properties
 
