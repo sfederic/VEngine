51824c2da5a09b682f49f5558bdb070081825e68
sfederic <furshazzle@gmail.com>
2023-04-16 14:52:32 +1000

    Misc. commit

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -1,7 +1,7 @@
 #include "vpch.h"
 #include "GameInstance.h"
 
-std::string GameInstance::startingMap = "church_library.vmap";
+std::string GameInstance::startingMap = "test.vmap";
 
 //Global properties
 
