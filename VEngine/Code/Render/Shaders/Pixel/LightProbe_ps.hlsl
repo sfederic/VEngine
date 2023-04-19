415ded52612e97f4bb4a4e0c9c8b0cee622674bf
sfederic <furshazzle@gmail.com>
2023-04-20 00:00:43 +1000

    LightProbe_ps using irradiance instead of debug colour

--- VEngine/Code/Render/Shaders/Pixel/LightProbe_ps.hlsl
+++ VEngine/Code/Render/Shaders/Pixel/LightProbe_ps.hlsl
@@ -2,7 +2,6 @@
 
 float4 main(VS_OUT o) : SV_TARGET
 {
-    //float3 irradiance = GetSHIrradiance(o.normal, SH);
-    //return float4(irradiance, 1.0);
-    return float4(1, 0, 0, 1);
+    float3 irradiance = GetSHIrradiance(o.normal, SH);
+    return float4(irradiance, 1.0);
 }
