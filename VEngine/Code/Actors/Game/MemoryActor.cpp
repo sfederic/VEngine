aec7c80adf77000d387ef17657519c0d365d0ca9
sfederic <furshazzle@gmail.com>
2023-04-20 23:17:22 +1000

    Tripped MemoryActor down from previous prototype implementations

--- VEngine/Code/Actors/Game/MemoryActor.cpp
+++ VEngine/Code/Actors/Game/MemoryActor.cpp
@@ -1,74 +1,15 @@
 #include "vpch.h"
 #include "MemoryActor.h"
-#include "Gameplay/GameUtils.h"
-#include "Render/Material.h"
 #include "Components/MeshComponent.h"
-#include "Components/BoxTriggerComponent.h"
-#include "Actors/Game/Grid.h"
-#include "Core/Input.h"
-#include "Gameplay/GridNode.h"
 
-MemoryActor::MemoryActor()
+void MemoryActor::Create()
 {
-    isGridObstacle = isMemoryCreated;
-
-    trigger = BoxTriggerComponent::system.Add("Trigger", this);
-    trigger->renderWireframeColour = XMFLOAT4(1.f, 0.f, 1.f, 1.f);
-    trigger->SetTargetAsPlayer();
-    rootComponent->AddChild(trigger);
-}
-
-void MemoryActor::Start()
-{
-    __super::Start();
-
-    mesh->SetTexture("memory_pattern.png");
-}
-
-void MemoryActor::Tick(float deltaTime)
-{
-    __super::Tick(deltaTime);
-
-    if (!isMemoryCreated && trigger->ContainsTarget())
-    {
-        if (Input::GetKeyUp(Keys::Down))
-        {
-            ActivateFromMemory();
-        }
-    }
+    mesh->SetShaderItem(ShaderItems::MemoryObject);
 }
 
 Properties MemoryActor::GetProps()
 {
     auto props = __super::GetProps();
     props.Add("Memory Name", &memoryName);
-    props.Add("IsMemoryCreated", &isMemoryCreated);
     return props;
-}
-
-void MemoryActor::Interact()
-{
-    ActivateFromMemory();
-}
-
-void MemoryActor::ActivateFromMemory()
-{
-    if (!isMemoryCreated && GameUtils::CheckIfMemoryExists(memoryName))
-    {
-        //Stop metarial animation
-        mesh->GetMaterial().uvOffsetSpeed = XMFLOAT2(0.f, 0.f);
-        mesh->GetMaterial().uvRotationSpeed = 0.f;
-        mesh->SetTexture(mesh->GetMaterial().textureData.filename);
-
-        GetCurrentNode()->Hide();
-
-        GameUtils::PlayAudioOneShot("intuition_gained.wav");
-
-        isGridObstacle = true;
-
-        //Reset world position of all grid nodes
-        Grid::system.GetFirstActor()->Awake();
-
-        isMemoryCreated = true;
-    }
 }
--- VEngine/Code/Actors/Game/MemoryActor.h
+++ VEngine/Code/Actors/Game/MemoryActor.h
@@ -1,29 +1,17 @@
 #pragma once
+
 #include "GridActor.h"
 
-struct BoxTriggerComponent;
-
-//Actor that is transparent in the world, giving a hint to the player to instantiate it
-//by interacting with it with the appropriate memory held by the player.
-struct MemoryActor : GridActor
+class MemoryActor : public GridActor
 {
+public:
 	ACTOR_SYSTEM(MemoryActor);
 
-	BoxTriggerComponent* trigger = nullptr;
+	void Create() override;
+	Properties GetProps() override;
 
-	//The memory to instantiate the object
-	std::string memoryName;
-
-	//Whether this actor has already been created via player's memory
-	bool isMemoryCreated = false;
-
-	MemoryActor();
-	virtual void Start() override;
-	virtual void Tick(float deltaTime) override;
-	virtual Properties GetProps() override;
-
-	virtual void Interact() override;
+	void Interact() override {}
 
 private:
-	void ActivateFromMemory();
+	std::string memoryName;
 };
