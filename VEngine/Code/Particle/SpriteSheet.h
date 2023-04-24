8f58c89ee429905073aedd83b5519cb5e7ca8d73
sfederic <furshazzle@gmail.com>
2023-04-24 23:37:51 +1000

    Anim loop bool arg in SetupSpriteData()

--- VEngine/Code/Particle/SpriteSheet.h
+++ VEngine/Code/Particle/SpriteSheet.h
@@ -15,11 +15,12 @@ public:
 	virtual void Create() override;
 	virtual Properties GetProps() override;
 
-	void SetupSpriteData(std::string_view textureFilename, int numRows, int numColumns)
+	void SetupSpriteData(std::string_view textureFilename, int numRows, int numColumns, bool loop)
 	{
 		textureData.filename = textureFilename;
 		numSheetRows = numRows;
 		numSheetColumns = numColumns;
+		loopAnimation = loop;
 	}
 
 	void UpdateSprite();
