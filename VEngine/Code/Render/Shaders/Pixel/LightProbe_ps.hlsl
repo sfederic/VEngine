959b173e2f50990be014cf8160af03894b68cc00
sfederic <furshazzle@gmail.com>
2023-04-24 22:55:18 +1000

    Use light probe instance data matrix is LightProbe_ps shader for SH

--- VEngine/Code/Render/Shaders/Pixel/LightProbe_ps.hlsl
+++ VEngine/Code/Render/Shaders/Pixel/LightProbe_ps.hlsl
@@ -2,6 +2,6 @@
 
 float4 main(VS_OUT i) : SV_TARGET
 {
-    float3 irradiance = CalcSHIrradiance(i.normal, SH);
-    return float4(irradiance, 1.0);
+    float3 irradiance = CalcSHIrradiance(normalize(i.normal), lightProbeInstanceData[i.instanceID].SH);
+    return float4(irradiance * PI * 4, 1.0);
 }
