6b9392e9e7e6ad2bfc4d0cf858318f57bc5a0eb7
sfederic <furshazzle@gmail.com>
2023-04-25 00:14:36 +1000

    Changed churchfire class name

---
+++ VEngine/Code/Actors/Game/MemoryActors/ChurchFire.cpp
@@ -0,0 +1,25 @@
+#include "vpch.h"
+#include "ChurchFire.h"
+#include "Gameplay/GameUtils.h"
+#include "Particle/SpriteSheet.h"
+
+ChurchFire::ChurchFire()
+{
+	flameSpriteSheet = CreateComponent<SpriteSheet>("FlameSprite");
+	rootComponent->AddChild(flameSpriteSheet);
+}
+
+void ChurchFire::Create()
+{
+	SetMemoryName(L"Church Fire");
+
+	flameSpriteSheet->SetLocalPosition(0.f, 0.75f, 0.f);
+	flameSpriteSheet->SetLocalScale(0.5f, 0.5f, 0.5f);
+	flameSpriteSheet->SetActive(false);
+	flameSpriteSheet->SetupSpriteData("Sprites/fire_sheet.png", 4, 4, true);
+}
+
+void ChurchFire::Interact()
+{
+	flameSpriteSheet->SetActive(true);
+}
