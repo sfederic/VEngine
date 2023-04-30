209403c807deea2f0dfa525991df4b97e3dfd4c4
sfederic <furshazzle@gmail.com>
2023-04-30 15:45:46 +1000

    Valve to toggle water source on rotate

--- VEngine/Code/Actors/Game/Valve.cpp
+++ VEngine/Code/Actors/Game/Valve.cpp
@@ -1,8 +1,31 @@
 #include "vpch.h"
 #include "Valve.h"
+#include "Core/Log.h"
 #include "Components/MeshComponent.h"
+#include "WaterSource.h"
 
 void Valve::Create()
 {
 	mesh->SetMeshFilename("valve.vmesh");
+}
+
+Properties Valve::GetProps()
+{
+	auto props = __super::GetProps();
+	props.title = GetTypeName();
+	props.Add("Water Source", &waterSourceName);
+	return props;
+}
+
+void Valve::OnLinkRotate()
+{
+	auto waterSource = dynamic_cast<WaterSource*>(World::GetActorByNameAllowNull(waterSourceName));
+	if (waterSource)
+	{
+		waterSource->ToggleActive();
+	}
+	else
+	{
+		Log("Water Source [%s] not found.", waterSourceName.c_str());
+	}
 }
--- VEngine/Code/Actors/Game/Valve.h
+++ VEngine/Code/Actors/Game/Valve.h
@@ -8,4 +8,10 @@ public:
 	ACTOR_SYSTEM(Valve);
 
 	void Create() override;
+	Properties GetProps() override;
+
+	void OnLinkRotate() override;
+
+private:
+	std::string waterSourceName;
 };
