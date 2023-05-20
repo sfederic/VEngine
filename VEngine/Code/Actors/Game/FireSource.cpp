f6fb6cee8bb434e6033f8e96e43d6e9cc00ac561
sfederic <furshazzle@gmail.com>
2023-05-20 13:25:34 +1000

    FireSource able to burn Burnable grid actors

--- VEngine/Code/Actors/Game/FireSource.cpp
+++ VEngine/Code/Actors/Game/FireSource.cpp
@@ -2,18 +2,41 @@
 #include "FireSource.h"
 #include "Particle/SpriteSheet.h"
 #include "Components/MeshComponent.h"
+#include "Components/BoxTriggerComponent.h"
+#include "Burnable.h"
 
 FireSource::FireSource()
 {
 	//Doesn't need to be a node, just needs some sort of mesh for Douse()s to hit
 	mesh->SetMeshFilename("node.vmesh");
+
+	fireTrigger = CreateComponent<BoxTriggerComponent>("FireTrigger");
+	rootComponent->AddChild(fireTrigger);
 }
 
 void FireSource::Create()
 {
+	fireTrigger->SetExtents(1.f, 1.f, 1.f);
+
 	fireSprite = CreateComponent<SpriteSheet>("FireSprite");
 	fireSprite->SetupSpriteData("Sprites/fire_sheet.png", 4, 4, true);
 	rootComponent->AddChild(fireSprite);
+}
+
+void FireSource::Tick(float deltaTime)
+{
+	__super::Tick(deltaTime);
+
+	for (auto& burnable : Burnable::system.GetActors())
+	{
+		for (auto mesh : burnable->GetComponentsOfType<MeshComponent>())
+		{
+			if (fireTrigger->Intersects(mesh->GetBoundsInWorldSpace()))
+			{
+				burnable->Burn();
+			}
+		}
+	}
 }
 
 void FireSource::Douse()
--- VEngine/Code/Actors/Game/FireSource.h
+++ VEngine/Code/Actors/Game/FireSource.h
@@ -3,6 +3,7 @@
 #include "GridActor.h"
 
 class SpriteSheet;
+class BoxTriggerComponent;
 
 class FireSource : public GridActor
 {
@@ -11,8 +12,11 @@ public:
 
 	FireSource();
 	void Create() override;
+	void Tick(float deltaTime) override;
+
 	void Douse() override;
 
 private:
 	SpriteSheet* fireSprite = nullptr;
+	BoxTriggerComponent* fireTrigger = nullptr;
 };
