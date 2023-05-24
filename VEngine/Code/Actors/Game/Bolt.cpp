4b40036e0fb2da73af1932a2743173a7ce0c73c3
sfederic <furshazzle@gmail.com>
2023-05-25 00:04:07 +1000

    Created Bolt grid actor

---
+++ VEngine/Code/Actors/Game/Bolt.cpp
@@ -0,0 +1,8 @@
+#include "vpch.h"
+#include "Bolt.h"
+#include "Components/MeshComponent.h"
+
+void Bolt::Create()
+{
+	mesh->SetMeshFilename("bolt.vmesh");
+}
---
+++ VEngine/Code/Actors/Game/Bolt.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "GridActor.h"
+
+//Metallic bolt to work with Nut grid actor.
+class Bolt : public GridActor
+{
+public:
+	ACTOR_SYSTEM(Bolt);
+
+	void Create() override;
+};
