e0437bfd227fcce90ef925b821044c1018b6208e
sfederic <furshazzle@gmail.com>
2023-05-28 13:11:09 +1000

    Misc. commit

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -1,7 +1,7 @@
 #include "vpch.h"
 #include "GameInstance.h"
 
-std::string GameInstance::startingMap = "broiler.vmap";
+std::string GameInstance::startingMap = "door_and_moat.vmap";
 std::string GameInstance::previousMapMovedFrom = startingMap;
 
 //Used when continuing from game save files
