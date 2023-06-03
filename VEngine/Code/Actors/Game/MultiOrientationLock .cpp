01215a6d1a42c63cdbc64aa73a3f3487be92d3d2
sfederic <furshazzle@gmail.com>
2023-06-03 12:15:27 +1000

    Created MultiOrientationLock

---
+++ VEngine/Code/Actors/Game/MultiOrientationLock .cpp
@@ -0,0 +1,61 @@
+#include "vpch.h"
+#include "MultiOrientationLock .h"
+#include "OrientationLock.h"
+#include "GridActor.h"
+
+MultiOrientationLock::MultiOrientationLock()
+{
+	SetEmptyRootComponent();
+}
+
+void MultiOrientationLock::Start()
+{
+	lockActors.push_back(dynamic_cast<OrientationLock*>(World::GetActorByNameAllowNull(lockActorName0)));
+	lockActors.push_back(dynamic_cast<OrientationLock*>(World::GetActorByNameAllowNull(lockActorName1)));
+	lockActors.push_back(dynamic_cast<OrientationLock*>(World::GetActorByNameAllowNull(lockActorName2)));
+	lockActors.push_back(dynamic_cast<OrientationLock*>(World::GetActorByNameAllowNull(lockActorName3)));
+
+	gridActorToActivateOnAllLocksBeingCorrect = 
+		dynamic_cast<GridActor*>(World::GetActorByNameAllowNull(gridActorToActivate));
+}
+
+void MultiOrientationLock::Tick(float deltaTime)
+{
+	CheckIfAllActorOrientationsAreCorrect();
+}
+
+Properties MultiOrientationLock::GetProps()
+{
+	auto props = __super::GetProps();
+	props.title = GetTypeName();
+	props.Add("Lock Actor 0", &lockActorName0);
+	props.Add("Lock Actor 1", &lockActorName1);
+	props.Add("Lock Actor 2", &lockActorName2);
+	props.Add("Lock Actor 3", &lockActorName3);
+	props.Add("GridActor Activate", &gridActorToActivate);
+	return props;
+}
+
+void MultiOrientationLock::CheckIfAllActorOrientationsAreCorrect()
+{
+	int numOfCorrectlyOrientedLockActorsToCheck = lockActors.size();
+
+	for (auto lockActor : lockActors)
+	{
+		if (lockActor == nullptr)
+		{
+			numOfCorrectlyOrientedLockActorsToCheck--;
+			continue;
+		}
+
+		if (lockActor->IsOrientationCorrect())
+		{
+			numOfCorrectlyOrientedLockActorsToCheck--;
+		}
+	}
+
+	if (numOfCorrectlyOrientedLockActorsToCheck == 0)
+	{
+		gridActorToActivateOnAllLocksBeingCorrect->Activate();
+	}
+}
---
+++ VEngine/Code/Actors/Game/MultiOrientationLock .h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "../Actor.h"
+#include "../ActorSystem.h"
+
+class OrientationLock;
+class GridActor;
+
+//Takes the names of multiple OrientatonLock actors and activates this actor if all their orientations are correct.
+//Note that this actor isn't a Grid Actor. It's more like an inanimate manager handling Orientation Locks.
+class MultiOrientationLock : public Actor
+{
+public:
+	ACTOR_SYSTEM(MultiOrientationLock);
+
+	MultiOrientationLock();
+	void Start() override;
+	void Tick(float deltaTime) override;
+	Properties GetProps() override;
+
+private:
+	void CheckIfAllActorOrientationsAreCorrect();
+
+	std::string lockActorName0;
+	std::string lockActorName1;
+	std::string lockActorName2;
+	std::string lockActorName3;
+
+	std::vector<OrientationLock*> lockActors;
+
+	std::string gridActorToActivate;
+	GridActor* gridActorToActivateOnAllLocksBeingCorrect = nullptr;
+};
