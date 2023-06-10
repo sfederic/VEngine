19af6f25ac28619ac1bcde687a9615983a3f86ed
sfederic <furshazzle@gmail.com>
2023-06-10 20:41:09 +1000

    Excavator code to mine Mineable

--- VEngine/Code/Actors/Game/Excavator.cpp
+++ VEngine/Code/Actors/Game/Excavator.cpp
@@ -2,6 +2,7 @@
 #include "Excavator.h"
 #include "Components/MeshComponent.h"
 #include "Physics/Raycast.h"
+#include "Mineable.h"
 
 void Excavator::Create()
 {
@@ -22,6 +23,10 @@ void Excavator::Dig()
 	SimpleBoxCast(center, XMFLOAT3(0.5f, 0.5f, 0.5f), hit, false, false);
 	for (auto actor : hit.hitActors)
 	{
-
+		auto mineable = dynamic_cast<Mineable*>(actor);
+		if(mineable)
+		{
+			mineable->Mine();
+		}
 	}
 }
