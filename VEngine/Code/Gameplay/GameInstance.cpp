21583fa1606900a192ca981f5b17d1c942e0cea0
sfederic <furshazzle@gmail.com>
2023-06-24 14:43:38 +1000

    Misc. commit

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -1,7 +1,7 @@
 #include "vpch.h"
 #include "GameInstance.h"
 
-std::string GameInstance::startingMap = "mines_oil_barrel_trail.vmap";
+std::string GameInstance::startingMap = "church_statue_weapons.vmap";
 std::string GameInstance::previousMapMovedFrom = startingMap;
 
 //Used when continuing from game save files
