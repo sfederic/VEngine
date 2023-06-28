f130add51d18c9cf55b2f4194fae8716d1f8470c
sfederic <furshazzle@gmail.com>
2023-06-29 00:17:32 +1000

    Created OrientationTrigger

---
+++ VEngine/Code/Actors/Game/OrientationTrigger.cpp
@@ -0,0 +1,47 @@
+#include "vpch.h"
+#include "OrientationTrigger.h"
+#include "OrientationLock.h"
+#include "Components/BoxTriggerComponent.h"
+
+OrientationTrigger::OrientationTrigger()
+{
+	boxTrigger = CreateComponent<BoxTriggerComponent>("BoxTrigger");
+	rootComponent = boxTrigger;
+}
+
+void OrientationTrigger::Tick(float deltaTime)
+{
+	CheckIfOrientationLockIsAlignedInTrigger();
+}
+
+Properties OrientationTrigger::GetProps()
+{
+	auto props = __super::GetProps();
+	props.title = GetTypeName();
+	props.Add("Target Actor", &actorNameToActivate);
+	return props;
+}
+
+void OrientationTrigger::CheckIfOrientationLockIsAlignedInTrigger()
+{
+	for (auto orientationLock : World::GetAllActorsOfTypeInWorld<OrientationLock>())
+	{
+		if (boxTrigger->Contains(orientationLock->GetPositionV()))
+		{
+			if (orientationLock->IsOrientationCorrect())
+			{
+				actorToActivate = World::GetActorByNameAllowNull(actorNameToActivate);
+				if (actorToActivate)
+				{
+					actorToActivate->Activate();
+					return;
+				}
+			}
+		}
+	}
+
+	if (actorToActivate)
+	{
+		actorToActivate->Deactivate();
+	}
+}
---
+++ VEngine/Code/Actors/Game/OrientationTrigger.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../Actor.h"
+#include "../ActorSystem.h"
+
+class BoxTriggerComponent;
+
+//Same as an OrientationLock, except this trigger expects an actor within it alongside the correct orientation.
+class OrientationTrigger : public Actor
+{
+public:
+	ACTOR_SYSTEM(OrientationTrigger);
+
+	OrientationTrigger();
+	void Tick(float deltaTime) override;
+	Properties GetProps() override;
+
+private:
+	void CheckIfOrientationLockIsAlignedInTrigger();
+
+	BoxTriggerComponent* boxTrigger = nullptr;
+
+	std::string actorNameToActivate;
+	Actor* actorToActivate = nullptr;
+};
