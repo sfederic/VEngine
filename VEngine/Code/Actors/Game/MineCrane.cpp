3481e889b384103673983d87e54369d72eb8da65
sfederic <furshazzle@gmail.com>
2023-06-29 23:25:03 +1000

    Created MineCrane grid actor

---
+++ VEngine/Code/Actors/Game/MineCrane.cpp
@@ -0,0 +1,35 @@
+#include "vpch.h"
+#include "MineCrane.h"
+#include "Components/MeshComponent.h"
+#include "Components/BoxTriggerComponent.h"
+
+MineCrane::MineCrane()
+{
+	hookTrigger = CreateComponent<BoxTriggerComponent>("HookTrigger");
+	rootComponent->AddChild(hookTrigger);
+}
+
+void MineCrane::Create()
+{
+	ignoreRotationValidCheck = true;
+	canBeRotatedPitchXAxis = false;
+
+	mesh->SetMeshFilename("crane.vmesh");
+
+	hookTrigger->SetLocalPosition(0.f, 0.f, 2.f);
+}
+
+void MineCrane::Tick(float deltaTime)
+{
+	__super::Tick(deltaTime);
+
+	if (hasActorHookedOn) return;
+	
+	for (auto gridActor : World::GetAllActorsAsBaseType<GridActor>())
+	{
+		if (hookTrigger->Contains(gridActor->GetPositionV()))
+		{
+			hasActorHookedOn = true;
+		}
+	}
+}
