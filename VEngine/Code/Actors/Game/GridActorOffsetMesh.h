a5fd5e255601cd3940c598a273237f8394cf817c
sfederic <furshazzle@gmail.com>
2023-07-03 18:22:57 +1000

    GridActorOffsetMesh todo on nextRot collision checks

--- VEngine/Code/Actors/Game/GridActorOffsetMesh.h
+++ VEngine/Code/Actors/Game/GridActorOffsetMesh.h
@@ -2,6 +2,9 @@
 
 #include "GridActor.h"
 
+//@Todo: GridActorOffsetMesh doesn't work with this GridActor::CheckNextRotationBoundsIntersect().
+//Will need to change the func around to work with non-root meshes.
+
 //Grid actor where the main mesh is child of a empty root to enable offset rotations during gameplay.
 class GridActorOffsetMesh : public GridActor
 {
