eb1f4ab64ecaaad6266288f45a4aacc252ee1e8c
sfederic <furshazzle@gmail.com>
2023-07-17 21:54:54 +1000

    Explodable getprops

--- VEngine/Code/Actors/Game/Explodable.cpp
+++ VEngine/Code/Actors/Game/Explodable.cpp
@@ -1,6 +1,13 @@
 #include "vpch.h"
 #include "Explodable.h"
 
+Properties Explodable::GetProps()
+{
+	auto props = __super::GetProps();
+	props.title = GetTypeName();
+	return props;
+}
+
 void Explodable::HitByExplosive()
 {
 	Destroy();
--- VEngine/Code/Actors/Game/Explodable.h
+++ VEngine/Code/Actors/Game/Explodable.h
@@ -7,5 +7,7 @@ class Explodable : public GridActor
 public:
 	ACTOR_SYSTEM(Explodable);
 
+	Properties GetProps() override;
+
 	void HitByExplosive();
 };
