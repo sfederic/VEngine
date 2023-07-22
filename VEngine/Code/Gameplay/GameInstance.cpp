e894c8df1306fdebdb5f25f43f24326b982bfff9
sfederic <furshazzle@gmail.com>
2023-07-22 20:36:48 +1000

    Misc. entrance trigger links in maps

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -1,7 +1,7 @@
 #include "vpch.h"
 #include "GameInstance.h"
 
-std::string GameInstance::startingMap = "mines_main_room.vmap";
+std::string GameInstance::startingMap = "market_courtyard.vmap";
 std::string GameInstance::previousMapMovedFrom = startingMap;
 
 //Used when continuing from game save files
