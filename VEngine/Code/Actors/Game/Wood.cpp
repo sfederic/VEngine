ffad59ed1ae8b7ddb85b75b507ca1a1326a5ac33
sfederic <furshazzle@gmail.com>
2023-07-24 22:55:19 +1000

    Wood::Interact()

--- VEngine/Code/Actors/Game/Wood.cpp
+++ VEngine/Code/Actors/Game/Wood.cpp
@@ -9,6 +9,18 @@ void Wood::Create()
 
 void Wood::Slice()
 {
+	hasBeenCut = true;
+
 	mesh->SetMeshFilename("cut_wood.vmesh");
 	mesh->ReCreate();
+}
+
+void Wood::Interact()
+{
+	if (hasBeenCut)
+	{
+		//@Todo: set to an "inventory"
+		RecalcCurrentNodePosition();
+		Destroy();
+	}
 }
