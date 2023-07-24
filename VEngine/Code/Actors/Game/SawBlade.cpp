2c0f243782401fd667d8a274cf99314a87288561
sfederic <furshazzle@gmail.com>
2023-07-24 22:40:23 +1000

    SawBlade func to Slice() grid actors

--- VEngine/Code/Actors/Game/SawBlade.cpp
+++ VEngine/Code/Actors/Game/SawBlade.cpp
@@ -1,5 +1,12 @@
 #include "vpch.h"
 #include "SawBlade.h"
+#include "Components/BoxTriggerComponent.h"
+
+SawBlade::SawBlade()
+{
+	sliceTrigger = CreateComponent<BoxTriggerComponent>("SliceTrigger");
+	rootComponent->AddChild(sliceTrigger);
+}
 
 void SawBlade::Create()
 {
@@ -9,10 +16,26 @@ void SawBlade::Create()
 void SawBlade::PowerUp()
 {
 	isCutting = true;
-	AddRotation(GetUpVectorV(), 50.5f);
+	AddRotation(GetUpVectorV(), 50.f);
 }
 
 void SawBlade::PowerDown()
 {
 	isCutting = false;
+}
+
+void SawBlade::SliceSurroundingGridActors()
+{
+	for (GridActor* actor : World::GetAllActorsAsBaseType<GridActor>())
+	{
+		if (actor == this)
+		{
+			continue;
+		}
+
+		if (sliceTrigger->Contains(actor->GetPositionV()))
+		{
+			actor->Slice();
+		}
+	}
 }
--- VEngine/Code/Actors/Game/SawBlade.h
+++ VEngine/Code/Actors/Game/SawBlade.h
@@ -2,16 +2,23 @@
 
 #include "GridActor.h"
 
+class BoxTriggerComponent;
+
 //Spinning saw blade that is powered by a crystal
 class SawBlade : public GridActor
 {
 public:
 	ACTOR_SYSTEM(SawBlade);
 
+	SawBlade();
 	void Create() override;
 	void PowerUp() override;
 	void PowerDown() override;
 
 private:
+	void SliceSurroundingGridActors();
+
+	BoxTriggerComponent* sliceTrigger = nullptr;
+
 	bool isCutting = false;
 };
