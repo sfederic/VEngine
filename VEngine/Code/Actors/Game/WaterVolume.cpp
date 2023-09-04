00ee33288e9aa9da401a6defdf2591ffd9f65db6
sfederic <furshazzle@gmail.com>
2023-09-05 00:36:53 +1000

    Removed setting WaterVolume trigger extents in Create()

--- VEngine/Code/Actors/Game/WaterVolume.cpp
+++ VEngine/Code/Actors/Game/WaterVolume.cpp
@@ -22,7 +22,6 @@ void WaterVolume::Create()
 
 	//Set just below the mesh representing the surface
 	waterVolumeTrigger->SetLocalPosition(0.f, -0.5f, 0.f);
-	waterVolumeTrigger->SetExtents(0.5f, 0.5f, 0.5f);
 }
 
 void WaterVolume::Tick(float deltaTime)
