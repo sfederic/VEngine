a7b71921546a3ada3e182389ffa6f278307b78b8
sfederic <furshazzle@gmail.com>
2023-09-04 23:51:11 +1000

    PickupWidget check is held item exists, render widget if it does

--- VEngine/Code/UI/Game/PickupWidget.cpp
+++ VEngine/Code/UI/Game/PickupWidget.cpp
@@ -1,8 +1,12 @@
 #include "vpch.h"
 #include "PickupWidget.h"
+#include "Gameplay/GameInstance.h"
 
 void PickupWidget::Draw(float deltaTime)
 {
-	auto layout = PercentAlignLayout(0.8f, 0.8f, 0.9f, 0.9f);
-	Image(heldItemIconFilename, layout);
+	if (!GameInstance::GetHeldPlayerItem().empty())
+	{
+		auto layout = PercentAlignLayout(0.8f, 0.8f, 0.9f, 0.9f);
+		Image(heldItemIconFilename, layout);
+	}
 }
