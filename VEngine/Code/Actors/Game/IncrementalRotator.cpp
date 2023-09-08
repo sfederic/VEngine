b375d605504b192b36e0ebe3d681424aee3d0c79
sfederic <furshazzle@gmail.com>
2023-09-08 17:29:28 +1000

    IncrementalRotator set player focus actor

--- VEngine/Code/Actors/Game/IncrementalRotator.cpp
+++ VEngine/Code/Actors/Game/IncrementalRotator.cpp
@@ -3,33 +3,34 @@
 
 void IncrementalRotator::Start()
 {
-    __super::Start();
+	__super::Start();
 
-    actorToRotate = dynamic_cast<GridActor*>(World::GetActorByName(actorNameToRotate));
+	actorToRotate = dynamic_cast<GridActor*>(World::GetActorByName(actorNameToRotate));
+	SetPlayerFocusGridActor(actorToRotate);
 }
 
 Properties IncrementalRotator::GetProps()
 {
-    auto props = __super::GetProps();
-    props.title = GetTypeName();
-    props.Add("Actor To Rotate", &actorNameToRotate);
-    props.Add("Rotation Increment", &rotationIncrement);
-    props.Add("Rotate Direction", &rotateDirection);
-    return props;
+	auto props = __super::GetProps();
+	props.title = GetTypeName();
+	props.Add("Actor To Rotate", &actorNameToRotate);
+	props.Add("Rotation Increment", &rotationIncrement);
+	props.Add("Rotate Direction", &rotateDirection);
+	return props;
 }
 
 void IncrementalRotator::OnLinkRotateLeft()
 {
-    XMVECTOR newNextRot = XMQuaternionMultiply(actorToRotate->GetNextRot(),
-        DirectX::XMQuaternionRotationAxis(XMLoadFloat3(&rotateDirection),
-            XMConvertToRadians(rotationIncrement)));
-    actorToRotate->SetNextRot(newNextRot);
+	XMVECTOR newNextRot = XMQuaternionMultiply(actorToRotate->GetNextRot(),
+		DirectX::XMQuaternionRotationAxis(XMLoadFloat3(&rotateDirection),
+			XMConvertToRadians(rotationIncrement)));
+	actorToRotate->SetNextRot(newNextRot);
 }
 
 void IncrementalRotator::OnLinkRotateRight()
 {
-    XMVECTOR newNextRot = XMQuaternionMultiply(actorToRotate->GetNextRot(),
-        DirectX::XMQuaternionRotationAxis(XMLoadFloat3(&rotateDirection),
-            XMConvertToRadians(-rotationIncrement)));
-    actorToRotate->SetNextRot(newNextRot);
+	XMVECTOR newNextRot = XMQuaternionMultiply(actorToRotate->GetNextRot(),
+		DirectX::XMQuaternionRotationAxis(XMLoadFloat3(&rotateDirection),
+			XMConvertToRadians(-rotationIncrement)));
+	actorToRotate->SetNextRot(newNextRot);
 }
