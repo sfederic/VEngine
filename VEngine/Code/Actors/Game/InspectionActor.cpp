c0691bf45b507d7b50de688a8daf8b11d44e21b3
sfederic <furshazzle@gmail.com>
2023-09-12 22:25:20 +1000

    Removed player camera zoom in on InspectionActor (moved to GridActor)

--- VEngine/Code/Actors/Game/InspectionActor.cpp
+++ VEngine/Code/Actors/Game/InspectionActor.cpp
@@ -1,16 +1,14 @@
 #include "vpch.h"
 #include "InspectionActor.h"
-#include "Player.h"
 
 void InspectionActor::Create()
 {
+	__super::Create();
+
 	isInspectable = true;
 }
 
 void InspectionActor::Inspect()
 {
-	auto player = Player::system.GetOnlyActor();
-	player->SetCameraTargetActor(this);
-	auto nextCameraPos = XMVectorSet(0.f, 0.4f, 0.f, 1.f);
-	player->SetNextCameraPosition(nextCameraPos);
+	__super::Inspect();
 }
