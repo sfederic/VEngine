e5e612db33e8bb46e86f35a2ab7680ecc4eef5cd
sfederic <furshazzle@gmail.com>
2023-09-13 22:52:30 +1000

    Round vectors before checking orientation against directions for OrientationLock

--- VEngine/Code/Actors/Game/OrientationLock.cpp
+++ VEngine/Code/Actors/Game/OrientationLock.cpp
@@ -5,73 +5,77 @@
 
 void OrientationLock::Start()
 {
-    __super::Start();
+	__super::Start();
 
-    auto actor = World::GetActorByNameAllowNull(gridActorToActivateOnCorrectOrientation);
-    if (actor)
-    {
-        linkedGridActor = dynamic_cast<GridActor*>(actor);
-        if (!linkedGridActor)
-        {
-            Log("Linked actor [%s] for [%s] isn't derived from GridActor.",
-                gridActorToActivateOnCorrectOrientation.c_str(), GetName().c_str());
-        }
-    }
-    else
-    {
-        Log("Linked actor [%s] for [%s] not found.",
-            gridActorToActivateOnCorrectOrientation.c_str(), GetName().c_str());
-    }
+	auto actor = World::GetActorByNameAllowNull(gridActorToActivateOnCorrectOrientation);
+	if (actor)
+	{
+		linkedGridActor = dynamic_cast<GridActor*>(actor);
+		if (!linkedGridActor)
+		{
+			Log("Linked actor [%s] for [%s] isn't derived from GridActor.",
+				gridActorToActivateOnCorrectOrientation.c_str(), GetName().c_str());
+		}
+	}
+	else
+	{
+		Log("Linked actor [%s] for [%s] not found.",
+			gridActorToActivateOnCorrectOrientation.c_str(), GetName().c_str());
+	}
 }
 
 void OrientationLock::Tick(float deltaTime)
 {
-    __super::Tick(deltaTime);
+	__super::Tick(deltaTime);
 
-    if (linkedGridActor)
-    {
-        if (IsOrientationCorrect())
-        {
-            linkedGridActor->Activate();
-        }
-        else
-        {
-            linkedGridActor->Deactivate();
-        }
-    }
+	if (linkedGridActor)
+	{
+		if (IsOrientationCorrect())
+		{
+			linkedGridActor->Activate();
+		}
+		else
+		{
+			linkedGridActor->Deactivate();
+		}
+	}
 }
 
 Properties OrientationLock::GetProps()
 {
-    auto props = __super::GetProps();
-    props.title = GetTypeName();
-    props.Add("Dir Forward", &forward);
-    props.Add("Dir Up", &up);
-    props.Add("Dir Right", &right);
-    props.Add("Linked GridActor", &gridActorToActivateOnCorrectOrientation);
-    return props;
+	auto props = __super::GetProps();
+	props.title = GetTypeName();
+	props.Add("Dir Forward", &forward);
+	props.Add("Dir Up", &up);
+	props.Add("Dir Right", &right);
+	props.Add("Linked GridActor", &gridActorToActivateOnCorrectOrientation);
+	return props;
 }
 
 bool OrientationLock::IsOrientationCorrect()
 {
-    auto forwardVector = GetForwardVectorV();
-    auto upVector = GetUpVectorV();
-    auto rightVector = GetRightVectorV();
+	auto forwardVector = GetForwardVectorV();
+	auto upVector = GetUpVectorV();
+	auto rightVector = GetRightVectorV();
 
-    auto dirForward = XMLoadFloat3(&forward);
-    auto dirUp = XMLoadFloat3(&up);
-    auto dirRight = XMLoadFloat3(&right);
+	VMath::RoundVector(forwardVector);
+	VMath::RoundVector(upVector);
+	VMath::RoundVector(rightVector);
 
-    int neededOrientations = 3;
+	auto dirForward = XMLoadFloat3(&forward);
+	auto dirUp = XMLoadFloat3(&up);
+	auto dirRight = XMLoadFloat3(&right);
 
-    if (VMath::VecEqual(XMVectorZero(), dirForward)) { neededOrientations--; }
-    if (VMath::VecEqual(XMVectorZero(), dirRight)) { neededOrientations--; }
-    if (VMath::VecEqual(XMVectorZero(), dirUp)) { neededOrientations--; }
+	int neededOrientations = 3;
 
-    bool forwardEqual = VMath::VecEqual(forwardVector, dirForward);
-    bool rightEqual = VMath::VecEqual(rightVector, dirRight);
-    bool upEqual = VMath::VecEqual(upVector, dirUp);
+	if (VMath::VecEqual(XMVectorZero(), dirForward)) { neededOrientations--; }
+	if (VMath::VecEqual(XMVectorZero(), dirRight)) { neededOrientations--; }
+	if (VMath::VecEqual(XMVectorZero(), dirUp)) { neededOrientations--; }
 
-    int finalMatchingOrientations = forwardEqual + rightEqual + upEqual;
-    return finalMatchingOrientations == neededOrientations;
+	bool forwardEqual = VMath::VecEqual(forwardVector, dirForward);
+	bool rightEqual = VMath::VecEqual(rightVector, dirRight);
+	bool upEqual = VMath::VecEqual(upVector, dirUp);
+
+	int finalMatchingOrientations = forwardEqual + rightEqual + upEqual;
+	return finalMatchingOrientations == neededOrientations;
 }
