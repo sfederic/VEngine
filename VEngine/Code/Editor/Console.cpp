1a45610cd10d6e9db21021936ce6d20b89a6a82e
sfederic <furshazzle@gmail.com>
2023-09-13 22:20:17 +1000

    Static debug notification for actor replace console command

--- VEngine/Code/Editor/Console.cpp
+++ VEngine/Code/Editor/Console.cpp
@@ -28,47 +28,47 @@ void Console::Init()
 
 	executeMap.emplace(L"LS",
 		std::make_pair([]() { debugMenu.consoleCommandsMenuOpen = !debugMenu.consoleCommandsMenuOpen; },
-		"List all console commands"));
+			"List all console commands"));
 
 	executeMap.emplace(L"PROF",
 		std::make_pair([]() { debugMenu.profileMenuOpen = !debugMenu.profileMenuOpen; },
-		"Show profile stats"));
+			"Show profile stats"));
 
 	executeMap.emplace(L"FPS",
 		std::make_pair([]() { debugMenu.fpsMenuOpen = !debugMenu.fpsMenuOpen; },
-		"Show FPS and GPU timing info"));
+			"Show FPS and GPU timing info"));
 
 	executeMap.emplace(L"GPU",
 		std::make_pair([]() { debugMenu.gpuMenuOpen = !debugMenu.gpuMenuOpen; },
-		"Show GPU info"));
+			"Show GPU info"));
 
 	executeMap.emplace(L"ACTOR",
 		std::make_pair([]() { debugMenu.actorInspectMenuOpen = !debugMenu.actorInspectMenuOpen; },
-		"Shows actor info while hovering over the actor with mouse"));
+			"Shows actor info while hovering over the actor with mouse"));
 
 	executeMap.emplace(L"ACTORSYSTEM",
 		std::make_pair([]() { debugMenu.actorSystemMenuOpen = !debugMenu.actorSystemMenuOpen; },
-		"Show actor system stats"));
+			"Show actor system stats"));
 
 	executeMap.emplace(L"COMPONENTSYSTEM",
 		std::make_pair([]() { debugMenu.componentSystemMenuOpen = !debugMenu.componentSystemMenuOpen; },
-		"Show component system stats"));
+			"Show component system stats"));
 
 	executeMap.emplace(L"STATS",
 		std::make_pair([]() { debugMenu.worldStatsMenuOpen = !debugMenu.worldStatsMenuOpen; },
-		"shows in - world stats (e.g.vertex count, actor count)"));
+			"shows in - world stats (e.g.vertex count, actor count)"));
 
 	executeMap.emplace(L"GAME",
 		std::make_pair([]() { debugMenu.gameInstaceMenuOpen = !debugMenu.gameInstaceMenuOpen; },
-		"Menu for manipulating game instance data"));
+			"Menu for manipulating game instance data"));
 
 	executeMap.emplace(L"PARTICLE",
 		std::make_pair([]() { debugMenu.particleMenuOpen = !debugMenu.particleMenuOpen; },
-		"Particle"));
+			"Particle"));
 
 	executeMap.emplace(L"SKEL",
 		std::make_pair([]() { debugMenu.skeletonViewMenuOpen = !debugMenu.skeletonViewMenuOpen; },
-		"Show skeleton heirarchy on actor's meshcomponent"));
+			"Show skeleton heirarchy on actor's meshcomponent"));
 
 	executeMap.emplace(L"ANIM",
 		std::make_pair([]() { debugMenu.skeletalAnimationMenuOpen = !debugMenu.skeletalAnimationMenuOpen; },
@@ -76,23 +76,23 @@ void Console::Init()
 
 	executeMap.emplace(L"CORE",
 		std::make_pair([]() { debugMenu.coreMenuOpen = !debugMenu.coreMenuOpen; },
-		"Show core engine variables"));
+			"Show core engine variables"));
 
 	executeMap.emplace(L"BAKE",
 		std::make_pair([]() { Renderer::RenderLightProbeViews(); },
-		"Work through light probes in map and get their RBG values from a cubemap rendering"));
+			"Work through light probes in map and get their RBG values from a cubemap rendering"));
 
 	executeMap.emplace(L"BIN",
 		std::make_pair([]() { FileSystem::WriteAllSystemsToBinary(); },
-		"Save current world to binary format"));
+			"Save current world to binary format"));
 
 	executeMap.emplace(L"LOADBIN",
 		std::make_pair([]() { FileSystem::ReadAllSystemsFromBinary(); },
-		"Load current world from existing binary file"));
+			"Load current world from existing binary file"));
 
 	executeMap.emplace(L"BUILD MESH",
 		std::make_pair([]() { AssetSystem::BuildAllVMeshDataFromFBXImport(); },
-		"Build meshes as their engine specific file format."));
+			"Build meshes as their engine specific file format."));
 
 	executeMap.emplace(L"BUILD ANIM",
 		std::make_pair([]() { AssetSystem::BuildAllAnimationFilesFromFBXImport(); },
@@ -100,15 +100,15 @@ void Console::Init()
 
 	executeMap.emplace(L"BUILD MAPS",
 		std::make_pair([]() { AssetSystem::BuildAllGameplayMapFiles(); },
-		"Write all game save maps."));
+			"Write all game save maps."));
 
 	executeMap.emplace(L"DEFAULT",
 		std::make_pair([]() { World::CreateDefaultMapActors(); },
-		"Load in default actors for most worlds (Player, Grid, DirectionalLight, etc.)"));
+			"Load in default actors for most worlds (Player, Grid, DirectionalLight, etc.)"));
 
 	executeMap.emplace(L"TEXTURE",
 		std::make_pair([]() { WorldEditor::texturePlacement = !WorldEditor::texturePlacement; },
-		"Enable texture placement mode in editor"));
+			"Enable texture placement mode in editor"));
 
 	executeMap.emplace(L"ENT",
 		std::make_pair([]() {
@@ -116,16 +116,16 @@ void Console::Init()
 			WorldEditor::entranceTriggerWorldLoadMode ?
 				debugMenu.AddStaticNotification(L"EntranceTrigger Load Mode Active") :
 				debugMenu.ClearAllStaticNotifications();
-		},
-		"Enable/Disable loading maps based on clicking an EntranceTrigger in-world."));
+			},
+			"Enable/Disable loading maps based on clicking an EntranceTrigger in-world."));
 
 	executeMap.emplace(L"MESH",
-		std::make_pair([]() { 
-			WorldEditor::meshPlacement = !WorldEditor::meshPlacement; 
+		std::make_pair([]() {
+			WorldEditor::meshPlacement = !WorldEditor::meshPlacement;
 			WorldEditor::meshPlacement ? debugMenu.AddStaticNotification(L"MeshReplace Mode Active") :
 				debugMenu.ClearAllStaticNotifications();
 			},
-		"Toggle mesh component placement mode in editor."));
+			"Toggle mesh component placement mode in editor."));
 
 	executeMap.emplace(L"PARENT",
 		std::make_pair([]() {
@@ -133,11 +133,11 @@ void Console::Init()
 			WorldEditor::parentSetActive ? debugMenu.AddStaticNotification(L"ParentSet Mode Active") :
 				debugMenu.ClearAllStaticNotifications();
 			},
-		"Enable setting parent to currently selected actor in editor."));
+			"Enable setting parent to currently selected actor in editor."));
 
 	executeMap.emplace(L"MATERIAL",
 		std::make_pair([]() { WorldEditor::materialPlacement = !WorldEditor::materialPlacement; },
-		"Enable material placement mode in editor"));
+			"Enable material placement mode in editor"));
 
 	executeMap.emplace(L"UV",
 		std::make_pair([]() { debugMenu.uvMenuOpen = !debugMenu.uvMenuOpen; },
@@ -145,17 +145,17 @@ void Console::Init()
 
 	executeMap.emplace(L"MS",
 		std::make_pair([]() { Renderer::MeshIconImageCapture(); },
-		"Take an editor icon image of the currently selected mesh (MS stands for Mesh Snap, like Pokemon 'Snap')"));
+			"Take an editor icon image of the currently selected mesh (MS stands for Mesh Snap, like Pokemon 'Snap')"));
 
 	executeMap.emplace(L"WORLD",
 		std::make_pair([]() { debugMenu.worldMenuOpen = !debugMenu.worldMenuOpen; },
-			"Menu to open worlds at runtime during gameplay."));	
-	
+			"Menu to open worlds at runtime during gameplay."));
+
 	executeMap.emplace(L"REP",
-		std::make_pair([]() { 
+		std::make_pair([]() {
 			WorldEditor::actorReplaceModeActive = !WorldEditor::actorReplaceModeActive;
-			WorldEditor::actorReplaceModeActive ? Log("Actor replace mode active.") :
-				Log("Actor replace mode inactive.");
+			WorldEditor::actorReplaceModeActive ? debugMenu.AddStaticNotification(L"Actor Replace Mode Active") :
+				debugMenu.ClearAllStaticNotifications();
 			},
 			"Replace clicked actor in world with the currently selected actor system."));
 
@@ -163,34 +163,34 @@ void Console::Init()
 		std::make_pair([]() {
 			WorldEditor::moveActorViaKeyboardInput = !WorldEditor::moveActorViaKeyboardInput;
 			WorldEditor::actorReplaceModeActive ? debugMenu.AddStaticNotification(L"ActorKeyboardMove Mode Active") :
-				debugMenu.ClearAllStaticNotifications();;
+				debugMenu.ClearAllStaticNotifications();
 			},
 			"Set World Editor to move selected actor via keyboard input."));
 
 	executeMap.emplace(L"PAINT",
-		std::make_pair([]() { 
+		std::make_pair([]() {
 			debugMenu.vertexPaintMenuOpen = !debugMenu.vertexPaintMenuOpen;
 			WorldEditor::vertexPaintActive = !WorldEditor::vertexPaintActive;
-		}, "Activate vertex paint mode."));
+			}, "Activate vertex paint mode."));
 
 	executeMap.emplace(L"RESET UID",
-		std::make_pair([]() { 
-				ResetUIDCache();
-				auto components = World::GetAllComponentsInWorld();
-				for (auto component : components)
-				{
-					component->SetUID(GenerateUID());
-				}
-				Log("Reset UIDs of %d components.", components.size());
+		std::make_pair([]() {
+			ResetUIDCache();
+			auto components = World::GetAllComponentsInWorld();
+			for (auto component : components)
+			{
+				component->SetUID(GenerateUID());
+			}
+			Log("Reset UIDs of %d components.", components.size());
 			},
 			"Resets all component UIDs in world."));
 
 	executeMap.emplace(L"NO TEXTURE",
 		std::make_pair([]() {
-				for (auto& mesh : MeshComponent::system.GetComponents())
-				{
-					mesh->SetUseTexture(false);
-				}
+			for (auto& mesh : MeshComponent::system.GetComponents())
+			{
+				mesh->SetUseTexture(false);
+			}
 			}, "Sets all materials in world to NOT use texture via material shader data."));
 
 	executeMap.emplace(L"USE TEXTURE",
