a947f9fe5255e6342013daa5aea87c2bb659c020
sfederic <furshazzle@gmail.com>
2023-10-19 22:19:54 +1100

    Mystery ChurchLockPropActivator todo

--- VEngine/Code/Actors/Game/ChurchLockPropActivator.h
+++ VEngine/Code/Actors/Game/ChurchLockPropActivator.h
@@ -2,6 +2,9 @@
 
 #include "ActivateOnGlobalProp.h"
 
+//@Todo: there's something wrong with this class/file. It always pops up in strange build errors
+//like changing to Release or precompiled header issues. I wonder what it is
+
 class ChurchLockPropActivator : public ActivateOnGlobalProp
 {
 public:
