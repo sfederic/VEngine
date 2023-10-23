39af9b67eb0e88a1374047b27c0b9ba276bf0363
sfederic <furshazzle@gmail.com>
2023-10-23 23:36:00 +1100

    Spawn sprite sheet on Crushable crush

--- VEngine/Code/Actors/Game/Crushable.cpp
+++ VEngine/Code/Actors/Game/Crushable.cpp
@@ -1,8 +1,11 @@
 #include "vpch.h"
 #include "Crushable.h"
+#include "Gameplay/GameUtils.h"
 
 void Crushable::Crushed()
 {
+	GameUtils::SpawnSpriteSheet("Sprites/dust.png", GetPositionV(), false, 3, 2, 3.f);
+
 	Destroy();
 }
 
