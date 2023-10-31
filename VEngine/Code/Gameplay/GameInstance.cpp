30f92141a882d784ab358861db5c60ac09a32567
sfederic <furshazzle@gmail.com>
2023-10-31 20:54:08 +1100

    Vertex colour data Misc. map changes

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -1,7 +1,7 @@
 #include "vpch.h"
 #include "GameInstance.h"
 
-std::string GameInstance::startingMap = "xenogears_room_2.vmap";
+std::string GameInstance::startingMap = "vagrant_story_test.vmap";
 std::string GameInstance::previousMapMovedFrom = startingMap;
 
 bool GameInstance::useGameSaves = false;
