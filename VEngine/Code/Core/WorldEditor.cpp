1c1324f588c4073e46ef8e8339c1e71c557086ec
sfederic <furshazzle@gmail.com>
2023-12-15 14:54:43 +1100

    Fix on swapping left and down for uv mapping (note that this orientation is based on the engine's global axis with Z being forward)

--- VEngine/Code/Core/WorldEditor.cpp
+++ VEngine/Code/Core/WorldEditor.cpp
@@ -504,12 +504,12 @@ void UVPainting()
 							const float u0 = WorldEditor::uvPaintData.x / textureWidth;
 							const float v0 = WorldEditor::uvPaintData.y / textureHeight;
 							const float u1 = WorldEditor::uvPaintData.w / textureWidth;
-							const float v1 = WorldEditor::uvPaintData.h / textureHeight;
-							const float u2 = WorldEditor::uvPaintData.x / textureWidth;
+							const float v1 = WorldEditor::uvPaintData.y / textureHeight;
+							const float u2 = WorldEditor::uvPaintData.w / textureWidth;
 							const float v2 = WorldEditor::uvPaintData.h / textureHeight;
 							*newUVs[0] = XMFLOAT2(u0, v0);
-							*newUVs[1] = XMFLOAT2(u1, v1);
-							*newUVs[2] = XMFLOAT2(u2, v2);
+							*newUVs[2] = XMFLOAT2(u1, v1);
+							*newUVs[1] = XMFLOAT2(u2, v2);
 							break;
 						}
 						case UVPaintRotate::Right:
@@ -543,12 +543,12 @@ void UVPainting()
 							const float u0 = WorldEditor::uvPaintData.x / textureWidth;
 							const float v0 = WorldEditor::uvPaintData.y / textureHeight;
 							const float u1 = WorldEditor::uvPaintData.w / textureWidth;
-							const float v1 = WorldEditor::uvPaintData.y / textureHeight;
-							const float u2 = WorldEditor::uvPaintData.w / textureWidth;
+							const float v1 = WorldEditor::uvPaintData.h / textureHeight;
+							const float u2 = WorldEditor::uvPaintData.x / textureWidth;
 							const float v2 = WorldEditor::uvPaintData.h / textureHeight;
 							*newUVs[0] = XMFLOAT2(u0, v0);
-							*newUVs[2] = XMFLOAT2(u1, v1);
-							*newUVs[1] = XMFLOAT2(u2, v2);
+							*newUVs[1] = XMFLOAT2(u1, v1);
+							*newUVs[2] = XMFLOAT2(u2, v2);
 							break;
 						}
 						}
@@ -562,12 +562,12 @@ void UVPainting()
 							const float u0 = WorldEditor::uvPaintData.x / textureWidth;
 							const float v0 = WorldEditor::uvPaintData.y / textureHeight;
 							const float u1 = WorldEditor::uvPaintData.w / textureWidth;
-							const float v1 = WorldEditor::uvPaintData.y / textureHeight;
-							const float u2 = WorldEditor::uvPaintData.w / textureWidth;
+							const float v1 = WorldEditor::uvPaintData.h / textureHeight;
+							const float u2 = WorldEditor::uvPaintData.x / textureWidth;
 							const float v2 = WorldEditor::uvPaintData.h / textureHeight;
 							*newUVs[0] = XMFLOAT2(u0, v0);
-							*newUVs[1] = XMFLOAT2(u1, v1);
-							*newUVs[2] = XMFLOAT2(u2, v2);
+							*newUVs[2] = XMFLOAT2(u1, v1);
+							*newUVs[1] = XMFLOAT2(u2, v2);
 							break;
 						}
 						case UVPaintRotate::Right:
@@ -601,12 +601,12 @@ void UVPainting()
 							const float u0 = WorldEditor::uvPaintData.x / textureWidth;
 							const float v0 = WorldEditor::uvPaintData.y / textureHeight;
 							const float u1 = WorldEditor::uvPaintData.w / textureWidth;
-							const float v1 = WorldEditor::uvPaintData.h / textureHeight;
-							const float u2 = WorldEditor::uvPaintData.x / textureWidth;
+							const float v1 = WorldEditor::uvPaintData.y / textureHeight;
+							const float u2 = WorldEditor::uvPaintData.w / textureWidth;
 							const float v2 = WorldEditor::uvPaintData.h / textureHeight;
 							*newUVs[0] = XMFLOAT2(u0, v0);
-							*newUVs[2] = XMFLOAT2(u1, v1);
-							*newUVs[1] = XMFLOAT2(u2, v2);
+							*newUVs[1] = XMFLOAT2(u1, v1);
+							*newUVs[2] = XMFLOAT2(u2, v2);
 							break;
 						}
 						}
