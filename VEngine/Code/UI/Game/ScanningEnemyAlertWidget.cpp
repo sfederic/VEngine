4e20ec61008a0d6e93491045c36c14521c74247d
sfederic <furshazzle@gmail.com>
2023-12-23 20:30:45 +1100

    Create ScanningEnemyAlertWidget

---
+++ VEngine/Code/UI/Game/ScanningEnemyAlertWidget.cpp
@@ -0,0 +1,9 @@
+#include "vpch.h"
+#include "ScanningEnemyAlertWidget.h"
+
+void ScanningEnemyAlertWidget::Draw(float deltaTime)
+{
+	auto layout = CenterLayoutOnScreenSpaceCoords(35.f, 55.f);
+	layout.AddVerticalSpace(-75.f);
+	Image("alert.png", layout);
+}
---
+++ VEngine/Code/UI/Game/ScanningEnemyAlertWidget.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../Widget.h"
+
+class ScanningEnemyAlertWidget : public Widget
+{
+public:
+	void Draw(float deltaTime) override;
+};
