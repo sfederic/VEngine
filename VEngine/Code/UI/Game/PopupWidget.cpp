77b619e648be6873dfa8855bcee907e60a51633b
sfederic <furshazzle@gmail.com>
2024-02-13 23:08:08 +1100

    Removed Core functions from PopupWidget to pause game world

--- VEngine/Code/UI/Game/PopupWidget.cpp
+++ VEngine/Code/UI/Game/PopupWidget.cpp
@@ -1,25 +1,9 @@
 #include "vpch.h"
 #include "PopupWidget.h"
-#include "Core/Input.h"
-#include "Core/Core.h"
 
 void PopupWidget::Draw(float deltaTime)
 {
-	Core::PauseGameWorld();
-
 	auto layout = PercentAlignLayout(0.3f, 0.3f, 0.7f, 0.7f);
-
 	FillRect(layout);
-
 	Text(popupText, layout);
-
-	layout.AddVerticalSpace(75.f);
-	Text(L"Push Down to Continue...", layout);
-
-	if (Input::GetKeyUp(Keys::Down))
-	{
-		Core::UnPauseGameWorld();
-
-		RemoveFromViewport();
-	}
 }
