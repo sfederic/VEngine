32cdcf6e60c38c7bb0762486c2614b765fbb8669
sfederic <furshazzle@gmail.com>
2024-02-13 23:31:54 +1100

    Early exit and empty display for JournalWidget if no entries exist

--- VEngine/Code/UI/Game/JournalWidget.cpp
+++ VEngine/Code/UI/Game/JournalWidget.cpp
@@ -12,13 +12,27 @@ void JournalWidget::Draw(float deltaTime)
 
 void JournalWidget::DrawJournalEntriesToGridLayout()
 {
+	auto& journalEntries = JournalSystem::Get().GetJournalEntries();
+
+	if (journalEntries.size() == 0)
+	{
+		//Nothing to display
+		const auto emptyLayout = PercentAlignLayout(0.4f, 0.4f, 0.6f, 0.6f);
+		FillRect(emptyLayout);
+		Text("Journal empty.", emptyLayout);
+		return;
+	}
+
 	if (selectedJournalEntry != nullptr)
 	{
 		auto entryLayout = PercentAlignLayout(0.3f, 0.3f, 0.7f, 0.7f);
+
 		FillRect(entryLayout);
 		Text(selectedJournalEntry->title, entryLayout);
+
 		entryLayout.AddVerticalSpace(30.f);
 		Text(selectedJournalEntry->text, entryLayout);
+
 		entryLayout.AddVerticalSpace(30.f);
 		if (Button("Close", entryLayout))
 		{
@@ -29,22 +43,25 @@ void JournalWidget::DrawJournalEntriesToGridLayout()
 	}
 
 	const auto gridOverallLayout = PercentAlignLayout(0.1f, 0.1f, 0.9f, 0.9f);
+
 	const int numRows = 3;
 	const int numColumns = 3;
+
 	GridLayout gridLayout;
 	gridLayout.SetLayouts(numRows, numColumns, gridOverallLayout, 20.f);
 
 	const std::vector<Layout> gridLayouts = gridLayout.GetAllLayouts();
 	int gridLayoutIndex = 0;
 
-	auto& journalEntries = JournalSystem::Get().GetJournalEntries();
 	for (auto& [title, entry] : journalEntries)
 	{
 		const auto& layout = gridLayouts.at(gridLayoutIndex);
+
 		if (Button(title, layout))
 		{
 			selectedJournalEntry = &entry;
 		}
+
 		gridLayoutIndex++;
 	}
 }
