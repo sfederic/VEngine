b1c863f0d56ba7d9675069df7be62097937a0531
sfederic <furshazzle@gmail.com>
2024-03-08 23:22:47 +1100

    Misc. commit

--- VEngine/Code/Gameplay/GameInstance.cpp
+++ VEngine/Code/Gameplay/GameInstance.cpp
@@ -1,7 +1,7 @@
 #include "vpch.h"
 #include "GameInstance.h"
 
-std::string GameInstance::startingMap = "big_docks.vmap";
+std::string GameInstance::startingMap = "test.vmap";
 std::string GameInstance::previousMapMovedFrom = startingMap;
 
 bool GameInstance::useGameSaves = false;
