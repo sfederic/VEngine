1431e74f5b84490d78b445244bb864f3df59aedb
sfederic <furshazzle@gmail.com>
2024-03-29 11:54:26 +1100

    FuseGridActor header comment

--- VEngine/Code/Actors/Game/FuseGridActor.h
+++ VEngine/Code/Actors/Game/FuseGridActor.h
@@ -4,6 +4,8 @@
 
 //Grid actor that fuses onto another grid actor. Think like Tears of the Kingdom's fuse ability.
 //FuseGridActors will probably have to be cubic to work best.
+//Note that previously, the code to check grid actor next rotations was trying to work with FuseGridActors
+//having multiple meshes. It didn't work well, so keep this actor functioning like how ProjectionCrystal does.
 class FuseGridActor : public GridActor
 {
 public:
