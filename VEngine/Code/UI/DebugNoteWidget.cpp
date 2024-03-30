da38968b2bf4f78c81913ea3dea678382161d794
sfederic <furshazzle@gmail.com>
2024-03-30 13:11:11 +1100

    Remove debug ifdef from DebugNoteWidget

--- VEngine/Code/UI/DebugNoteWidget.cpp
+++ VEngine/Code/UI/DebugNoteWidget.cpp
@@ -3,10 +3,8 @@
 
 void DebugNoteWidget::Draw(float deltaTime)
 {
-#ifdef _DEBUG
 	Layout layout = CenterLayoutOnScreenSpaceCoords(175.f, 75.f);
 
-	FillRect(layout, {0.5f, 0.5f, 0.5f, 0.5f}, 0.5f);
+	FillRect(layout, { 0.5f, 0.5f, 0.5f, 0.5f }, 0.5f);
 	Text(noteText, layout);
-#endif
 }
