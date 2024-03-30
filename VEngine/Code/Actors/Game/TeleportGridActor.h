4e85978b94c4fbc1efd1569ed2995605db090101
sfederic <furshazzle@gmail.com>
2024-03-30 11:56:06 +1100

    Add to TeleportGridActor header comment

--- VEngine/Code/Actors/Game/TeleportGridActor.h
+++ VEngine/Code/Actors/Game/TeleportGridActor.h
@@ -4,6 +4,9 @@
 
 //Actor that 'teleports' to the furthest node it can on the axis it's moved. Think like when you
 //do a quick place in Tetris.
+// 
+//The code here is only going to work with grid aligned actors. If you do use this actor,
+//make sure the actors in the world are cubic.
 class TeleportGridActor : public GridActor
 {
 public:
