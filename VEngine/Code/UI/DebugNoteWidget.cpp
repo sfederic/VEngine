634dfc9e96f7d09e6bc7173e28b96d48c2321bf1
sfederic <furshazzle@gmail.com>
2024-04-01 11:57:40 +1100

    Made DebugNoteWidget transparency little higher

--- VEngine/Code/UI/DebugNoteWidget.cpp
+++ VEngine/Code/UI/DebugNoteWidget.cpp
@@ -4,6 +4,6 @@
 void DebugNoteWidget::Draw(float deltaTime)
 {
 	const auto layout = CenterLayoutOnScreenSpaceCoords(175.f, 75.f);
-	FillRect(layout);
+	FillRect(layout, { 0.75f, 0.75f, 0.75f, 0.75f }, 0.75f);
 	Text(noteText, layout);
 }
