69d7abc56c35b3b45b4a423b26b09d036409a050
sfederic <furshazzle@gmail.com>
2024-04-08 22:03:25 +1000

    Change PivotDoor mesh local offset and scale

--- VEngine/Code/Actors/Game/PivotDoor.cpp
+++ VEngine/Code/Actors/Game/PivotDoor.cpp
@@ -13,7 +13,8 @@ void PivotDoor::Create()
 {
 	__super::Create();
 
-	mesh->SetLocalPosition(-1.f, 0.f, 0.f);
+	mesh->SetLocalPosition(0.5f, 0.25f, 0.f);
+	mesh->SetLocalScale(0.5f, 0.75f, 1.f);
 	mesh->SetMeshFilename("plane.vmesh");
 	mesh->SetRastState(RastStates::noBackCull);
 }
