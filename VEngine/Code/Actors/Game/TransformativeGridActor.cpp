85f97972086b8275a6e8065d9f04eba08f5661be
sfederic <furshazzle@gmail.com>
2024-04-21 12:10:13 +1000

    Removed scale up/down visual for TransformativeGridActor

--- VEngine/Code/Actors/Game/TransformativeGridActor.cpp
+++ VEngine/Code/Actors/Game/TransformativeGridActor.cpp
@@ -10,36 +10,6 @@ void TransformativeGridActor::Create()
 	mesh->SetMeshFilename(originalMeshFilename);
 }
 
-void TransformativeGridActor::Tick(float deltaTime)
-{
-	__super::Tick(deltaTime);
-
-	if (scalingDown)
-	{
-		auto scale = GetScaleV();
-		scale -= scale * deltaTime * 10.f;
-		SetScale(scale);
-
-		if (scale.m128_f32[0] <= 0.2f)
-		{
-			scalingDown = false;
-			scalingUp = true;
-		}
-	}
-
-	if (scalingUp)
-	{
-		auto scale = GetScaleV();
-		scale += scale * deltaTime * 10.f;
-		SetScale(scale);
-
-		if (scale.m128_f32[0] >= 0.95f)
-		{
-			scalingUp = false;
-		}
-	}
-}
-
 Properties TransformativeGridActor::GetProps()
 {
 	auto props = __super::GetProps();
@@ -53,8 +23,6 @@ void TransformativeGridActor::OnLinkMove()
 	__super::OnLinkMove();
 
 	ChangeMeshShape();
-
-	scalingDown = true;
 
 	//Player's link effect mesh needs to be reset to reflect the changes in this actor.
 	Player::system.GetOnlyActor()->SetLinkEffectMeshForTransformativeGridActors(*mesh);
--- VEngine/Code/Actors/Game/TransformativeGridActor.h
+++ VEngine/Code/Actors/Game/TransformativeGridActor.h
@@ -9,7 +9,6 @@ public:
 	ACTOR_SYSTEM(TransformativeGridActor);
 
 	void Create() override;
-	void Tick(float deltaTime) override;
 	Properties GetProps() override;
 
 	void OnLinkMove() override;
@@ -20,7 +19,4 @@ private:
 	std::string originalMeshFilename = "cube.vmesh";
 	std::string changeMeshFilename = "barrel.vmesh";
 	bool changeMeshActive = false;
-
-	bool scalingDown = false;
-	bool scalingUp = false;
 };
