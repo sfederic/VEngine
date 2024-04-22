6e9b86b599ff09d6ecdcbdbb2c46ef29f24d7516
sfederic <furshazzle@gmail.com>
2024-04-22 22:43:16 +1000

    Changed grid actor fall raycast distance. Actors that were falling were setting their current node too high.

--- VEngine/Code/Actors/Game/GridActor.cpp
+++ VEngine/Code/Actors/Game/GridActor.cpp
@@ -169,7 +169,7 @@ void GridActor::Tick(float deltaTime)
 	if (canFall)
 	{
 		HitResult hit(this);
-		if (!Physics::Raycast(hit, GetPositionV(), -VMath::GlobalUpVector(), 1.f))
+		if (!Physics::Raycast(hit, GetPositionV(), -VMath::GlobalUpVector(), 0.5f))
 		{
 			inFall = true;
 			constexpr float fallSpeed = 2.f;
