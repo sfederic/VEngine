c5743a7bd81210641bd3135385151e6762b5b01c
sfederic <furshazzle@gmail.com>
2024-04-27 14:49:29 +1000

    FuseGridActor todo on node position problem

--- VEngine/Code/Actors/Game/FuseGridActor.h
+++ VEngine/Code/Actors/Game/FuseGridActor.h
@@ -2,6 +2,9 @@
 
 #include "GridActor.h"
 
+//@Todo: there's a problem with this actor where when you initially fuse it, the new node created from the mesh
+//will have the same position as the initial node. Unselecting the grid actor from link fixes it though.
+
 //Grid actor that fuses onto another grid actor. Think like Tears of the Kingdom's fuse ability.
 //FuseGridActors will probably have to be cubic to work best.
 //Note that previously, the code to check grid actor next rotations was trying to work with FuseGridActors
