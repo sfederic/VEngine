3fa929122cc22bb48f97c43d3500f33a8f1f3496
sfederic <furshazzle@gmail.com>
2024-05-18 23:46:15 +1000

    SequenceEntryData testing virtuals for audio

--- VEngine/Code/Editor/Sequencer/SequenceEntryData.h
+++ VEngine/Code/Editor/Sequencer/SequenceEntryData.h
@@ -1,13 +1,27 @@
 #pragma once
 
 #include <string>
+#include "Gameplay/GameUtils.h"
+#include "Audio/AudioSystem.h"
 
 struct SequenceEntryData
 {
-	virtual void Init() {}
+	virtual void Activate() {}
+	virtual void Deactivate() {}
 };
 
 struct AudioSequenceEntryData : SequenceEntryData
 {
 	std::string audioFilename;
+
+	void Activate() override
+	{
+		GameUtils::PlayAudioOneShot(audioFilename);
+	}
+
+	void Deactivate() override
+	{
+		//@Todo: Just for testing. Remove.
+		AudioSystem::StopAllAudio();
+	}
 };
