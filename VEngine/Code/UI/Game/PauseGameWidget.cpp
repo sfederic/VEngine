f9ce3adecd0ab14647a755d954e6ad2a980dd045
sfederic <furshazzle@gmail.com>
2024-05-31 22:46:41 +1000

    Transparent rect background overlay onto pause widget

--- VEngine/Code/UI/Game/PauseGameWidget.cpp
+++ VEngine/Code/UI/Game/PauseGameWidget.cpp
@@ -4,8 +4,11 @@
 
 void PauseGameWidget::Draw(float deltaTime)
 {
-	const auto layout = PercentAlignLayout(0.4f, 0.4f, 0.6f, 0.6f);
-	Text(L"Paused", layout);
+	const auto textLayout = PercentAlignLayout(0.4f, 0.4f, 0.6f, 0.6f);
+	Text(L"Paused", textLayout);
+
+	const auto backLayout = PercentAlignLayout(0.f, 0.f, 1.f, 1.f);
+	FillRect(backLayout, { 0.5f, 0.5f, 0.5f, 1.f }, 0.5f);
 }
 
 void PauseGameWidget::ToggleGamePaused()
