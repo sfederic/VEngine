10e48e72b107e08ac27ff8b67f05336f1ca7d39b
sfederic <furshazzle@gmail.com>
2024-06-16 16:15:48 +1000

    Expanded on AudioBase cleanup todo

--- VEngine/Code/Audio/AudioBase.h
+++ VEngine/Code/Audio/AudioBase.h
@@ -8,6 +8,17 @@ struct AudioBase
 	WAVEFORMATEXTENSIBLE waveFormat = {};
 	XAUDIO2_BUFFER buffer = {};
 
-	AudioBase();
-	~AudioBase();
+	AudioBase() {}
+	~AudioBase()
+	{
+		//@Todo: there's a potential issue here with how vengine is seperating out AudioBase and AudioChannel.
+		//From the XAudio2 docs (https://learn.microsoft.com/en-us/windows/win32/api/xaudio2/ns-xaudio2-xaudio2_buffer#remarks),
+		//they're saying that, "The data the structure points to (pAudioData and pDecodedPacketCumulativeBytes, respectively)
+		//can't be freed until the buffer completes (as signaled by the IXAudio2VoiceCallback::OnBufferEnd callback)
+		// or the voice is stopped or destroyed." This might mean that you'll have to look again into the audio code
+		//and bring together both AudioBase and AudioChannel, that or have references to ensure that AudioBases are 
+		//deleted only when everything is unloaded, which is fine for small scale projects, but will blow up
+		//as soon as streaming is introduced.
+		delete buffer.pAudioData;
+	}
 };
