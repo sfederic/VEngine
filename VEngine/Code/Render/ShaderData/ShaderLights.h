d41c84da6ee86c3ba5cda89784c2502c0ba05cae
sfederic <furshazzle@gmail.com>
2024-06-21 22:17:19 +1000

    Global ambient comment

--- VEngine/Code/Render/ShaderData/ShaderLights.h
+++ VEngine/Code/Render/ShaderData/ShaderLights.h
@@ -6,7 +6,9 @@ struct ShaderLights
 {
 	static const int MAX_LIGHTS = 32;
 
+	//The global ambient is also based on 1 directional light in the world.
 	DirectX::XMFLOAT4 globalAmbient = DirectX::XMFLOAT4(0.5f, 0.5f, 0.5f, 1.f);
+
 	int numLights = 0;
 
 	//Shadows for now are based on 1 directional light in the level. Set this to false to avoid the 
