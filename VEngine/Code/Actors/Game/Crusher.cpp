520f74174443e05a52f4a13f63f7a1d67f10e406
sfederic <furshazzle@gmail.com>
2024-06-21 23:37:25 +1000

    Simple Crusher actor movement lerping

--- VEngine/Code/Actors/Game/Crusher.cpp
+++ VEngine/Code/Actors/Game/Crusher.cpp
@@ -1,6 +1,7 @@
 #include "vpch.h"
 #include "Crusher.h"
 #include "Components/MeshComponent.h"
+#include "Core/VMath.h"
 
 Crusher::Crusher()
 {
@@ -12,14 +13,45 @@ void Crusher::Create()
 {
 	__super::Create();
 
+	mesh->SetMeshFilename("cube.vmesh");
+}
 
+void Crusher::Start()
+{
+	__super::Start();
+
+	originalPos = GetPositionV();
+	nextPos = originalPos + GetForwardVectorV();
 }
 
 void Crusher::Tick(float deltaTime)
 {
 	__super::Tick(deltaTime);
 
+	if (!retracted)
+	{
+		retractTimer += deltaTime;
 
+		if (retractTimer > 3.0f)
+		{
+			retractTimer = 0.0f;
+			retracted = true;
+		}
+
+		SetPosition(VMath::VectorConstantLerp(GetPositionV(), originalPos, deltaTime, 2.5f));
+	}
+	else
+	{
+		moveTimer += deltaTime;
+
+		if (moveTimer > 3.0f)
+		{
+			moveTimer = 0.0f;
+			retracted = false;
+		}
+
+		SetPosition(VMath::VectorConstantLerp(GetPositionV(), nextPos, deltaTime, 5.0f));
+	}
 }
 
 Properties Crusher::GetProps()
--- VEngine/Code/Actors/Game/Crusher.h
+++ VEngine/Code/Actors/Game/Crusher.h
@@ -5,7 +5,10 @@
 
 class MeshComponent;
 
-//Think like that Attack of the Clones scene in the factory.
+//Todo: actor needs more work with lerping movement values and actually registering that its crushed actors
+//via box/ray casts.
+
+//Think like that Attack of the Clones scene in the factory. Moves back and forth and crushes things.
 class Crusher : public Actor
 {
 public:
@@ -13,9 +16,15 @@ public:
 
 	Crusher();
 	void Create() override;
+	void Start() override;
 	void Tick(float deltaTime) override;
 	Properties GetProps() override;
 
 private:
+	XMVECTOR nextPos = XMVectorSet(0.f, 0.f, 0.f, 1.f);
+	XMVECTOR originalPos = XMVectorSet(0.f, 0.f, 0.f, 1.f);
 	MeshComponent* mesh = nullptr;
+	float moveTimer = 0.f;
+	float retractTimer = 0.f;
+	bool retracted = true;
 };
