754c1e979ecaede3eb5d89f162f9364a8574baaf
Cahntz <furshazzle@gmail.com>
2024-07-15 22:41:23 +1000

    Trim down UVPaintWidget todo into header comment. The todo was leftover thoughts from the initial implementation.

--- VEngine/Code/UI/UVPaintWidget.h
+++ VEngine/Code/UI/UVPaintWidget.h
@@ -4,6 +4,10 @@
 
 //Displays a reticle to select a segment over the displayed texture.
 //Links with WorldEditor to UV paint meshes.
+//Is based on works like:
+//https://jeiel.itch.io/sprytile
+//https://crocotile3d.com/
+//https://www.youtube.com/watch?v=vl1uGM-KmY8 (FFTactics)
 class UVPaintWidget : public Widget
 {
 public:
@@ -28,27 +32,3 @@ private:
 	float previousMouseX = 0.f;
 	float previousMouseY = 0.f;
 };
-
-//@Todo: One workflow idea these sort of PS1 JRPG titles have (or at least what I thought would be good)
-//is to be able to paint textures directly onto faces. Everything in the engine is triangulated but the idea 
-//is based on works like:
-//https://jeiel.itch.io/sprytile
-//https://crocotile3d.com/
-//What those two example are missing though is directly painting to existing faces instead of creating quads from
-//the selected area of the texture map.
-
-//What this tool would need is a picker that could be resized to various dimensions (e.g. 1x16, 4x4, 8x4, etc.)
-//with the texture map image loaded, and then able to map the selected triangles to the texture map picker.
-
-//The workflow currently in Blender isn't too bad, but this sort of tool is worth looking into down the line.
-//The hardest part would be trying to map two triangles and orient them into a single quad so that the texture
-//picker would line up. The output would have to remape the UVs to the selcted mesh's .vmesh file, which wouldn't 
-//show up in the original .fbx export which is a worry.
-
-//Note that this tool might be easier to go in ImGui instead of Qt. Maybe even Direct2D.
-//Try use the FFTactics https://www.youtube.com/watch?v=vl1uGM-KmY8 tools as a reference too.
-
-//IMPLEMENTATION NOTES AND IDEAS
-//Trying to think on how to do this, I'm thinking have this texture map reticle/picker be split into two triangles.
-//So the left click will asign the selected mesh face to the top left triangle, and the right click will assign the
-//bottom right. Should be able to rotate this sort of setup to offer more flexibility.
