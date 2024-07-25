63a127958ee1e891cf15fc7200276be25d5a09e5
Cahntz <furshazzle@gmail.com>
2024-07-25 22:44:55 +1000

    Fix SavePoint not saving out global props on setting map name to continue from

--- VEngine/Code/Actors/Game/SavePoint.cpp
+++ VEngine/Code/Actors/Game/SavePoint.cpp
@@ -27,6 +27,7 @@ void SavePoint::Tick(float deltaTime)
 		if (Input::GetKeyUp(Keys::Enter))
 		{
 			GameInstance::SetContinueMapName(World::worldFilename);
+			GameUtils::SaveGameInstanceData();
 			GameUtils::SaveGameWorldState();
 		}
 	}
