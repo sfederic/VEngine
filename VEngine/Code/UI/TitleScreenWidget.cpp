5133819c3ffaf3cc7ccbf092d2ba2084f38ade9b
Cahntz <furshazzle@gmail.com>
2024-07-25 22:40:22 +1000

    TitleScreenWidget fade in and fade out logic, as well as start and continue level loading timers

--- VEngine/Code/UI/TitleScreenWidget.cpp
+++ VEngine/Code/UI/TitleScreenWidget.cpp
@@ -1,8 +1,19 @@
 #include "vpch.h"
 #include "TitleScreenWidget.h"
-#include "Gameplay/GameUtils.h"
+#include "Core/Timer.h"
 #include "Gameplay/GameInstance.h"
+#include "Gameplay/GameUtils.h"
 #include "UI/Colours.h"
+#include "UI/ScreenFadeWidget.h"
+#include "UI/UISystem.h"
+
+void TitleScreenWidget::Start()
+{
+	__super::Start();
+
+	//Load the "map to continue from" string in.
+	GameUtils::LoadGameInstanceData();
+}
 
 void TitleScreenWidget::Draw(float deltaTime)
 {
@@ -10,9 +21,16 @@ void TitleScreenWidget::Draw(float deltaTime)
 
 	Text(L"Vagrant Tactics", AlignLayout(300.f, 100.f, Align::Center), TextAlign::Center, Colours::White);
 
+	if (!_isInteractable)
+	{
+		return;
+	}
+
 	if (Button(L"Begin", AlignLayout(100.f, 50.f, Align::BottomLeft), 1.f, TextAlign::Center, Colours::White))
 	{
-		GameUtils::LoadWorldDeferred("starting_dock.vmap");
+		_isInteractable = false;
+		SetToFadeOut();
+		Timer::SetTimer(2.f, std::bind(&TitleScreenWidget::MoveToStartingLevel, this));
 	}
 
 	const auto mapName = GameInstance::GetContinueMapName();
@@ -20,7 +38,25 @@ void TitleScreenWidget::Draw(float deltaTime)
 	if (Button(L"Continue", AlignLayout(100.f, 50.f, Align::BottomRight), 1.f, TextAlign::Center,
 		Colours::White, 1.f, continueButtonActive))
 	{
-		GameUtils::LoadGameInstanceData();
-		GameUtils::LoadWorldDeferred(mapName);
+		_isInteractable = false;
+		_continueMapName = mapName;
+		SetToFadeOut();
+		Timer::SetTimer(2.f, std::bind(&TitleScreenWidget::MoveToContinueLevel, this));
 	}
+}
+
+void TitleScreenWidget::SetToFadeOut()
+{
+	UISystem::screenFadeWidget->SetToFadeOut();
+	UISystem::screenFadeWidget->AddToViewport();
+}
+
+void TitleScreenWidget::MoveToStartingLevel()
+{
+	GameUtils::LoadWorldDeferred("starting_dock.vmap");
+}
+
+void TitleScreenWidget::MoveToContinueLevel()
+{
+	GameUtils::LoadWorldDeferred(_continueMapName);
 }
--- VEngine/Code/UI/TitleScreenWidget.h
+++ VEngine/Code/UI/TitleScreenWidget.h
@@ -1,9 +1,20 @@
 #pragma once
 
 #include "Widget.h"
+#include <string>
 
 class TitleScreenWidget : public Widget
 {
 public:
-	virtual void Draw(float deltaTime) override;
+	void Start() override;
+	void Draw(float deltaTime) override;
+
+private:
+	void SetToFadeOut();
+	void MoveToStartingLevel();
+	void MoveToContinueLevel();
+
+	std::string _continueMapName;
+
+	bool _isInteractable = true;
 };
