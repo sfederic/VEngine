227b936ec80a7e1a59520c97e9dd05a343bb9790
Cahntz <furshazzle@gmail.com>
2024-08-03 23:34:37 +1000

    Moved MeshComponent class structure around

--- VEngine/Code/Components/MeshComponent.h
+++ VEngine/Code/Components/MeshComponent.h
@@ -22,33 +22,6 @@ public:
 	static MeshComponent* GetDebugMesh(std::string name);
 	static std::vector<MeshComponent*> GetAllStaticMeshes();
 
-	MeshComponentData meshComponentData;
-
-	MeshDataProxy meshDataProxy;
-	VertexBuffer vertexBuffer;
-
-	//Cached indexes into the diffuse light probe map for static meshes, so that they're not constantly
-	//searching out their closest light probe to take data from.
-	int cachedLightProbeMapIndex = 0; //0 should be safe as a default, but it can cause problems.
-
-	//This is for meshes that are messing with transparent meshes given that they're sorted by distance on render.
-	//It's mostly used for floor and wall meshes where their position wouldn't line up with the space they're occupying.
-	bool alwaysSortLast = false;
-
-	bool castsShadow = true;
-
-	bool skipPhysicsCreation = false;
-
-	//Whether to make transparent when in between player and camera
-	bool transparentOcclude = false;
-
-	//For VagrantTactics. Tells the Grid that any nodes that are placed on this mesh are to become inactive.
-	bool gridObstacle = false;
-	bool ignoreGridRaycasts = false;
-	bool canPlayerTraverse = true;
-
-	bool canBeLinkedTo = true;
-
 	MeshComponent();
 	MeshComponent(const std::string filename_,
 		const std::string textureFilename_,
@@ -110,10 +83,39 @@ public:
 	void SetPhysicsStatic(bool value) { isPhysicsStatic = value; }
 	bool IsPhysicsStatic() const { return isPhysicsStatic; }
 
-private:
+public:
+	MeshComponentData meshComponentData;
+	MeshDataProxy meshDataProxy;
+
 	std::string collisionMeshFilename;
 
+	VertexBuffer vertexBuffer;
+
+private:
 	Material* material = nullptr;
+
+public:
+	//Cached indexes into the diffuse light probe map for static meshes, so that they're not constantly
+	//searching out their closest light probe to take data from.
+	int cachedLightProbeMapIndex = 0; //0 should be safe as a default, but it can cause problems.
+
+	//This is for meshes that are messing with transparent meshes given that they're sorted by distance on render.
+	//It's mostly used for floor and wall meshes where their position wouldn't line up with the space they're occupying.
+	bool alwaysSortLast = false;
+
+	bool castsShadow = true;
+
+	bool skipPhysicsCreation = false;
+
+	//Whether to make transparent when in between player and camera
+	bool transparentOcclude = false;
+
+	//For VagrantTactics. Tells the Grid that any nodes that are placed on this mesh are to become inactive.
+	bool gridObstacle = false;
+	bool ignoreGridRaycasts = false;
+	bool canPlayerTraverse = true;
+
+	bool canBeLinkedTo = true;
 
 	bool isRenderStatic = true;
 
