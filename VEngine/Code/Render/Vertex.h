48139e4e4b1ccdb55bf41004d0cce2273961d8c5
Cahntz <furshazzle@gmail.com>
2024-08-11 11:06:10 +1000

    Vertex todo on splitting up bone data across the codebase

--- VEngine/Code/Render/Vertex.h
+++ VEngine/Code/Render/Vertex.h
@@ -9,6 +9,10 @@ struct Vertex
 	DirectX::XMFLOAT3 normal = DirectX::XMFLOAT3(0.f, 0.f, 0.f);
 	DirectX::XMFLOAT3 tangent = DirectX::XMFLOAT3(0.f, 0.f, 0.f);
 	DirectX::XMFLOAT2 uv = DirectX::XMFLOAT2(0.f, 0.f);
+
+	//Todo: Vertex should be split up into Vertex and BoneVertex. Having the bone indices and weights adds too memory.
+	//VertexShader::CreateInputLayoutDescFromVertexShaderSignature() will be able to make the changes work, but
+	//as of now, might be too much work with existing .vmesh files.
 	uint32_t boneIndices[4]{};
 	float weights[3]{};
 };
