587a3a37c337ee1644e7dc7c9828eead9228acc8
Cahntz <furshazzle@gmail.com>
2024-08-18 13:41:22 +1000

    Update property todo on making wrapper as template class

--- VEngine/Code/Core/Property.h
+++ VEngine/Code/Core/Property.h
@@ -8,11 +8,6 @@
 
 struct Property
 {
-	// @Todo: there's something I want to try with the 'change' function. Since most actors have the direct type
-	// of the property (e.g. bool isHot;), if you could wrap all properties on an actor in some class, what you could
-	// do is call the Change() function every time the variable is set/get, then you wouldn't have to do weird shit
-	// in Tick() where you're constantly looping over bools.
-
 	//Function to call when the set value is changed.
 	std::function<void(Property&)> change;
 
@@ -44,3 +39,36 @@ struct Property
 		return (T*)data;
 	}
 };
+
+// @Todo: there's something I want to try with the 'change' function. Since most actors have the direct type
+// of the property (e.g. bool isHot;), if you could wrap all properties on an actor in some class, what you could
+// do is call the Change() function every time the variable is set/get, then you wouldn't have to do weird shit
+// in Tick() where you're constantly looping over bools. It's too much to implement right now, but it does work
+// and needs a bit of heavy reworking with Properties. Below is basically how it would look.
+
+//template <typename T>
+//class PropertyWrapper
+//{
+//private:
+//	T _data;
+//	Property _prop;
+//
+//public:
+//	PropertyWrapper(T value) : _data(value) {}
+//
+//	T& Get() { return _data; }
+//
+//	void SetChange(std::function<void(Property&)> func)
+//	{
+//		_prop.change = func;
+//	}
+//
+//	void Set(T value)
+//	{
+//		_data = value;
+//		if (_prop.change)
+//		{
+//			_prop.change(_prop);
+//		}
+//	}
+//};
