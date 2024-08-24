a0f503a1bc70d8c06094c186d5c1a7df9792294f
Cahntz <furshazzle@gmail.com>
2024-08-24 13:08:51 +1000

    MapProjectionCrystal logic to toggle all projection actors on interact

--- VEngine/Code/Actors/Game/MapProjectionCrystal.cpp
+++ VEngine/Code/Actors/Game/MapProjectionCrystal.cpp
@@ -1,7 +1,13 @@
 #include "vpch.h"
 #include "MapProjectionCrystal.h"
+#include "MapProjectionActor.h"
 
 void MapProjectionCrystal::Interact()
 {
 	__super::Interact();
+
+	for (auto& actor : MapProjectionActor::system.GetActors())
+	{
+		actor->ToggleActive();
+	}
 }
