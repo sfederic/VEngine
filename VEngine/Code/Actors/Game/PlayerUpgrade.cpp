8fbfb318e44b968323a6b971af333aae7e81cba5
Cahntz <furshazzle@gmail.com>
2024-08-25 14:32:50 +1000

    Player upgrade Interact() code changes

--- VEngine/Code/Actors/Game/PlayerUpgrade.cpp
+++ VEngine/Code/Actors/Game/PlayerUpgrade.cpp
@@ -1,6 +1,8 @@
 #include "vpch.h"
 #include "PlayerUpgrade.h"
 #include "Components/MeshComponent.h"
+#include "Gameplay/GameInstance.h"
+#include "Core/Log.h"
 
 PlayerUpgrade::PlayerUpgrade()
 {
@@ -9,10 +11,30 @@ PlayerUpgrade::PlayerUpgrade()
 	SetRootComponent(_upgradeMesh);
 }
 
+void PlayerUpgrade::Create()
+{
+	__super::Create();
+
+	canBeMovedInLink = false;
+	canBeRotatedInLink = false;
+}
+
 Properties PlayerUpgrade::GetProps()
 {
 	auto props = __super::GetProps();
 	props.title = GetTypeName();
 	props.Add("Upgrade Name", &_upgradeGlobalMappingName);
 	return props;
+}
+
+void PlayerUpgrade::Interact()
+{
+	__super::Interact();
+
+	bool* upgradeProperty = GameInstance::GetGlobalProp<bool>(_upgradeGlobalMappingName);
+	if (upgradeProperty)
+	{
+		*upgradeProperty = true;
+		Log("[%s] added as player upgrade.", _upgradeGlobalMappingName.c_str());
+	}
 }
--- VEngine/Code/Actors/Game/PlayerUpgrade.h
+++ VEngine/Code/Actors/Game/PlayerUpgrade.h
@@ -1,17 +1,20 @@
 #pragma once
 
-#include "../Actor.h"
+#include "GridActor.h"
 
 class MeshComponent;
 
 //Physical actor that gives the player a new upgrade, think Metroid powerups in the world.
-class PlayerUpgrade : public Actor
+class PlayerUpgrade : public GridActor
 {
 public:
 	ACTOR_SYSTEM(PlayerUpgrade);
 
 	PlayerUpgrade();
+	void Create() override;
 	Properties GetProps() override;
+
+	void Interact() override;
 
 private:
 	std::string _upgradeGlobalMappingName;
