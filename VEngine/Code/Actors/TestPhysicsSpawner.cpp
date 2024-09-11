361f4b6c011af62263d34db0c22f28873a22a7f4
Cahntz <furshazzle@gmail.com>
2024-09-11 14:11:26 +1000

    TestPhysicsSpawner::End() function to remove all meshes

--- VEngine/Code/Actors/TestPhysicsSpawner.cpp
+++ VEngine/Code/Actors/TestPhysicsSpawner.cpp
@@ -13,14 +13,23 @@ void TestPhysicsSpawner::Start()
 {
 	__super::Start();
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 25; i++)
 	{
 		const auto spawnPos = meshSpawnTrigger->GetRandomPointInTrigger();
-		auto mesh = MeshComponent::system.Add(GetName() + std::to_string(i), this);
+		auto mesh = MeshComponent::system.Add(GetName() + "Mesh" + std::to_string(i), this);
 		mesh->SetWorldPosition(spawnPos);
 		mesh->SetMeshFilename("sphere.vmesh");
-		mesh->SetWorldScale(0.35f);
+		mesh->SetWorldScale(0.1f);
 		mesh->SetPhysicsStatic(false);
 		mesh->Create();
+	}
+}
+
+void TestPhysicsSpawner::End()
+{
+	for (auto mesh : GetComponents<MeshComponent>())
+	{
+		RemoveComponent(mesh);
+		mesh->Remove();
 	}
 }
--- VEngine/Code/Actors/TestPhysicsSpawner.h
+++ VEngine/Code/Actors/TestPhysicsSpawner.h
@@ -10,6 +10,7 @@ public:
 
 	TestPhysicsSpawner();
 	void Start() override;
+	void End() override;
 	DEFAULT_PROPS;
 
 private:
