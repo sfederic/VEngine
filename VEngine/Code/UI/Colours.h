5829f3e977046c6cdf74a97d8e6ae6ab6183cd5f
Cahntz <furshazzle@gmail.com>
2024-09-26 22:35:12 +1000

    Add magneta colour to Colous.h

--- VEngine/Code/UI/Colours.h
+++ VEngine/Code/UI/Colours.h
@@ -8,4 +8,5 @@ namespace Colours
 	static D2D1_COLOR_F Black = { 0.f, 0.f, 0.f, 1.f };
 	static D2D1_COLOR_F Grey = { 0.5f, 0.5f, 0.5f, 1.f };
 	static D2D1_COLOR_F Green = { 0.f, 1.f, 0.f, 1.f };
+	static D2D1_COLOR_F Magneta = { 1.f, 0.f, 1.f, 1.f };
 }
