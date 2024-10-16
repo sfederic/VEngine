d349fbab4fba16c2d8049edbca6566bc2b004d90
Cahntz <furshazzle@gmail.com>
2024-10-16 22:08:16 +1100

    Todo for localisation functions to replace .dialogue files

--- VEngine/Code/Asset/Localisation.h
+++ VEngine/Code/Asset/Localisation.h
@@ -2,6 +2,11 @@
 
 #include <string>
 
+//Todo: "Localisation" here is veeeery simple. Dialogue files are the option right now when you need
+//multiple lines of text and events to go through. The localisation functions here basically only
+//work for once off static text. Think about where a JSON string localisation system can go and
+//whether it can replace .dialogue files entirely.
+
 namespace Localise
 {
 	std::wstring GetString(const std::string& key, const std::string& filename);
