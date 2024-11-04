ec5fea6ca429ea9c8df972635bc8919dab7b4b8c
Cahntz <furshazzle@gmail.com>
2024-11-05 10:00:42 +1100

    Change Colours.h variables to constexpr from static

--- VEngine/Code/UI/Colours.h
+++ VEngine/Code/UI/Colours.h
@@ -4,9 +4,9 @@
 
 namespace Colours
 {
-	static D2D1_COLOR_F White = { 1.f, 1.f, 1.f, 1.f };
-	static D2D1_COLOR_F Black = { 0.f, 0.f, 0.f, 1.f };
-	static D2D1_COLOR_F Grey = { 0.5f, 0.5f, 0.5f, 1.f };
-	static D2D1_COLOR_F Green = { 0.f, 1.f, 0.f, 1.f };
-	static D2D1_COLOR_F Magneta = { 1.f, 0.f, 1.f, 1.f };
+	constexpr D2D1_COLOR_F White = { 1.f, 1.f, 1.f, 1.f };
+	constexpr D2D1_COLOR_F Black = { 0.f, 0.f, 0.f, 1.f };
+	constexpr D2D1_COLOR_F Grey = { 0.5f, 0.5f, 0.5f, 1.f };
+	constexpr D2D1_COLOR_F Green = { 0.f, 1.f, 0.f, 1.f };
+	constexpr D2D1_COLOR_F Magneta = { 1.f, 0.f, 1.f, 1.f };
 }
