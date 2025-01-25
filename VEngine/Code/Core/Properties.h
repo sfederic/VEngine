1a827ab1b9146f216eb59427f38da114c1cf227f
Cahntz <furshazzle@gmail.com>
2025-01-26 10:15:01 +1100

    Converted Property size calc to use std::is_same_v<T>() instead of reinterpret casts. Not sure on any performance gains on this or if it breaks anything.

--- VEngine/Code/Core/Properties.h
+++ VEngine/Code/Core/Properties.h
@@ -2,126 +2,121 @@
 
 #include <map>
 #include <vector>
+#include <string>
+#include <typeinfo>
+#include <typeindex>
+#include <cassert>
 #include "Property.h"
 #include "Render/RenderPropertyStructs.h"
 
 struct Properties
 {
-	std::string title;
-	std::map<std::string, Property> propMap;
-	UID ownerUID = 0;
+    std::string title;
+    std::map<std::string, Property> propMap;
+    UID ownerUID = 0;
 
-	Properties() {}
+    Properties() = default;
 
-	Properties(const char* title_)
-	{
-		title = title_;
-	}
+    explicit Properties(const char* title_) : title(title_) {}
 
-	bool Find(const std::string& name)
-	{
-		return propMap.find(name) != propMap.end();
-	}
+    bool Find(const std::string& name) const
+    {
+        return propMap.find(name) != propMap.end();
+    }
 
-	template <typename T>
-	Property& Add(const std::string& name, T* data)
-	{
-		Property prop = {};
+    template <typename T>
+    Property& Add(const std::string& name, T* data)
+    {
+        assert(propMap.find(name) == propMap.end() && "name key already exists in properties map");
 
-		if (typeid(T) == typeid(std::string))
-		{
-			auto str = reinterpret_cast<std::string*>(data);
-			prop.size = str->size();
-		}
-		else if (typeid(T) == typeid(std::wstring))
-		{
-			auto str = reinterpret_cast<std::wstring*>(data);
-			prop.size = str->size() * sizeof(wchar_t);
-		}
-		else if (typeid(T) == typeid(TextureData))
-		{
-			auto textureData = reinterpret_cast<TextureData*>(data);
-			prop.size = textureData->filename.size() * sizeof(char);
-		}
-		else if (typeid(T) == typeid(MeshComponentData))
-		{
-			auto meshComponentData = reinterpret_cast<MeshComponentData*>(data);
-			prop.size = meshComponentData->filename.size() * sizeof(char);
-		}
-		else
-		{
-			prop.size = sizeof(T);
-		}
+        Property prop;
+        SetPropertySize<T>(prop, data);
+        prop.info = typeid(T);
+        prop.name = name;
+        prop.data = data;
+        prop.ownerUID = ownerUID;
 
-		prop.info = typeid(T);
-		prop.name = name;
-		prop.data = data;
-		prop.ownerUID = ownerUID;
+        return propMap.emplace(prop.name, std::move(prop)).first->second;
+    }
 
-		//If you ever need the offset for a property, you can do it where you pass in the Actor/Object
-		//as a 'this' pointer through a template param. eg. <typename P> P* = this. Might help for serialisation?
-		//prop.offset = ((uint64_t*)data - (uint64_t*)parent) * sizeof(T);
-
-		assert(propMap.find(name) == propMap.end() && "name key already exists in properties map");
-
-		propMap.emplace(name, prop);
-
-		return propMap.find(name)->second;
-	}
-
-	void Merge(Properties& propsToMerge)
-	{
-		propMap.merge(propsToMerge.propMap);
-	}
+    void Merge(Properties& propsToMerge)
+    {
+        propMap.merge(propsToMerge.propMap);
+    }
 
 	//For properties that aren't from an actor or component, but fetched from within those GetProps() (e.g. Material)
-	void SetAllPropertyOwnerUIDs(UID ownerUID_)
-	{
-		ownerUID = ownerUID_;
+    void SetAllPropertyOwnerUIDs(UID ownerUID_)
+    {
+        ownerUID = ownerUID_;
+        for (auto& [name, prop] : propMap)
+        {
+            prop.ownerUID = ownerUID;
+        }
+    }
 
-		for (auto& [name, prop] : propMap)
-		{
-			prop.ownerUID = ownerUID;
-		}
-	}
-
-	template <typename T>
+    template <typename T>
 	bool CheckType(const std::string& name)
-	{
+    {
 		return propMap.find(name)->second.info.value() == typeid(T);
-	}
+    }
 
-	template <typename T>
+    template <typename T>
 	T* GetData(const std::string& name)
-	{
+    {
 		return reinterpret_cast<T*>(propMap.find(name)->second.data);
-	}
+    }
 
-	template <typename T>
+    template <typename T>
 	T* GetDataAllowNull(const std::string& name)
 	{
 		auto dataIt = propMap.find(name);
 		if (dataIt != propMap.end())
-		{
+    {
 			return reinterpret_cast<T*>(dataIt->second.data);
 		}
 		return nullptr;
-	}
+    }
 
 	std::type_index GetType(const std::string& name)
-	{
+    {
 		return propMap.find(name)->second.info.value();
-	}
+    }
 
 	Property* GetProperty(const std::string& name)
-	{
+    {
 		return &propMap.find(name)->second;
-	}
+    }
 
 	//Used to copy data in from other Properties (types have to be matching)
 	void CopyData(const std::string& name, Property& propToCopy);
 
 	//Copy matching vectors of properties across (currently only works for exact matching properties collections,
 	//should be able to work with partial matching collections though without the vectors).
-	static void CopyProperties(std::vector<Properties>& src, std::vector<Properties>& dst);
+    static void CopyProperties(std::vector<Properties>& src, std::vector<Properties>& dst);
+
+private:
+    template <typename T>
+    void SetPropertySize(Property& prop, T* data)
+    {
+        if constexpr (std::is_same_v<T, std::string>)
+        {
+            prop.size = data->size();
+        }
+        else if constexpr (std::is_same_v<T, std::wstring>)
+        {
+            prop.size = data->size() * sizeof(wchar_t);
+        }
+        else if constexpr (std::is_same_v<T, TextureData>)
+        {
+            prop.size = data->filename.size();
+        }
+        else if constexpr (std::is_same_v<T, MeshComponentData>)
+        {
+            prop.size = data->filename.size();
+        }
+        else
+        {
+            prop.size = sizeof(T);
+        }
+    }
 };
